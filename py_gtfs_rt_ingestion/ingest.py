01389d1c343fb375ebbc5b9fc076a7e12a4c375c
Mike Zappitello <mzappitello@gmail.com>
2022-05-13 15:28:44 -0400

    Make ingest dot py more testable
    * Split out `parseArgs` function so that it can be tested on its own
    * Give `run` function explicit arguments for testing
    * use both of these if running ingest as a script
    * add blocks to test ingest dot py to check that they are working as
      expected.

--- py_gtfs_rt_ingestion/ingest.py
+++ py_gtfs_rt_ingestion/ingest.py
@@ -1,7 +1,8 @@
-# from awsglue.utils import getResolvedOptions
 import argparse
 import gzip
 import json
+import sys
+
 from pathlib import Path
 import pyarrow as pa
 import pyarrow.parquet as pq
@@ -9,21 +10,28 @@ import pyarrow.parquet as pq
 from py_gtfs_rt_ingestion.helpers import get_datetime_from_header
 from py_gtfs_rt_ingestion.helpers import get_vehicle_schema
 
-def run() -> None:
-    """
-    Accepts VehiclePositions filename to parse 
+DESCRIPTION = ""
 
-    Currently utuilizing argparse.ArgumentParser()
-    Will require getResolveOptions from asglue.utils for Glue Job parameter parsing
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('fname', type=str, help='provide filename to ingest')
-    args = parser.parse_args()
 
-    if 'https_cdn.mbta.com_realtime_VehiclePositions_enhanced' not in args.fname:
+def parseArgs(args) -> dict:
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument(
+        '--fname',
+        dest='fname',
+        type=str,
+        required=True,
+        help='provide filename to ingest')
+
+    parsed_args = parser.parse_args(args)
+
+    return parsed_args
+
+
+def run(filename) -> None:
+    if 'https_cdn.mbta.com_realtime_VehiclePositions_enhanced' not in filename:
         raise Exception('Requires VehiclePostitions file.')
 
-    fname = Path(args.fname)
+    fname = Path(filename)
     schema = get_vehicle_schema()
 
     pa_table_dict = {k.name:[] for k in schema}
@@ -32,7 +40,7 @@ def run() -> None:
         json_data = json.loads(f.read())
         feed_timestamp = json_data['header']['timestamp']
         feed_dt = get_datetime_from_header(json_data)
-        
+
         for ent in json_data['entity']:
             v = ent['vehicle']
             r = {
@@ -53,18 +61,18 @@ def run() -> None:
             }
             if v['vehicle'].get('consist') is not None:
                 r['consist_labels'] = [c["label"] for c in v["vehicle"].get("consist")]
-                
+
             for k in r:
                 pa_table_dict[k].append(r[k])
 
     pa_table = pa.table(pa_table_dict, schema=schema)
     pq.write_to_dataset(
-        pa_table, 
-        root_path='./sample_data', 
+        pa_table,
+        root_path='./sample_data',
         partition_cols=['year','month','day','hour']
     )
 
+
 if __name__ == '__main__':
-    run()
-
-
+    args = parseArgs(sys.argv[1:])
+    run(args)
