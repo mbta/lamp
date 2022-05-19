fc4be31ec11f6186618b6874865a50309a905b42
Mike Zappitello <mzappitello@gmail.com>
2022-05-19 14:43:12 -0400

    Add batching from S3 Bucket Functionality
    Add ability to pull file information from our s3 bucket to batch files
    dot py using the boto3 module.
    
    the batch_files method is now expecting file info to come in as an
    iterable of tuples containing the filename and the filesize. the helper
    methods in batch files dot py pull these tuples out from files recording
    the output from `aws s3 ls <bucket>` and from s3 itself, depending on
    args.

--- py_gtfs_rt_ingestion/batch_files.py
+++ py_gtfs_rt_ingestion/batch_files.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 import argparse
+import boto3
 import sys
+
+from collections.abc import Iterable
 
 from py_gtfs_rt_ingestion import batch_files
 
@@ -13,14 +16,18 @@ def parseArgs(args) -> dict:
         '--input',
         dest='input_file',
         type=str,
-        required=True,
         help='text file of AWS S3 ls call')
+
+    parser.add_argument(
+        '--s3-prefix',
+        dest='s3_prefix',
+        type=str,
+        help='prefix to files in the mbta-gtfs-s3 bucket')
 
     parser.add_argument(
         '--threshold',
         dest='filesize_threshold',
         type=int,
-        required=True,
         default=100000,
         help='filesize threshold for batch sizes')
 
@@ -28,23 +35,38 @@ def parseArgs(args) -> dict:
 
     return parsed_args
 
-def file_list_from_file(input_file):
+def file_list_from_file(input_file: str) -> Iterable[(str, int)]:
     """
-    take a file that is the output of `aws s3 ls <dir>` and yield out objects
-    one at a time
+    take a file that is the output of `aws s3 ls <dir>` and yield out filename,
+    filesize tuples.
     """
-    for line in open(input_file):
-        yield line
+    for file_info in open(input_file):
+        (date, time, size, filename) = file_info.split()
+        yield (filename, sie)
 
-def make_file_list(args):
+def file_list_from_s3(file_prefix) -> Iterable[(str, int)]:
+    """
+    look in the mbta-gtfs-s3 bucket for files begining with this prefix and
+    yield out filename, filesize tuples.
+    """
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('mbta-gtfs-s3')
+
+    for bucket_object in bucket.objects.filter(Prefix=file_prefix):
+        yield (bucket_object.key, bucket_object.size)
+
+def make_file_list(args: dict) -> Iterable[(str, int)]:
+    """
+    based on args, yield out filename, filesize tuples either from a local file
+    or from our s3 bucket.
+    """
     if args.input_file:
         return file_list_from_file(args.input_file)
 
-    if args.s3_filepath:
-        raise Exception("haven't impl'ed yet")
+    if args.s3_prefix:
+        return file_list_from_s3(args.s3_prefix)
 
     raise Exception("shouldn't get here")
-
 
 if __name__ == '__main__':
     args = parseArgs(sys.argv[1:])
