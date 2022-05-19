83df4e365cf6e4f1b4aa73a3b3a2b0143cb9eaf4
Mike Zappitello <mzappitello@gmail.com>
2022-05-19 14:43:12 -0400

    Add batching from S3 Bucket Functionality
    Add ability to pull file information from our s3 bucket to batch files
    dot py using the boto3 module.
    
    the batch_files method is now expecting file info to come in as an
    iterable of tuples containing the filename and the filesize. the helper
    methods in batch files dot py pull these tuples out from files recording
    the output from `aws s3 ls <bucket>` and from s3 itself, depending on
    args.

--- py_gtfs_rt_ingestion/py_gtfs_rt_ingestion/batcher.py
+++ py_gtfs_rt_ingestion/py_gtfs_rt_ingestion/batcher.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 from .config_base import ConfigType
 
 class Batch(object):
@@ -5,38 +6,50 @@ class Batch(object):
     will store a collection of filenames that should be downloaded and converted
     from json into parquet.
     """
-    def __init__(self, config_type) -> None:
+    def __init__(self, config_type: ConfigType) -> None:
         self.config_type = config_type
         self.filenames = []
         self.total_size = 0
 
     def __str__(self) -> None:
-        return "%d mb across %d files of type %s" % (self.total_size,
-                                                     len(self.filenames),
-                                                     self.config_type)
+        return "%d bytes across %d files of type %s" % (self.total_size,
+                                                        len(self.filenames),
+                                                        self.config_type)
 
-    def add_file(self, filename, filesize) -> None:
+    def add_file(self, filename: str, filesize: int) -> None:
         self.filenames.append(filename)
         self.total_size += filesize
 
     def trigger_lambda(self) -> None:
         raise Exception("not impled yet")
 
-def batch_files(list_of_files, threshold) -> list:
+def batch_files(files: Iterable[(str, int)], threshold: int) -> list[Batch]:
+    """
+    Take a bunch of files and sort them into Batches based on their config type
+    (derrived from filename). Each Batch should be under a limit in total
+    filesize.
+
+    :param file: An iterable of filename and filesize tubles to be sorted into
+        Batches. The filename is used to determine config type.
+    :param threshold: upper bounds on how large the sum filesize of a batch can
+        be.
+
+    :return: List of Batches containing all files passed in.
+    """
+    ongoing_batches = {t: Batch(t) for t in ConfigType}
     complete_batches = []
-    ongoing_batches = {
-        ConfigType.RT_ALERTS: Batch(ConfigType.RT_ALERTS),
-        ConfigType.RT_TRIP_UPDATES: Batch(ConfigType.RT_TRIP_UPDATES),
-        ConfigType.RT_VEHICLE_POSITIONS: Batch(ConfigType.RT_VEHICLE_POSITIONS),
-        ConfigType.BUS_TRIP_UPDATES: Batch(ConfigType.BUS_TRIP_UPDATES),
-        ConfigType.BUS_VEHICLE_POSITIONS: Batch(ConfigType.BUS_VEHICLE_POSITIONS),
-        ConfigType.VEHICLE_COUNT: Batch(ConfigType.VEHICLE_COUNT)
-    }
 
-    for file_info in list_of_files:
-        (date, time, size, filename) = file_info.split()
+    # iterate over file tuples, and add them to the ongoing batches. if a batch
+    # is going to go past the threshold limit, move it to complete batches, and
+    # create a new batch.
+    for (filename, size) in files:
+        try:
+            config_type = ConfigType.from_filename(filename)
+        except:
+            # for now just print out the error.
+            print("Encontered Unknown File Type: %s" % filename)
+            continue
 
-        config_type = ConfigType.from_filename(filename)
         batch = ongoing_batches[config_type]
 
         if batch.total_size + int(size) > threshold:
@@ -46,9 +59,8 @@ def batch_files(list_of_files, threshold) -> list:
 
         batch.add_file(filename, int(size))
 
+    # add the ongoing batches too complete ones and return.
     for (_, batch) in ongoing_batches.items():
         complete_batches.append(batch)
 
     return complete_batches
-
-
