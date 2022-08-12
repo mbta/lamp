792acdd6d5abf82b3f679129c35bfb1ddeb2495f
Mike Zappitello <mzappitello@gmail.com>
2022-08-12 14:52:07 -0400

    Create startup dot py to launch application with
    startup dot py is the entry point into the performance manager
    processing event loop. its main processes schedules repeated calls to a
    function that will check the metadata table for unprocessed parquet
    files. if it finds them, they are passed to `process_vehicle_positions`,
    a function that converts vehicle position gtfs_rt parquet files into row
    entries in the vehicle position events table using the same logic that
    was in (the now deleted) load_vp_events dot py script. the performance
    manager image is now launched with a startup dot py invocation.

--- performance_manager/lib/rt_vehicle_positions.py
+++ performance_manager/lib/rt_vehicle_positions.py
@@ -1,20 +1,16 @@
 import logging
-
-import pandas
-import numpy
-import sqlalchemy
-import re
 import datetime
+import re
 
+from typing import Dict, List, Optional, Union
+
+import numpy
+import pandas
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 
-from typing import Optional
-from typing import Union
-from typing import List
-
 from .s3_utils import read_parquet
-from .postgres_utils import VehiclePositionEvents
+from .postgres_utils import VehiclePositionEvents, MetadataLog
 
 
 def get_vp_dataframe(to_load: Union[str, List[str]]) -> pandas.DataFrame:
@@ -45,94 +41,108 @@ def start_time_to_seconds(
     """
     if time is None:
         return time
-    (hour, min, sec) = time.split(":")
-    return int(hour) * 3600 + int(min) * 60 + int(sec)
+    (hour, minute, second) = time.split(":")
+    return int(hour) * 3600 + int(minute) * 60 + int(second)
 
 
-def transform_vp_dtyes(df: pandas.DataFrame) -> pandas.DataFrame:
+def transform_vp_dtyes(vehicle_positions: pandas.DataFrame) -> pandas.DataFrame:
     """
     ingest dataframe of vehicle position data from parquet file and transform
     column datatypes
     """
     # current_staus: 1 = MOVING, 0 = STOPPED_AT
-    df["is_moving"] = numpy.where(
-        df["current_status"] != "STOPPED_AT", True, False
+    vehicle_positions["is_moving"] = numpy.where(
+        vehicle_positions["current_status"] != "STOPPED_AT", True, False
     ).astype(numpy.bool8)
-    df = df.drop(columns=["current_status"])
+    vehicle_positions = vehicle_positions.drop(columns=["current_status"])
 
     # store start_date as Int64 [nullable] instead of string
-    df["start_date"] = pandas.to_numeric(df["start_date"]).astype("Int64")
+    vehicle_positions["start_date"] = pandas.to_numeric(
+        vehicle_positions["start_date"]
+    ).astype("Int64")
 
     # store current_stop_sequence as Int64 [nullable]
-    df["current_stop_sequence"] = pandas.to_numeric(
-        df["current_stop_sequence"]
+    vehicle_positions["current_stop_sequence"] = pandas.to_numeric(
+        vehicle_positions["current_stop_sequence"]
     ).astype("Int64")
 
     # store direction_id as Int64 [nullable]
-    df["direction_id"] = pandas.to_numeric(df["direction_id"]).astype("Int64")
+    vehicle_positions["direction_id"] = pandas.to_numeric(
+        vehicle_positions["direction_id"]
+    ).astype("Int64")
 
     # store start_time as seconds from start of day (Int64 [nullable])
-    df["start_time"] = (
-        df["start_time"].apply(start_time_to_seconds).astype("Int64")
+    vehicle_positions["start_time"] = (
+        vehicle_positions["start_time"]
+        .apply(start_time_to_seconds)
+        .astype("Int64")
     )
 
     # hash row data, exluding vehicle_timestamp column
     # This hash is used to identify continuous series of records with the same
     # categorical data
-    df["hash"] = df[list(set(df.columns) - {"vehicle_timestamp"})].apply(
-        lambda row: hash(tuple(row)), axis=1
-    )
+    vehicle_positions["hash"] = vehicle_positions[
+        list(set(vehicle_positions.columns) - {"vehicle_timestamp"})
+    ].apply(lambda row: hash(tuple(row)), axis=1)
 
     # drop duplicates of hash and vehicle_timestamp columns (extraneous data)
-    df = df.drop_duplicates(subset=["vehicle_timestamp", "hash"])
+    vehicle_positions = vehicle_positions.drop_duplicates(
+        subset=["vehicle_timestamp", "hash"]
+    )
 
-    return df
+    return vehicle_positions
 
 
-def transform_vp_timestamps(df: pandas.DataFrame) -> pandas.DataFrame:
+def transform_vp_timestamps(
+    vehicle_positions: pandas.DataFrame,
+) -> pandas.DataFrame:
     """
     convert raw vehicle position data with one timestamp field into
     vehicle position events with timestamp_start and timestamp_end
     """
-    df = df.sort_values(by=["vehicle_id", "vehicle_timestamp"])
-
-    """
-    calculate mask to identify time series hash changes
-    this keeps first and last event for matching consecutive hash events
-    result is a 'start' event and 'end' event in consecutive rows for 
-    matching hash events
-    """
-    first_last_mask = (df["hash"] - df["hash"].shift(1) != 0) | (
-        df["hash"] - df["hash"].shift(-1) != 0
+    vehicle_positions = vehicle_positions.sort_values(
+        by=["vehicle_id", "vehicle_timestamp"]
     )
-    df = df.loc[first_last_mask, :]
 
-    """
-    transform vehicle_timestamp with matching consectuive hash events
-    into one record with timestamp_start and timestamp_end columns
-    if event is not part of consectuive matching events, then 
-    timestamp_start and timestamp_end will be same value
-    """
-    df.rename(columns={"vehicle_timestamp": "timestamp_start"}, inplace=True)
-    df["timestamp_end"] = numpy.where(
-        (df["hash"] - df["hash"].shift(-1) == 0),
-        df["timestamp_start"].shift(-1),
-        df["timestamp_start"],
+    # calculate mask to identify time series hash changes
+    # this keeps first and last event for matching consecutive hash events
+    # result is a 'start' event and 'end' event in consecutive rows for
+    # matching hash events
+    first_last_mask = (
+        vehicle_positions["hash"] - vehicle_positions["hash"].shift(1) != 0
+    ) | (vehicle_positions["hash"] - vehicle_positions["hash"].shift(-1) != 0)
+    vehicle_positions = vehicle_positions.loc[first_last_mask, :]
+
+    # transform vehicle_timestamp with matching consectuive hash events into
+    # one record with timestamp_start and timestamp_end columns if event is not
+    # part of consectuive matching events, then timestamp_start and
+    # timestamp_end will be same value
+    vehicle_positions.rename(
+        columns={"vehicle_timestamp": "timestamp_start"}, inplace=True
+    )
+    vehicle_positions["timestamp_end"] = numpy.where(
+        (vehicle_positions["hash"] - vehicle_positions["hash"].shift(-1) == 0),
+        vehicle_positions["timestamp_start"].shift(-1),
+        vehicle_positions["timestamp_start"],
     ).astype("int64")
 
-    """
-    for matching consectuive hash events, drop 2nd event because timestamp
-    has been captured in timestamp_end value of previous event
-    """
-    drop_last_mask = df["hash"] - df["hash"].shift(1) != 0
-    df = df.loc[drop_last_mask, :].reset_index(drop=True)
+    # for matching consectuive hash events, drop 2nd event because timestamp has
+    # been captured in timestamp_end value of previous event
+    drop_last_mask = (
+        vehicle_positions["hash"] - vehicle_positions["hash"].shift(1) != 0
+    )
+    vehicle_positions = vehicle_positions.loc[drop_last_mask, :].reset_index(
+        drop=True
+    )
 
-    df.insert(0, "pk_id", numpy.nan)
+    vehicle_positions.insert(0, "pk_id", numpy.nan)
 
     # fill all dataframe na values with Python None (for DB writing)
-    df = df.fillna(numpy.nan).replace([numpy.nan], [None])
+    vehicle_positions = vehicle_positions.fillna(numpy.nan).replace(
+        [numpy.nan], [None]
+    )
 
-    return df
+    return vehicle_positions
 
 
 def get_utc_timestamp(path: str) -> float:
@@ -143,23 +153,21 @@ def get_utc_timestamp(path: str) -> float:
     month = int(re.findall(r"month=(\d{1,2})", path)[0])
     day = int(re.findall(r"day=(\d{1,2})", path)[0])
     hour = int(re.findall(r"hour=(\d{1,2})", path)[0])
-    dt = datetime.datetime(
+    date = datetime.datetime(
         year=year, month=month, day=day, hour=hour, tzinfo=datetime.timezone.utc
     )
-    return datetime.datetime.timestamp(dt)
+    return datetime.datetime.timestamp(date)
 
 
-def merge_vehicle_position_events(
+def get_event_overlap(
     folder: str,
     new_events: pandas.DataFrame,
-    session: sqlalchemy.orm.session.sessionmaker,
-) -> None:
+    sql_session: sessionmaker,
+) -> pandas.DataFrame:
     """
-    merge new events from parquet file with exiting events found in database
-    new events will be merged with existing events in a 5 minutes window
-    surrounding the year/month/day/hour value found in path of parquet files
+    get a dataframe consisting of the new events and any overlapping events from
+    the vehicle positions events table, sorted by vehicle id and then timestamp
     """
-
     start_window_ts = get_utc_timestamp(folder)
     timestamp_to_pull_min = start_window_ts - 300
     timestamp_to_pull_max = start_window_ts + 300 + 3600
@@ -175,23 +183,31 @@ def merge_vehicle_position_events(
         (VehiclePositionEvents.timestamp_start > timestamp_to_pull_min)
         & (VehiclePositionEvents.timestamp_end < timestamp_to_pull_max)
     )
-    with session.begin() as s:  # type: ignore
-        if s.execute(get_db_events).first() is None:
-            logging.info(f"No merge events found in database.")
-            merge_events = new_events.sort_values(
-                by=["vehicle_id", "timestamp_start"]
-            )
-        else:
-            # Join records from db with records from parquet
-            # db records should contain 'index' column that does not exist in parquet dataset
-            merge_events = pandas.concat(
-                [
-                    pandas.DataFrame(
-                        [r._asdict() for r in s.execute(get_db_events)]
-                    ),
-                    new_events,
-                ]
-            ).sort_values(by=["vehicle_id", "timestamp_start"])
+
+    with sql_session.begin() as session:  # type: ignore
+        # Join records from db with records from parquet db records should
+        # contain 'index' column that does not exist in parquet dataset
+        return pandas.concat(
+            [
+                pandas.DataFrame(
+                    [row._asdict() for row in session.execute(get_db_events)]
+                ),
+                new_events,
+            ]
+        ).sort_values(by=["vehicle_id", "timestamp_start"])
+
+
+def merge_vehicle_position_events(
+    folder: str,
+    new_events: pandas.DataFrame,
+    session: sessionmaker,
+) -> None:
+    """
+    merge new events from parquet file with exiting events found in database
+    new events will be merged with existing events in a 5 minutes window
+    surrounding the year/month/day/hour value found in path of parquet files
+    """
+    merge_events = get_event_overlap(folder, new_events, session)
 
     # Identify records that are continuing from existing db
     # If such records are found, update timestamp_end with latest value
@@ -216,32 +232,35 @@ def merge_vehicle_position_events(
     )
 
     logging.info(
-        f"Size of existing update df: {existing_was_updated_mask.sum():,}"
+        "Size of existing update vehicle_positions: %d",
+        existing_was_updated_mask.sum(),
     )
-    logging.info(f"Size of existing delete df: {existing_to_del_mask.sum():,}")
+    logging.info(
+        "Size of existing delete vehicle_positions: %d",
+        existing_to_del_mask.sum(),
+    )
 
-    """
-    new events that will be inserted into db table
-    """
+    # new events that will be inserted into db table
     new_to_insert_mask = (
         merge_events["pk_id"].isna()
     ) & ~last_of_consecutive_events
-    logging.info(f"Size of new insert df: {new_to_insert_mask.sum():,}")
+    logging.info(
+        "Size of new insert vehicle_positions: %d", new_to_insert_mask.sum()
+    )
 
-    """
-    new events that were used to update existing records, will not be used
-    """
+    # new events that were used to update existing records, will not be used
     new_to_drop_mask = (
         merge_events["pk_id"].isna()
     ) & last_of_consecutive_events
-    logging.info(f"Size of new being dropped df: {new_to_drop_mask.sum():,}")
+    logging.info(
+        "Size of new being dropped vehicle_positions: %d",
+        new_to_drop_mask.sum(),
+    )
 
-    """
-    DB UPDATE operation
-    """
+    # DB UPDATE operation
     if existing_was_updated_mask.sum() > 0:
         update_db_events = sa.update(VehiclePositionEvents.__table__).where(
-            VehiclePositionEvents.pk_id == sqlalchemy.bindparam("b_pk_id")
+            VehiclePositionEvents.pk_id == sa.bindparam("b_pk_id")
         )
         with session.begin() as s:  # type: ignore
             s.execute(
@@ -251,9 +270,7 @@ def merge_vehicle_position_events(
                 .to_dict(orient="records"),
             )
 
-    """
-    DB DELETE operation
-    """
+    # DB DELETE operation
     if existing_to_del_mask.sum() > 0:
         delete_db_events = sa.delete(VehiclePositionEvents.__table__).where(
             VehiclePositionEvents.pk_id.in_(
@@ -263,9 +280,7 @@ def merge_vehicle_position_events(
         with session.begin() as s:  # type: ignore
             s.execute(delete_db_events)
 
-    """
-    DB INSERT operation
-    """
+    # DB INSERT operation
     if new_to_insert_mask.sum() > 0:
         insert_cols = list(set(merge_events.columns) - {"pk_id"})
         with session.begin() as s:  # type: ignore
@@ -275,3 +290,45 @@ def merge_vehicle_position_events(
                     orient="records"
                 ),
             )
+
+
+def process_vehicle_positions(
+    paths_to_load: Dict[str, Dict[str, List]], sql_session: sessionmaker
+) -> None:
+    """
+    process a bunch of vehicle position files
+    create events for them
+    merge those events with existing events
+    """
+    for folder, folder_data in paths_to_load.items():
+        ids = folder_data["ids"]
+        paths = folder_data["paths"]
+
+        try:
+            new_events = get_vp_dataframe(paths)
+            logging.info(
+                "Size of dataframe from %s is %d", folder, new_events.shape[0]
+            )
+            new_events = transform_vp_dtyes(new_events)
+            logging.info(
+                "Size of dataframe with updated dtypes is %d",
+                new_events.shape[0],
+            )
+            new_events = transform_vp_timestamps(new_events)
+            logging.info(
+                "Size of dataframe with transformed timestamps is %d",
+                new_events.shape[0],
+            )
+
+            merge_vehicle_position_events(str(folder), new_events, sql_session)
+        except Exception as e:
+            logging.info("Error Processing Vehicle Positions")
+            logging.exception(e)
+        else:
+            update_md_log = (
+                sa.update(MetadataLog.__table__)
+                .where(MetadataLog.pk_id.in_(ids))
+                .values(processed=1)
+            )
+            with sql_session.begin() as session:  # type: ignore
+                session.execute(update_md_log)
