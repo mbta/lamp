a08040fd2f7e55a1c5f735c48b3b2d9553c154ad
Mike Zappitello <mzappitello@mbta.com>
2022-11-03 15:24:26 -0400

    add mechanism to sort out incoming files

--- py_gtfs_rt_ingestion/lib/utils.py
+++ py_gtfs_rt_ingestion/lib/utils.py
@@ -1,5 +1,8 @@
 import os
 import logging
+import pathlib
+
+from typing import List, Dict
 
 
 def load_environment() -> None:
@@ -31,3 +34,35 @@ def load_environment() -> None:
     except Exception as exception:
         logging.exception("error while trying to bootstrap")
         raise exception
+
+
+def group_sort_file_list(filepaths: List[str]) -> Dict[str, List[str]]:
+    """
+    group and sort list of filepaths by filename
+
+    expects s3 file paths that can be split on timestamp:
+
+    full_path:
+    s3://mbta-ctd-dataplatform-dev-incoming/lamp/delta/2022/10/12/2022-10-12T23:58:52Z_https_cdn.mbta.com_MBTA_GTFS.zip
+
+    splits "2022-10-12T23:58:52Z_https_cdn.mbta.com_MBTA_GTFS.zip"
+    from full_path
+
+    into
+     - 2022-10-12T23:58:52Z
+     - https_cdn.mbta.com_MBTA_GTFS.zip
+
+    groups by "https_cdn.mbta.com_MBTA_GTFS.zip"
+    """
+    grouped_files: Dict[str, List[str]] = {}
+
+    for file in filepaths:
+        _, file_type = pathlib.Path(file).name.split("_", maxsplit=1)
+        if file_type not in grouped_files:
+            grouped_files[file_type] = []
+        grouped_files[file_type].append(file)
+
+    for group in grouped_files.values():
+        group.sort(key=lambda s3_path: pathlib.Path(s3_path).name)
+
+    return grouped_files
