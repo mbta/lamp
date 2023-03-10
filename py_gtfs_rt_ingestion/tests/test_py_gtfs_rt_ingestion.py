d2b42996a7c60ae2f945cd70f6905a82c850d169
Mike Zappitello <mzappitello@mbta.com>
2023-03-10 17:13:20 -0500

    Move Python Tests Code into Mono-Package
    * Move tests from previous packages into mono-package tests directory.
      This new directory mimics the directory structure of the new source
      directory.
    * Move the Ingestion Tests over and fix problems with them.
    * Move the Performance Tests over as well.

--- py_gtfs_rt_ingestion/tests/test_py_gtfs_rt_ingestion.py
+++
@@ -1,6 +0,0 @@
-from lib import __version__
-
-
-def test_version() -> None:
-    """simple test stub"""
-    assert __version__ == "0.1.0"
---
+++ python_src/tests/ingestion/test_py_gtfs_rt_ingestion.py
@@ -0,0 +1,6 @@
+from lamp_py.ingestion import __version__
+
+
+def test_version() -> None:
+    """simple test stub"""
+    assert __version__ == "0.1.0"
