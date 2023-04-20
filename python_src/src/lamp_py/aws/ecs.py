238c62ececc2dd164f4765b04dab384a6693d26b
Ryan Rymarczyk <rymarczy@gmail.com>
2023-04-20 12:06:07 -0400

    verbose ecs logging

--- python_src/src/lamp_py/aws/ecs.py
+++ python_src/src/lamp_py/aws/ecs.py
@@ -1,16 +1,20 @@
-import logging
+import time
 import os
 import sys
 from multiprocessing import Process, Queue
 from typing import Any, Optional
+
+from lamp_py.runtime_utils.process_logger import ProcessLogger
 
 
 def handle_ecs_sigterm(_: int, __: Any) -> None:
     """
     handler function for when ECS recieves ECS SIGTERM
     """
-    logging.info("AWS ECS SIGTERM received")
+    process_logger = ProcessLogger("sigterm_received")
+    process_logger.log_start()
     os.environ["GOT_SIGTERM"] = "TRUE"
+    process_logger.log_complete()
 
 
 def check_for_sigterm(
@@ -21,12 +25,18 @@ def check_for_sigterm(
     check if SIGTERM recived from ECS. If found, terminate process.
     """
     if os.environ.get("GOT_SIGTERM") is not None:
-        logging.info("SIGTERM received, terminating process...")
+        process_logger = ProcessLogger("stopping_ecs")
+        process_logger.log_start()
 
         # send signal to stop rds writer process and wait for exit
         if metadata_queue is not None:
             metadata_queue.put(None)
         if rds_process is not None:
             rds_process.join()
+
+        process_logger.log_complete()
+
+        # delay for log statements to write before ecs death
+        time.sleep(5)
 
         sys.exit()
