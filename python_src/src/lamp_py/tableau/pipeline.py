bf0a368d01609cd86a2eebe5ec8a764e6e5f29c5
Ryan Rymarczyk <rymarczy@gmail.com>
2023-10-27 10:28:17 -0400

    FIX: Tableau Server Import Exception (#179 )
    `tableau_server` function call being in function signature was causing Exception at import time. Move function calls out of parameter declaration.
    
    Also move HyperJob creation into function to avoid creation of jobs at import.

--- python_src/src/lamp_py/tableau/pipeline.py
+++ python_src/src/lamp_py/tableau/pipeline.py
@@ -1,7 +1,9 @@
 import os
+from typing import List
 
 from lamp_py.runtime_utils.env_validation import validate_environment
 
+from lamp_py.tableau.hyper import HyperJob
 from lamp_py.tableau.jobs.rt_rail import HyperRtRail
 from lamp_py.tableau.jobs.gtfs_rail import (
     HyperServiceIdByRoute,
@@ -14,17 +16,20 @@ from lamp_py.tableau.jobs.gtfs_rail import (
     HyperStaticTrips,
 )
 
-HYPER_JOBS = (
-    HyperRtRail(),
-    HyperServiceIdByRoute(),
-    HyperStaticCalendar(),
-    HyperStaticCalendarDates(),
-    HyperStaticFeedInfo(),
-    HyperStaticRoutes(),
-    HyperStaticStops(),
-    HyperStaticStopTimes(),
-    HyperStaticTrips(),
-)
+
+def create_hyper_jobs() -> List[HyperJob]:
+    """Create Hyper Jobs to Run"""
+    return [
+        HyperRtRail(),
+        HyperServiceIdByRoute(),
+        HyperStaticCalendar(),
+        HyperStaticCalendarDates(),
+        HyperStaticFeedInfo(),
+        HyperStaticRoutes(),
+        HyperStaticStops(),
+        HyperStaticStopTimes(),
+        HyperStaticTrips(),
+    ]
 
 
 def start_hyper_updates() -> None:
@@ -39,12 +44,11 @@ def start_hyper_updates() -> None:
             "PUBLIC_ARCHIVE_BUCKET",
         ],
     )
-
-    for job in HYPER_JOBS:
+    for job in create_hyper_jobs():
         job.run_hyper()
 
 
 def start_parquet_updates() -> None:
     """Run all Parquet Update jobs"""
-    for job in HYPER_JOBS:
+    for job in create_hyper_jobs():
         job.run_parquet()
