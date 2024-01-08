39a5843e8bc8075b1aff6897dee9e9d6d2d9ec3c
Mike Zappitello <mzappitello@mbta.com>
2024-01-08 14:42:29 -0500

    Don't Install Tableau API on arm64
    The tableauhyperapi module is unavailable on mac with arm64 processors.
    
    * Update poetry to take advantage of dependency groups.
    * Add a tableau dependency group that can be excluded when installing
      dependencies.
    * Update python_src to fallback with error messages when using the
      tableau subdir that creates hyperfiles fails to import everything.
    * Use new `BUILDPLATFORM` arg in dockerfile to build without tableau
      deps on an arm64 platform.
    * Add an export for the BUILDPLATFORM variable to .envrc and pass it
      through via docker-compose when building the lamp py images.

--- python_src/src/lamp_py/tableau/__init__.py
+++ python_src/src/lamp_py/tableau/__init__.py
@@ -1,1 +1,29 @@
-"""Utilites for Interacting with Tableau and Hyper files"""
+"""Utilities for Interacting with Tableau and Hyper files"""
+
+try:
+    # pylint: disable=C0414
+    #
+    # Import alias does not rename original package. The intent is to grab it
+    # here and pass it through other portions of the codebase.
+    from .pipeline import start_parquet_updates as start_parquet_updates
+
+    # pylint: enable=C0414
+
+except ModuleNotFoundError as mfl_exception:
+    import logging
+    from lamp_py.postgres.postgres_utils import DatabaseManager
+
+    # pylint: disable=W0613
+    #
+    # db_manaager is unused because this method has to match the function
+    # signature of the method its replacing.
+    def start_parquet_updates(db_manager: DatabaseManager) -> None:
+        """
+        re-implimentation of start parquet updates in the event that the
+        tableauhyperapi module cannot be found.
+        """
+        logging.exception(
+            "Unable to run parquet files on this machine due to Module Not Found error"
+        )
+
+    # pylint: enable=W0613
