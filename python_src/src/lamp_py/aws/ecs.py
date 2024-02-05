93a0066a93db57e245044f3c4d9fae887a372c81
Ryan Rymarczyk <rymarczy@gmail.com>
2024-02-05 15:09:48 -0500

    FEAT: Add parallel task checking to Tableau Publisher (#230)
    Adds a check to the Tableau Publisher pipeline to make sure that only one instance of the application is running at a time.
    
    Asana Task: https://app.asana.com/0/1205827492903547/1206184120740467

--- python_src/src/lamp_py/aws/ecs.py
+++ python_src/src/lamp_py/aws/ecs.py
@@ -5,6 +5,8 @@ from multiprocessing import Process
 from queue import Queue
 from typing import Any, Optional
 
+import boto3
+
 from lamp_py.runtime_utils.process_logger import ProcessLogger
 
 
@@ -41,3 +43,53 @@ def check_for_sigterm(
         time.sleep(5)
 
         sys.exit()
+
+
+def running_in_aws() -> bool:
+    """
+    return True if running on aws, else False
+    """
+    return bool(os.getenv("AWS_DEFAULT_REGION"))
+
+
+def check_for_parallel_tasks() -> None:
+    """
+    Check that that this task is not already running on ECS
+    """
+    if not running_in_aws():
+        return
+
+    process_logger = ProcessLogger("check_for_tasks")
+    process_logger.log_start()
+
+    client = boto3.client("ecs")
+    ecs_cluster = os.environ["ECS_CLUSTER"]
+    ecs_task_group = os.environ["ECS_TASK_GROUP"]
+
+    try:
+        # get all of the tasks running on the cluster
+        task_arns = client.list_tasks(cluster=ecs_cluster)["taskArns"]
+
+        # if tasks are running on the cluster, get their descriptions and check to
+        # count matches the ecs task group.
+        match_count = 0
+        if task_arns:
+            running_tasks = client.describe_tasks(
+                cluster=ecs_cluster, tasks=task_arns
+            )["tasks"]
+
+            for task in running_tasks:
+                if ecs_task_group == task["group"]:
+                    match_count += 1
+
+        # if the group matches, raise an exception that will terminate the process
+        if match_count > 1:
+            raise SystemError(
+                f"Multiple {ecs_task_group} ECS Tasks Running in {ecs_cluster}"
+            )
+
+    except Exception as exception:
+        process_logger.log_failure(exception)
+        raise exception
+
+    process_logger.log_complete()
