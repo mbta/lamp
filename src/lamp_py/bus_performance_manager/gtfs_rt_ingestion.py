c695ae2de63babf92c2c39ce21f568ffe0bc9c9e
Mike Zappitello <mzappitello@mbta.com>
2024-08-06 09:53:18 -0400

    Address Final Comments from PR 410
    * split gtfs_rt bus ingestion into two functions to allow better testing
    * fix formatting errors
    * ensure the resulting schema from vehicle position pivot is consistent
      regardless of input

--- src/lamp_py/bus_performance_manager/gtfs_rt_ingestion.py
+++ src/lamp_py/bus_performance_manager/gtfs_rt_ingestion.py
@@ -8,13 +8,31 @@ from lamp_py.bus_performance_manager.gtfs_utils import (
 )
 
 
-def generate_gtfs_rt_events(
+def read_vehicle_positions(
     service_date: date, gtfs_rt_files: List[str]
 ) -> pl.DataFrame:
     """
-    generate a polars dataframe for bus vehicle events from gtfs realtime
-    vehicle position files for a given service date
+    Read gtfs realtime vehicle position files and pull out unique bus vehicle
+    positions for a given service day.
+
+    :param service_date: the service date to filter on
+    :param gtfs_rt_files: a list of gtfs realtime files, either s3 urls or a
+        local path
+
+    :return dataframe:
+        route_id -> String
+        trip_id -> String
+        stop_id -> String
+        stop_sequence -> String
+        direction_id -> Int8
+        start_time -> String
+        service_date -> String
+        vehicle_id -> String
+        vehicle_label -> String
+        current_status -> String
+        vehicle_timestamp -> Datetime
     """
+
     bus_routes = bus_routes_for_service_date(service_date)
 
     # build a dataframe of every gtfs record
@@ -42,7 +60,9 @@ def generate_gtfs_rt_events(
             pl.col("vehicle.trip.route_id").cast(pl.String).alias("route_id"),
             pl.col("vehicle.trip.trip_id").cast(pl.String).alias("trip_id"),
             pl.col("vehicle.stop_id").cast(pl.String).alias("stop_id"),
-            pl.col("vehicle.current_stop_sequence").cast(pl.String).alias("stop_sequence"),
+            pl.col("vehicle.current_stop_sequence")
+            .cast(pl.String)
+            .alias("stop_sequence"),
             pl.col("vehicle.trip.direction_id")
             .cast(pl.Int8)
             .alias("direction_id"),
@@ -53,7 +73,9 @@ def generate_gtfs_rt_events(
             .cast(pl.String)
             .alias("service_date"),
             pl.col("vehicle.vehicle.id").cast(pl.String).alias("vehicle_id"),
-            pl.col("vehicle.vehicle.label").cast(pl.String).alias("vehicle_label"),
+            pl.col("vehicle.vehicle.label")
+            .cast(pl.String)
+            .alias("vehicle_label"),
             pl.col("vehicle.current_status")
             .cast(pl.String)
             .alias("current_status"),
@@ -70,40 +92,106 @@ def generate_gtfs_rt_events(
         .collect()
     )
 
-    if vehicle_positions.is_empty():
-        # if there are now elements, adjust the columns to match the return
-        # dataframe schema.
-        vehicle_events = vehicle_positions.with_columns(
-            pl.lit(None).cast(pl.Datetime).alias("arrival_gtfs"),
-            pl.lit(None).cast(pl.Datetime).alias("travel_towards_gtfs"),
-        ).drop("current_status", "vehicle_timestamp")
+    return vehicle_positions
 
-    else:
-        # using the vehicle positions dataframe, create a dataframe for each
-        # event by pivoting and mapping the current status onto arrivals and
-        # departures. initially, they map to arrival times and traveling
-        # towards times. shift the traveling towards datetime over trip id to
-        # get the departure time.
-        vehicle_events = vehicle_positions.pivot(
-            values="vehicle_timestamp",
-            aggregate_function="min",
-            index=[
-                "route_id",
-                "direction_id",
-                "trip_id",
-                "stop_id",
-                "stop_sequence",
-                "start_time",
-                "service_date",
-                "vehicle_id",
-                "vehicle_label"
-            ],
-            columns="current_status",
-        ).rename(
-            {
-                "STOPPED_AT": "arrival_gtfs",
-                "IN_TRANSIT_TO": "travel_towards_gtfs",
-            }
-        )
+
+def positions_to_events(vehicle_positions: pl.DataFrame) -> pl.DataFrame:
+    """
+    using the vehicle positions dataframe, create a dataframe for each event by
+    pivoting and mapping the current status onto arrivals and departures.
+
+    :param vehicle_positions: Dataframe of vehiclie positions
+
+    :return dataframe:
+        route_id -> String
+        trip_id -> String
+        stop_id -> String
+        stop_sequence -> String
+        direction_id -> Int8
+        start_time -> String
+        service_date -> String
+        vehicle_id -> String
+        vehicle_label -> String
+        current_status -> String
+        arrival_gtfs -> Datetime
+        travel_towards_gtfs -> Datetime
+    """
+    vehicle_events = vehicle_positions.pivot(
+        values="vehicle_timestamp",
+        aggregate_function="min",
+        index=[
+            "route_id",
+            "direction_id",
+            "trip_id",
+            "stop_id",
+            "stop_sequence",
+            "start_time",
+            "service_date",
+            "vehicle_id",
+            "vehicle_label",
+        ],
+        columns="current_status",
+    )
+
+    for column in ["STOPPED_AT", "IN_TRANSIT_TO"]:
+        if column not in vehicle_events.columns:
+            vehicle_events = vehicle_events.with_columns(
+                pl.lit(None).cast(pl.Datetime).alias(column)
+            )
+
+    vehicle_events = vehicle_events.rename(
+        {
+            "STOPPED_AT": "arrival_gtfs",
+            "IN_TRANSIT_TO": "travel_towards_gtfs",
+        }
+    ).select(
+        [
+            "route_id",
+            "trip_id",
+            "stop_id",
+            "stop_sequence",
+            "direction_id",
+            "start_time",
+            "service_date",
+            "vehicle_id",
+            "vehicle_label",
+            "arrival_gtfs",
+            "travel_towards_gtfs",
+        ]
+    )
+
+    return vehicle_events
+
+
+def generate_gtfs_rt_events(
+    service_date: date, gtfs_rt_files: List[str]
+) -> pl.DataFrame:
+    """
+    generate a polars dataframe for bus vehicle events from gtfs realtime
+    vehicle position files for a given service date
+
+    :param service_date: the service date to filter on
+    :param gtfs_rt_files: a list of gtfs realtime files, either s3 urls or a
+        local path
+
+    :return dataframe:
+        route_id -> String
+        trip_id -> String
+        stop_id -> String
+        stop_sequence -> String
+        direction_id -> Int8
+        start_time -> String
+        service_date -> String
+        vehicle_id -> String
+        vehicle_label -> String
+        current_status -> String
+        arrival_gtfs -> Datetime
+        travel_towards_gtfs -> Datetime
+    """
+    vehicle_positions = read_vehicle_positions(
+        service_date=service_date, gtfs_rt_files=gtfs_rt_files
+    )
+
+    vehicle_events = positions_to_events(vehicle_positions=vehicle_positions)
 
     return vehicle_events
--- tests/bus_performance_manager/test_gtfs_rt_ingestion.py
+++ tests/bus_performance_manager/test_gtfs_rt_ingestion.py
@@ -1,24 +1,27 @@
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
+from typing import Tuple, List
 
 import polars as pl
 
 from lamp_py.aws.s3 import get_datetime_from_partition_path
 from lamp_py.bus_performance_manager.gtfs_rt_ingestion import (
+    read_vehicle_positions,
+    positions_to_events,
     generate_gtfs_rt_events,
 )
 
 from ..test_resources import LocalFileLocaions
 
 
-def test_gtfs_rt_to_bus_events() -> None:
+def get_service_date_and_files() -> Tuple[date, List[str]]:
     """
-    generate vehicle event dataframes from gtfs realtime vehicle position files.
-    inspect them to ensure they
-        * are not empty
-        * have records matching known properties
+    get a list of service dates and vehicle position files from the vehicle
+    positions test files directory. assert that they are all for the same
+    service date and return them along with it.
 
-    generate an empty vechicle event frame and ensure it has the correct columns.
+    NOTE: for now, we have to use just 2024 dates, as there are 2023 files that
+    only have rail performance test data.
     """
     vp_files = []
     vp_dir = LocalFileLocaions.vehicle_positions.get_s3_path()
@@ -42,12 +45,61 @@ def test_gtfs_rt_to_bus_events() -> None:
 
     assert service_date is not None
 
+    return service_date, vp_files
+
+
+# schema for vehicle position dataframes
+VP_SCHEMA = {
+    "route_id": pl.String,
+    "direction_id": pl.Int8,
+    "trip_id": pl.String,
+    "stop_id": pl.String,
+    "stop_sequence": pl.String,
+    "start_time": pl.String,
+    "service_date": pl.String,
+    "vehicle_id": pl.String,
+    "vehicle_label": pl.String,
+    "current_status": pl.String,
+    "vehicle_timestamp": pl.Datetime,
+}
+
+# schema for vehicle events dataframes
+VE_SCHEMA = {
+    "route_id": pl.String,
+    "direction_id": pl.Int8,
+    "trip_id": pl.String,
+    "stop_id": pl.String,
+    "stop_sequence": pl.String,
+    "start_time": pl.String,
+    "service_date": pl.String,
+    "vehicle_id": pl.String,
+    "vehicle_label": pl.String,
+    "current_status": pl.String,
+    "arrival_gtfs": pl.Datetime,
+    "travel_towards_gtfs": pl.Datetime,
+}
+
+
+def test_gtfs_rt_to_bus_events() -> None:
+    """
+    generate vehicle event dataframes from gtfs realtime vehicle position files.
+    inspect them to ensure they
+        * are not empty
+        * have records matching known properties
+
+    generate an empty vechicle event frame and ensure it has the correct columns.
+    """
+    service_date, vp_files = get_service_date_and_files()
+
     # get the bus vehicle events for these files / service date
     bus_vehicle_events = generate_gtfs_rt_events(
         service_date=service_date, gtfs_rt_files=vp_files
     )
 
     assert not bus_vehicle_events.is_empty()
+
+    for col, data_type in bus_vehicle_events.schema.items():
+        assert VE_SCHEMA[col] == data_type
 
     # the following properties are known to be in the consumed dataset
     y1808_events = bus_vehicle_events.filter(
@@ -120,6 +172,246 @@ def test_gtfs_rt_to_bus_events() -> None:
 
     assert empty_bus_vehicle_events.is_empty()
 
-    assert set(bus_vehicle_events.columns) == set(
-        empty_bus_vehicle_events.columns
+    for col, data_type in empty_bus_vehicle_events.schema.items():
+        assert VE_SCHEMA[col] == data_type
+
+    pl.concat([bus_vehicle_events, empty_bus_vehicle_events])
+
+
+def test_read_vehicle_positions() -> None:
+    """
+    test that vehicle positions can be read from files and return a df with the
+    correct schema.
+    """
+    service_date, vp_files = get_service_date_and_files()
+
+    vehicle_positions = read_vehicle_positions(
+        service_date=service_date, gtfs_rt_files=vp_files
+    )
+
+    for col, data_type in vehicle_positions.schema.items():
+        assert VP_SCHEMA[col] == data_type
+
+
+def route_one() -> pl.DataFrame:
+    """
+    straight forward vehicle positions dataframe
+    """
+    data = {
+        "route_id": ["1", "1", "1", "1"],
+        "direction_id": ["0", "0", "0", "0"],
+        "trip_id": ["101", "101", "101", "101"],
+        "stop_id": ["1", "1", "2", "2"],
+        "stop_sequence": ["1", "1", "2", "2"],
+        "start_time": ["08:45:00", "08:45:00", "08:45:00", "08:45:00"],
+        "service_date": ["20240601", "20240601", "20240601", "20240601"],
+        "vehicle_id": ["y1001", "y1001", "y1001", "y1001"],
+        "vehicle_label": ["1001", "1001", "1001", "1001"],
+        "current_status": [
+            "IN_TRANSIT_TO",
+            "STOPPED_AT",
+            "IN_TRANSIT_TO",
+            "STOPPED_AT",
+        ],
+        "vehicle_timestamp": [
+            datetime(year=2024, month=6, day=1, hour=8, minute=45),
+            datetime(year=2024, month=6, day=1, hour=8, minute=47),
+            datetime(year=2024, month=6, day=1, hour=8, minute=55),
+            datetime(year=2024, month=6, day=1, hour=8, minute=57),
+        ],
+    }
+
+    return pl.DataFrame(data, schema=VP_SCHEMA)
+
+
+def route_two() -> pl.DataFrame:
+    """
+    multiple in transit to and stopped positions per stop
+    """
+    data = {
+        "route_id": ["2", "2", "2", "2", "2", "2"],
+        "direction_id": ["0", "0", "0", "0", "0", "0"],
+        "trip_id": ["202", "202", "202", "202", "202", "202"],
+        "stop_id": ["1", "1", "1", "2", "2", "2"],
+        "stop_sequence": ["1", "1", "1", "2", "2", "2"],
+        "start_time": [
+            "09:45:00",
+            "09:45:00",
+            "09:45:00",
+            "09:45:00",
+            "09:45:00",
+            "09:45:00",
+        ],
+        "service_date": [
+            "20240601",
+            "20240601",
+            "20240601",
+            "20240601",
+            "20240601",
+            "20240601",
+        ],
+        "vehicle_id": ["y2002", "y2002", "y2002", "y2002", "y2002", "y2002"],
+        "vehicle_label": ["2002", "2002", "2002", "2002", "2002", "2002"],
+        "current_status": [
+            "IN_TRANSIT_TO",
+            "IN_TRANSIT_TO",
+            "STOPPED_AT",
+            "IN_TRANSIT_TO",
+            "STOPPED_AT",
+            "STOPPED_AT",
+        ],
+        "vehicle_timestamp": [
+            datetime(year=2024, month=6, day=1, hour=9, minute=45),
+            datetime(year=2024, month=6, day=1, hour=9, minute=46),
+            datetime(year=2024, month=6, day=1, hour=9, minute=47),
+            datetime(year=2024, month=6, day=1, hour=9, minute=55),
+            datetime(year=2024, month=6, day=1, hour=9, minute=57),
+            datetime(year=2024, month=6, day=1, hour=9, minute=59),
+        ],
+    }
+
+    return pl.DataFrame(data, schema=VP_SCHEMA)
+
+
+def route_three() -> pl.DataFrame:
+    """
+    No In Transit To data, only stopped at
+    """
+    data = {
+        "route_id": ["3", "3", "3", "3", "3", "3"],
+        "direction_id": ["0", "0", "0", "0", "0", "0"],
+        "trip_id": ["303", "303", "303", "303", "303", "303"],
+        "stop_id": ["1", "1", "1", "2", "2", "2"],
+        "stop_sequence": ["1", "1", "1", "2", "2", "2"],
+        "start_time": [
+            "10:45:00",
+            "10:45:00",
+            "10:45:00",
+            "10:45:00",
+            "10:45:00",
+            "10:45:00",
+        ],
+        "service_date": [
+            "20240601",
+            "20240601",
+            "20240601",
+            "20240601",
+            "20240601",
+            "20240601",
+        ],
+        "vehicle_id": ["y3003", "y3003", "y3003", "y3003", "y3003", "y3003"],
+        "vehicle_label": ["3003", "3003", "3003", "3003", "3003", "3003"],
+        "current_status": [
+            "STOPPED_AT",
+            "STOPPED_AT",
+            "STOPPED_AT",
+            "STOPPED_AT",
+            "STOPPED_AT",
+            "STOPPED_AT",
+        ],
+        "vehicle_timestamp": [
+            datetime(year=2024, month=6, day=1, hour=10, minute=45),
+            datetime(year=2024, month=6, day=1, hour=10, minute=46),
+            datetime(year=2024, month=6, day=1, hour=10, minute=47),
+            datetime(year=2024, month=6, day=1, hour=10, minute=55),
+            datetime(year=2024, month=6, day=1, hour=10, minute=57),
+            datetime(year=2024, month=6, day=1, hour=10, minute=59),
+        ],
+    }
+
+    return pl.DataFrame(data, schema=VP_SCHEMA)
+
+
+def route_four() -> pl.DataFrame:
+    """
+    No Sopped At data, only In Transit To
+    """
+    data = {
+        "route_id": ["4", "4", "4", "4", "4", "4"],
+        "direction_id": ["0", "0", "0", "0", "0", "0"],
+        "trip_id": ["404", "404", "404", "404", "404", "404"],
+        "stop_id": ["1", "1", "1", "2", "2", "2"],
+        "stop_sequence": ["1", "1", "1", "2", "2", "2"],
+        "start_time": [
+            "11:45:00",
+            "11:45:00",
+            "11:45:00",
+            "11:45:00",
+            "11:45:00",
+            "11:45:00",
+        ],
+        "service_date": [
+            "20240601",
+            "20240601",
+            "20240601",
+            "20240601",
+            "20240601",
+            "20240601",
+        ],
+        "vehicle_id": ["y4004", "y4004", "y4004", "y4004", "y4004", "y4004"],
+        "vehicle_label": ["4004", "4004", "4004", "4004", "4004", "4004"],
+        "current_status": [
+            "IN_TRANSIT_TO",
+            "IN_TRANSIT_TO",
+            "IN_TRANSIT_TO",
+            "IN_TRANSIT_TO",
+            "IN_TRANSIT_TO",
+            "IN_TRANSIT_TO",
+        ],
+        "vehicle_timestamp": [
+            datetime(year=2024, month=6, day=1, hour=11, minute=45),
+            datetime(year=2024, month=6, day=1, hour=11, minute=46),
+            datetime(year=2024, month=6, day=1, hour=11, minute=47),
+            datetime(year=2024, month=6, day=1, hour=11, minute=55),
+            datetime(year=2024, month=6, day=1, hour=11, minute=57),
+            datetime(year=2024, month=6, day=1, hour=11, minute=59),
+        ],
+    }
+
+    return pl.DataFrame(data, schema=VP_SCHEMA)
+
+
+def test_positions_to_events() -> None:
+    """
+    test that vehicle positions can be converted to vehicle events
+    correctly for different edge cases.
+    """
+    route_one_positions = route_one()
+    route_one_events = positions_to_events(
+        vehicle_positions=route_one_positions
+    )
+    assert len(route_one_events) == 2
+
+    route_two_positions = route_two()
+    route_two_events = positions_to_events(
+        vehicle_positions=route_two_positions
+    )
+    assert len(route_two_events) == 2
+
+    route_three_positions = route_three()
+    route_three_events = positions_to_events(
+        vehicle_positions=route_three_positions
+    )
+    assert len(route_three_events) == 2
+
+    route_four_positions = route_four()
+    route_four_events = positions_to_events(
+        vehicle_positions=route_four_positions
+    )
+    assert len(route_four_events) == 2
+
+    empty_positions = pl.DataFrame(schema=VP_SCHEMA)
+    empty_events = positions_to_events(vehicle_positions=empty_positions)
+    assert len(empty_events) == 0
+
+    # confirm that vehicle events have the same ordered schema and can be
+    # concatted.
+    pl.concat(
+        [
+            route_one_events,
+            route_two_events,
+            route_three_events,
+            route_four_events,
+            empty_events,
+        ]
     )
