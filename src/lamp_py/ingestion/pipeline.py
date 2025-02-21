916e62cf32df2bc8eef81ecb0fb2ede58f7a0ce6
Henry Huang <hhuang@mbta.com>
2025-02-21 06:34:11 -0500

    initialize and pass through config from pipeline.py

--- src/lamp_py/ingestion/pipeline.py
+++ src/lamp_py/ingestion/pipeline.py
@@ -4,6 +4,7 @@ import os
 import time
 import logging
 import signal
+from typing import Dict
 
 from lamp_py.aws.ecs import handle_ecs_sigterm, check_for_sigterm
 from lamp_py.aws.kinesis import KinesisReader
@@ -15,12 +16,13 @@ from lamp_py.runtime_utils.process_logger import ProcessLogger
 from lamp_py.ingestion.ingest_gtfs import ingest_gtfs
 from lamp_py.ingestion.glides import ingest_glides_events
 from lamp_py.ingestion.light_rail_gps import ingest_light_rail_gps
+from lamp_py.runtime_utils.remote_files import LAMP, S3_INCOMING
 
 logging.getLogger().setLevel("INFO")
 DESCRIPTION = """Entry Point For GTFS Ingestion Scripts"""
 
 
-def main() -> None:
+def main(config: Dict) -> None:
     """
     run the ingestion pipeline
 
@@ -45,7 +47,7 @@ def main() -> None:
 
         check_for_sigterm(metadata_queue, rds_process)
         ingest_light_rail_gps()
-        ingest_gtfs(metadata_queue)
+        ingest_gtfs(metadata_queue, config)
         ingest_glides_events(glides_reader, metadata_queue)
         check_for_sigterm(metadata_queue, rds_process)
 
@@ -54,7 +56,7 @@ def main() -> None:
         time.sleep(30)
 
 
-def start() -> None:
+def start( config: Dict ) -> None:
     """configure and start the ingestion process"""
     # setup handling shutdown commands
     signal.signal(signal.SIGTERM, handle_ecs_sigterm)
@@ -78,8 +80,14 @@ def start() -> None:
     alembic_upgrade_to_head(db_name=os.environ["ALEMBIC_MD_DB_NAME"])
 
     # run the main method
-    main()
+    main(config)
 
 
 if __name__ == "__main__":
-    start()
+    CONFIG_FILE_LIST_FROM_S3 = {"bucket_name" : S3_INCOMING, 
+                            "file_prefix" : LAMP, 
+                            "max_list_size" : 250_000, 
+                            "in_filter": None,
+                            "multiprocessing": True
+                            }
+    start(CONFIG_FILE_LIST_FROM_S3)
