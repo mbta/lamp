64b87839806ea7cf77814ed5f9a8b48d893b35bb
Henry Huang <hhuang@mbta.com>
2025-03-07 17:37:46 -0500

    fix test and add slimmed down test data parquet (100 records)

--- tests/bus_performance_manager/test_bus_convert_for_tableau.py
+++ tests/bus_performance_manager/test_bus_convert_for_tableau.py
@@ -7,8 +7,10 @@ from lamp_py.tableau.conversions.convert_bus_performance_data import apply_bus_a
 
 # poetry run pytest -s tests/bus_performance_manager/test_bus_convert_for_tableau.py
 def test_apply_bus_analysis_conversions() -> None:
-    df = pl.read_parquet("tests/test_files/PUBLIC_ARCHIVE/lamp/bus_vehicle_events/20250306.parquet")
+    """
+    Test extracted conversions for tableau user view
+    """
+    df = pl.read_parquet("tests/test_files/PUBLIC_ARCHIVE/lamp/bus_vehicle_events/test_events.parquet")
     table = apply_bus_analysis_conversions(polars_df=df)
     print(df)
     print(table)
-    breakpoint()
