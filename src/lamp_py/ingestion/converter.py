3b6b1eb23dd362a1be4fbf3726b232752ced6496
Henry Huang <hhuang@mbta.com>
2025-03-18 16:44:08 -0400

    mypy annotate type of multiprocess arg

--- src/lamp_py/ingestion/converter.py
+++ src/lamp_py/ingestion/converter.py
@@ -113,7 +113,7 @@ class Converter(ABC):
         self.metadata_queue.put(written_file)
 
     @abstractmethod
-    def convert(self) -> None:
+    def convert(self, multiprocess: bool =True) -> None:
         """
         convert files to pyarrow tables, write them to s3 as parquete, and move
         files from incoming to archive (or error)
