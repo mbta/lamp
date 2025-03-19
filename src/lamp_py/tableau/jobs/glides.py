ed71a074bdbb22d96f2d2ab3337bdffc14f40bc7
Henry Huang <hhuang@mbta.com>
2025-03-19 15:53:00 -0400

    black

--- src/lamp_py/tableau/jobs/glides.py
+++ src/lamp_py/tableau/jobs/glides.py
@@ -89,6 +89,7 @@ glides_operator_signed_in_schema = pyarrow.schema(
     ]
 )
 
+
 def create_trips_updated_glides_parquet(job: HyperJob, num_files: Optional[int]) -> None:
     """
     Grab the glides_events files and process
@@ -116,7 +117,6 @@ def create_trips_updated_glides_parquet(job: HyperJob, num_files: Optional[int])
                 .dt.convert_time_zone(time_zone="US/Eastern")
                 .dt.replace_time_zone(None),
                 pl.col("time").dt.convert_time_zone(time_zone="US/Eastern").dt.replace_time_zone(None),
-
                 # all of these service dates and start/end times are already EST - don't do any conversions
                 pl.col("data.tripUpdates.previousTripKey.serviceDate").str.to_date("%Y-%m-%d", strict=False),
                 pl.col("data.tripUpdates.previousTripKey.startTime").str.to_time("%H:%M:%S", strict=False),
@@ -128,7 +128,6 @@ def create_trips_updated_glides_parquet(job: HyperJob, num_files: Optional[int])
                 pl.col("data.tripUpdates.endTime").str.to_time("%H:%M:%S", strict=False),
             )
 
-        polars_df['data.tripUpdates.tripKey.startTime']
         writer.write_table(polars_df.to_arrow())
 
 
@@ -157,15 +156,15 @@ def create_operator_signed_in_glides_parquet(job: HyperJob, num_files: Optional[
                 raise TypeError(f"Expected a Polars DataFrame or Series, but got {type(polars_df)}")
             polars_df = polars_df.with_columns(
                 pl.col("data.metadata.inputTimestamp")
-                .str.strptime(pl.Datetime(time_unit='ms'), "%Y-%m-%dT%H:%M:%SZ", strict=False)
+                .str.strptime(pl.Datetime(time_unit="ms"), "%Y-%m-%dT%H:%M:%SZ", strict=False)
                 .dt.convert_time_zone(time_zone="US/Eastern")
                 .dt.replace_time_zone(None),
                 pl.col("data.signedInAt")
-                .str.strptime(pl.Datetime(time_unit='ms'), "%Y-%m-%dT%H:%M:%SZ", strict=False)
+                .str.strptime(pl.Datetime(time_unit="ms"), "%Y-%m-%dT%H:%M:%SZ", strict=False)
                 .dt.convert_time_zone(time_zone="US/Eastern")
                 .dt.replace_time_zone(None),
                 pl.col("time").dt.convert_time_zone(time_zone="US/Eastern").dt.replace_time_zone(None),
-                )
+            )
 
         writer.write_table(polars_df.to_arrow())
 
@@ -180,7 +179,6 @@ class HyperGlidesTripUpdates(HyperJob):
             remote_parquet_path=tableau_glides_all_trips_updated.s3_uri,
             lamp_version=tableau_glides_all_trips_updated.version,
         )
-
 
     @property
     def parquet_schema(self) -> pyarrow.schema:
@@ -215,8 +213,6 @@ class HyperGlidesOperatorSignIns(HyperJob):
             remote_parquet_path=tableau_glides_all_operator_signed_in.s3_uri,
             lamp_version=tableau_glides_all_operator_signed_in.version,
         )
-
-    
 
     @property
     def parquet_schema(self) -> pyarrow.schema:
