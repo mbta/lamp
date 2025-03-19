6f752d3455337f1ae5ecf44ce18ee50ca7105f4d
Henry Huang <hhuang@mbta.com>
2025-03-19 15:54:35 -0400

    split out parquet converting from hyper required tasks - add test for this unit

---
+++ tests/tableau/test_parquet_converter.py
@@ -0,0 +1,21 @@
+from typing import List
+import pytest
+
+from lamp_py.tableau.conversions.converter import Converter
+from lamp_py.tableau.jobs.glides import HyperGlidesOperatorSignIns, HyperGlidesTripUpdates
+
+
+def start_glides_parquet_updates() -> None:
+    """Run all Glides Parquet Update jobs"""
+
+    parquet_update_jobs: List[Converter] = [
+        HyperGlidesTripUpdates(),
+        HyperGlidesOperatorSignIns(),
+    ]
+
+    for job in parquet_update_jobs:
+        job.run_parquet(None)
+
+@pytest.mark.tableau
+def test_glides_parquet_updates():
+    start_glides_parquet_updates()
