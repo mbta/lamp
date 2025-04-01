d9d686a121d16618a42880355f07501b259a8bcb
Henry Huang <hhuang@mbta.com>
2025-04-01 17:14:13 -0400

    update to process all files from march 10-march-19 as a stress test to simulate backlog processing

--- src/lamp_py/ingestion/ingest_gtfs.py
+++ src/lamp_py/ingestion/ingest_gtfs.py
@@ -77,8 +77,9 @@ def ingest_s3_files(metadata_queue: Queue[Optional[str]]) -> None:
     try:
         files = file_list_from_s3(
             bucket_name=S3_INCOMING,
-            file_prefix=LAMP,
+            file_prefix="lamp/delta/2025/03/1",
         )
+
 
         grouped_files = group_sort_file_list(files)
         
