626c4efa359935b4ef6e7c182bb34a629208381c
Henry Huang <hhuang@mbta.com>
2025-04-16 09:22:13 -0400

    add test data for rt_trips_for_backup_match

---
+++ runners/run_static_trips_subquery.py
@@ -0,0 +1,7 @@
+
+from lamp_py.performance_manager.l1_cte_statements import static_trips_subquery_pq
+
+
+static_trips_sub_res = static_trips_subquery_pq(20250415).sort(by="static_trip_id")
+
+static_trips_sub_res.write_csv('20250415_static_trips_subquery.csv')
