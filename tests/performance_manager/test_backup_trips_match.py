9c5261da3560ae6e8bc73f714892565a49abefe2
Henry Huang <hhuang@mbta.com>
2025-04-16 15:39:13 -0400

    clean up/disable tests that wont work

--- tests/performance_manager/test_backup_trips_match.py
+++ tests/performance_manager/test_backup_trips_match.py
@@ -18,10 +18,7 @@ def test_backup_trips_match() -> None:
     )
     # static_trips = pl.read_csv('tests/test_files/replace_perf_mgr_query_test_data/20250415_static_trips_subquery.csv', infer_schema=False)
     rt_trips = rt_trips_raw.with_columns(
-        pl.when(pl.col("direction_id") == "f")
-        .then(pl.lit(False))
-        .otherwise(pl.lit(True))
-        .alias("direction_id"),
+        pl.when(pl.col("direction_id") == "f").then(pl.lit(False)).otherwise(pl.lit(True)).alias("direction_id"),
         pl.col("start_time").cast(pl.Int64).alias("start_time"),
     )
     # rt_trips = rt_trips.with_columns(
@@ -43,10 +40,10 @@ def test_backup_trips_match() -> None:
     #                             (((pl.col("start_time_int").mod(3600)/60).mod(60)).floor().cast(pl.Int64).cast(pl.String).alias("ss"))
 
     static_trips = static_trips_subquery_pl(20250415)
-    breakpoint()
+    # breakpoint()
     backup_matched_trips = backup_trips_match_pl(rt_trips, static_trips)
 
-    breakpoint()
+    # breakpoint()
     print(backup_matched_trips)
     # is it going to be strings IRL? What is the datatype of this stuff when it comes back
 
