d5b089e782e013bce1a79f9973450a17fa61efe1
Henry Huang <hhuang@mbta.com>
2025-04-17 00:26:35 -0400

    move the cast to the right statement, clean up test a whole lot now that we are smarter. @pytest ignore for now instead of comments

--- tests/performance_manager/test_static_trips_subquery.py
+++ tests/performance_manager/test_static_trips_subquery.py
@@ -1,75 +1,40 @@
-# from lamp_py. l1_cte_statements import (
-#     static_trips_subquery,
-# )
-# import polars as pl
-# from polars.testing import assert_frame_equal, assert_series_equal
-# from lamp_py.performance_manager.l1_cte_statements import static_trips_subquery_pl
+import pytest
+import polars as pl
+from polars.testing import assert_frame_equal
+from lamp_py.performance_manager.l1_cte_statements import static_trips_subquery_pl
 
 # look at bus performance to get a monkeypatch/thing to read from bucket from ci/cd
-# def test_static_trips_subquery_pl() -> None:
-#     """
-#     Passing unit test for static_trips_subquery implementation in polars/parquet
-#     """
-#     # ┌─────────────────────────┬──────────────┬───────────────────┬───────────────────┬────────────────┐
-#     # │ static_trip_id          ┆ direction_id ┆ static_stop_count ┆ static_start_time ┆ route_id       │
-#     # │ ---                     ┆ ---          ┆ ---               ┆ ---               ┆ ---            │
-#     # │ str                     ┆ bool         ┆ u32               ┆ i64               ┆ str            │
-#     # ╞═════════════════════════╪══════════════╪═══════════════════╪═══════════════════╪════════════════╡
-#     static_trips_sub_res = static_trips_subquery_pl(20250410).sort(by="static_trip_id")
+@pytest.mark.skip(reason="Temporarily disabled: PR #500")
+def test_static_trips_subquery_pl() -> None:
+    """
+    Passing unit test for static_trips_subquery implementation in polars/parquet
+    """
+    # ┌─────────────────────────┬──────────────┬───────────────────┬───────────────────┬────────────────┐
+    # │ static_trip_id          ┆ direction_id ┆ static_stop_count ┆ static_start_time ┆ route_id       │
+    # │ ---                     ┆ ---          ┆ ---               ┆ ---               ┆ ---            │
+    # │ str                     ┆ bool         ┆ u32               ┆ i64               ┆ str            │
+    # ╞═════════════════════════╪══════════════╪═══════════════════╪═══════════════════╪════════════════╡
+    static_trips_pl = static_trips_subquery_pl(20250410).sort(by="static_trip_id")
 
-#     # ┌─────────────────────────┬────────────────┬──────────────┬───────────────────┬───────────────────┐
-#     # │ static_trip_id          ┆ route_id       ┆ direction_id ┆ static_start_time ┆ static_stop_count │
-#     # │ ---                     ┆ ---            ┆ ---          ┆ ---               ┆ ---               │
-#     # │ str                     ┆ str            ┆ str          ┆ str               ┆ str               │
-#     # ╞═════════════════════════╪════════════════╪══════════════╪═══════════════════╪═══════════════════╡
+    # ┌─────────────────────────┬────────────────┬──────────────┬───────────────────┬───────────────────┐
+    # │ static_trip_id          ┆ route_id       ┆ direction_id ┆ static_start_time ┆ static_stop_count │
+    # │ ---                     ┆ ---            ┆ ---          ┆ ---               ┆ ---               │
+    # │ str                     ┆ str            ┆ str          ┆ str               ┆ str               │
+    # ╞═════════════════════════╪════════════════╪══════════════╪═══════════════════╪═══════════════════╡
 
-#     compare_sql = pl.read_csv(
-#         "tests/test_files/replace_perf_mgr_query_test_data/staging_test_summary_sub.csv", infer_schema=False
-#     )
+    compare_sql = pl.read_csv(
+        "tests/test_files/replace_perf_mgr_query_test_data/staging_test_summary_sub.csv", infer_schema=False
+    )
 
-#     # line up the types - convert the 24+ hour timestamp to math-able values
-#     static_trips_pl = static_trips_sub_res.with_columns(
-#         pl.col("static_start_time")
-#         .str.splitn(":", 3)
-#         .struct.rename_fields(["hour", "minute", "second"])
-#         .alias("fields")
-#     ).unnest("fields")
-#     breakpoint()
+    # need to do a few things because the csv output doesn't do types well
+    static_trips_sql = compare_sql.with_columns(
+        pl.col("static_stop_count").cast(pl.UInt32),
+        pl.col("static_start_time").cast(pl.Int32),
+        pl.when(pl.col("direction_id") == "f")
+        .then(pl.lit(False))
+        .otherwise(pl.lit(True))
+        .alias("direction_id")
+    )
 
-#     static_trips_sql = compare_sql.with_columns(
-#         pl.col("static_stop_count").cast(pl.UInt32),
-#         pl.col("static_start_time").cast(pl.Int32),
-#         pl.when(pl.col("direction_id") == "f")
-#         .then(pl.lit(False))
-#         .otherwise(pl.lit(True))
-#         .alias("direction_id")
-#     )
-
-#     breakpoint()
-#     # assert against test csv for all rows
-#     # all the easy ones
-#     assert_frame_equal(
-#         static_trips_pl.select(["static_trip_id", "direction_id", "static_stop_count", "route_id"]),
-#         static_trips_sql.select(["static_trip_id", "direction_id", "static_stop_count", "route_id"]),
-#     )
-
-#     # now the timestamp...
-#     tmp = static_trips_pl.select(["hour", "minute", "second"]).cast(pl.Int32)
-#     assert_series_equal(
-#         tmp["hour"] * 60 * 60 + tmp["minute"] * 60 + tmp["second"],
-#         static_trips_sql["static_start_time"],
-#         check_names=False,
-#     )
-
-
-# # def insert_dataframe(self, dataframe: pandas.DataFrame, insert_table: Any) -> None:
-# # """
-# # insert data into db table from pandas dataframe
-# # """
-# # insert_as = self._get_schema_table(insert_table)
-
-# # with self.session.begin() as cursor:
-# #     cursor.execute(
-# #         sa.insert(insert_as),
-# #         dataframe.to_dict(orient="records"),
-# #     )
+    # assert against test csv for all rows
+    assert_frame_equal(static_trips_pl, static_trips_sql, check_column_order=False)
