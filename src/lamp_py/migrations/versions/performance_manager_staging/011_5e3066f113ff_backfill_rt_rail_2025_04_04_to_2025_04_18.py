4b27612346aafcf6641c0159f6f01040d866cfe2
Henry Huang <hhuang@mbta.com>
2025-04-24 10:38:05 -0400

    move md query back to main query to eliminate race condition. wrap in try catch so it passes ci/cd

--- src/lamp_py/migrations/versions/performance_manager_staging/011_5e3066f113ff_backfill_rt_rail_2025_04_04_to_2025_04_18.py
+++ src/lamp_py/migrations/versions/performance_manager_staging/011_5e3066f113ff_backfill_rt_rail_2025_04_04_to_2025_04_18.py
@@ -13,6 +13,7 @@ This will also clean up duplication of data in prod from 4/17-4/22
 * downgrade -> Nothing
 """
 
+import logging
 import os
 import tempfile
 import polars as pl
@@ -22,6 +23,7 @@ from typing import List
 
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.exc import ProgrammingError
 
 from lamp_py.aws.s3 import download_file, upload_file
 from lamp_py.postgres.postgres_utils import DatabaseIndex, DatabaseManager
@@ -43,6 +45,43 @@ def upgrade() -> None:
     # ~ (75788 rows)
     clear_trips = "DELETE FROM vehicle_trips WHERE service_date >= 20250404 AND service_date <= 20250422;"
     op.execute(clear_trips)
+
+    # Query to Check
+    # SELECT created_on, rail_pm_processed, rail_pm_process_fail
+    # FROM public.metadata_log
+    # WHERE created_on > '2025-04-04' and created_on < '2025-04-22 23:59:59'
+    # AND (path LIKE '%/RT_TRIP_UPDATES/%' or path LIKE '%/RT_VEHICLE_POSITIONS/%')
+    # ORDER BY created_on;
+
+    try:
+        update_md_query = """
+        UPDATE
+            metadata_log
+        SET
+            rail_pm_process_fail = false
+            , rail_pm_processed = false
+        WHERE
+            created_on > '2025-04-04 00:00:00'
+            and created_on < '2024-04-22 23:59:59'
+            and (
+                path LIKE '%/RT_TRIP_UPDATES/%'
+                or path LIKE '%/RT_VEHICLE_POSITION/%'
+            )
+        ;
+        """
+        md_manager = DatabaseManager(DatabaseIndex.METADATA)
+        md_manager.execute(sa.text(update_md_query))
+
+    except ProgrammingError as error:
+        # Error 42P01 is an 'Undefined Table' error. This occurs when there is
+        # no metadata_log table in the rail performance manager database
+        #
+        # Raise all other sql errors
+        original_error = error.orig
+        if original_error is not None and hasattr(original_error, "pgcode") and original_error.pgcode == "42P01":
+            logging.info("No Metadata Table in Rail Performance Manager")
+        else:
+            raise
 
 
 def downgrade() -> None:
