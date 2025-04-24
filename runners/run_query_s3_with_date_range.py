de23cdef1532739432e92cd11e2d5ec6a9a4b111
Henry Huang <hhuang@mbta.com>
2025-04-24 16:28:38 -0400

    add a runner for the range s3 query - useful bc S3_SPRINGBOARD var doesn't get mocked out

---
+++ runners/run_query_s3_with_date_range.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+
+from lamp_py.aws.s3 import file_list_from_s3_date_range
+from lamp_py.runtime_utils.remote_files import LAMP, S3_SPRINGBOARD
+from lamp_py.runtime_utils.remote_files import rt_vehicle_positions
+
+template = "year={yy}/month={mm}/day={dd}/"
+end_date = datetime.now()
+end_date = datetime(year=2025, month=3, day=30)
+start_date = end_date - timedelta(days=15)  # type: ignore
+
+breakpoint()
+s3_uris = file_list_from_s3_date_range(
+    bucket_name=S3_SPRINGBOARD,
+    file_prefix=rt_vehicle_positions.prefix,
+    path_template=template,
+    end_date=end_date,
+    start_date=start_date,
+)
+
+print(s3_uris)
