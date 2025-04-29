03eed71a71755abd7d36e9b67cb3ef1379a8ad96
Henry Huang <hhuang@mbta.com>
2025-04-29 01:40:08 -0400

    add heavy rail and individual lines to parquet level filters

--- src/lamp_py/utils/filter_bank.py
+++ src/lamp_py/utils/filter_bank.py
@@ -1,8 +1,6 @@
 import pyarrow.compute as pc
 import pyarrow as pa
 
-lrtp_terminals = pa.array([70110, 70162, 70236, 70274, 70502, 70510])
-
 
 class FilterBankRtVehiclePositions:
     """
@@ -20,8 +18,24 @@ class FilterBankRtVehiclePositions:
         green_e = pc.field("vehicle.trip.route_id") == "Green-E"
         mattapan = pc.field("vehicle.trip.route_id") == "Mattapan"
 
+        green = pc.is_in(pc.field("vehicle.trip.route_id"), pa.array(["Green-B", "Green-C", "Green-D", "Green-E"]))
+        orange = pc.field("vehicle.trip.route_id") == "Orange"
+        blue = pc.field("vehicle.trip.route_id") == "Blue"
+        red = pc.field("vehicle.trip.route_id") == "Red"
+
+        # don't filter with these IDs - do this in Polars.
+        # this is significantly slower
+        light_rail_terminal_stop_ids = pa.array(list(map(str, [70110, 70162, 70236, 70274, 70502, 70510])))
+        heavy_rail_terminal_stop_ids = pa.array(list(map(str, [70003, 70034, 70040, 70057, 70063, 70092, 70104])))
+        light_rail_terminal_by_stop_id = pc.is_in(
+            pc.field("vehicle.stop_id"), light_rail_terminal_stop_ids
+        )
+        heavy_rail_terminal_by_stop_id = pc.is_in(
+            pc.field("vehicle.stop_id"), heavy_rail_terminal_stop_ids
+        )
+
         light_rail = green_b | green_c | green_d | green_e | mattapan
-        green = green_b | green_c | green_d | green_e
+        heavy_rail = orange | red | blue
 
 
 class FilterBankRtTripUpdates:
@@ -40,5 +54,25 @@ class FilterBankRtTripUpdates:
         green_e = pc.field("trip_update.trip.route_id") == "Green-E"
         mattapan = pc.field("trip_update.trip.route_id") == "Mattapan"
 
-        light_rail = green_b | green_c | green_d | green_e | mattapan
-        green = green_b | green_c | green_d | green_e
+        green = pc.is_in(pc.field("trip_update.trip.route_id"), pa.array(["Green-B", "Green-C", "Green-D", "Green-E"]))
+        orange = pc.field("trip_update.trip.route_id") == "Orange"
+        blue = pc.field("trip_update.trip.route_id") == "Blue"
+        red = pc.field("trip_update.trip.route_id") == "Red"
+
+
+        # don't filter with these IDs - do this in Polars.
+        # this is significantly slower
+        light_rail_terminal_stop_ids = pa.array(
+            list(map(str, [70106, 70160, 70161, 70238, 70276, 70503, 70504, 70511, 70512]))
+        )
+        heavy_rail_terminal_stop_ids = pa.array(list(map(str, [70001, 70036, 70038, 70059, 70061, 70094, 70105])))
+
+        light_rail_terminal_by_stop_id = pc.is_in(
+            pc.field("trip_update.stop_time_update.stop_id"), light_rail_terminal_stop_ids
+        )
+        heavy_rail_terminal_by_stop_id = pc.is_in(
+            pc.field("trip_update.stop_time_update.stop_id"), heavy_rail_terminal_stop_ids
+        )
+
+        light_rail = green | mattapan
+        heavy_rail = orange | red | blue
