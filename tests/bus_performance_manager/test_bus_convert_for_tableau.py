8fc31d71d625316cf291fc5e0b6aff5156e9e501
Henry Huang <hhuang@mbta.com>
2025-05-27 15:20:50 -0400

    module construction requires s3 - mock this away in all tests in tree

--- tests/bus_performance_manager/test_bus_convert_for_tableau.py
+++ tests/bus_performance_manager/test_bus_convert_for_tableau.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
+from unittest.mock import patch
 import polars as pl
 
 from lamp_py.tableau.conversions.convert_bus_performance_data import apply_bus_analysis_conversions
 
-
 # poetry run pytest -s tests/bus_performance_manager/test_bus_convert_for_tableau.py
+@patch("lamp_py.utils.filter_bank.GTFS_ARCHIVE", "https://performancedata.mbta.com/lamp/gtfs_archive")
 def test_apply_bus_analysis_conversions() -> None:
     """
     Test extracted conversions for tableau user view
