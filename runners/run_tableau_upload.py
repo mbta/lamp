db041a813408e7b0420bbd943e8cd0747d55cde1
Henry Huang <hhuang@mbta.com>
2025-06-12 10:51:08 -0400

    add runner to upload all hyper to tableau

--- runners/run_tableau_upload.py
+++ runners/run_tableau_upload.py
@@ -5,16 +5,36 @@ from lamp_py.aws.ecs import check_for_parallel_tasks
 from lamp_py.runtime_utils.env_validation import validate_environment
 from lamp_py.tableau.hyper import HyperJob
 from lamp_py.tableau.jobs.bus_performance import HyperBusPerformanceAll, HyperBusPerformanceRecent
-from lamp_py.tableau.pipeline import start_bus_parquet_updates
-from lamp_py.tableau.server import datasource_from_name
+from lamp_py.tableau.jobs.rt_rail import HyperRtRail
+from lamp_py.tableau.jobs.rt_alerts import HyperRtAlerts
+from lamp_py.tableau.jobs.gtfs_rail import (
+    HyperServiceIdByRoute,
+    HyperStaticCalendar,
+    HyperStaticCalendarDates,
+    HyperStaticFeedInfo,
+    HyperStaticRoutes,
+    HyperStaticStops,
+    HyperStaticStopTimes,
+    HyperStaticTrips,
+)
+
+from lamp_py.tableau.jobs.glides import HyperGlidesOperatorSignIns
+from lamp_py.tableau.jobs.glides import HyperGlidesTripUpdates
+from lamp_py.tableau.pipeline import (
+    HyperGtfsRtVehiclePositions,
+    HyperGtfsRtTripUpdates,
+    HyperGtfsRtVehiclePositionsHeavyRail,
+    HyperGtfsRtTripUpdatesHeavyRail,
+    HyperGtfsRtVehiclePositionsAllLightRail,
+    HyperDevGreenGtfsRtVehiclePositions,
+    HyperDevGreenGtfsRtTripUpdates,
+)
 
 
 def start_bus_hyper() -> None:
     """Run all HyperFile Update Jobs"""
     # configure the environment
-    os.environ["SERVICE_NAME"] = "tableau_hyper_update"
-
-    # os.environ["TABLEAU_PROJECT"] = "GTFS-RT"
+    os.environ["SERVICE_NAME"] = "tableau_hyper_update_hh"
 
     validate_environment(
         required_variables=[
@@ -35,12 +55,34 @@ def start_bus_hyper() -> None:
     check_for_parallel_tasks()
 
     hyper_jobs: List[HyperJob] = [
-        # HyperBusPerformanceAll(),
+        HyperGlidesTripUpdates(),
+        HyperGlidesOperatorSignIns(),
+        # HyperRtRail(), # skip this one its big
+        HyperServiceIdByRoute(),
+        HyperStaticCalendar(),
+        HyperStaticCalendarDates(),
+        HyperStaticFeedInfo(),
+        HyperStaticRoutes(),
+        HyperStaticStops(),
+        HyperStaticStopTimes(),
+        HyperStaticTrips(),
+        HyperRtAlerts(),
+        # HyperBusPerformanceAll(), # skip this one it's big
         HyperBusPerformanceRecent(),
+        HyperGtfsRtVehiclePositions,
+        HyperGtfsRtTripUpdates,
+        HyperDevGreenGtfsRtVehiclePositions,
+        HyperDevGreenGtfsRtTripUpdates,
+        HyperGtfsRtVehiclePositionsHeavyRail,
+        HyperGtfsRtTripUpdatesHeavyRail,
+        HyperGtfsRtVehiclePositionsAllLightRail,
     ]
 
     for job in hyper_jobs:
-        outs = job.run_hyper()
+        try:
+            outs = job.run_hyper()
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
