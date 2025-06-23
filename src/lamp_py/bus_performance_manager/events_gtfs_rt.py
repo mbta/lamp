ef15b13e6c778622f0f1917b8bcbb9ed8f5f5145
Henry Huang <hhuang@mbta.com>
2025-06-23 14:22:54 -0400

    make latitude and longitude only come from IN_TRANSIT_TO rows as those seem to better match operational reality. STOPPED_AT needs to be investigated as they seemed to be non-rev points (though the rev column said True for all) or some other issue

--- src/lamp_py/bus_performance_manager/events_gtfs_rt.py
+++ src/lamp_py/bus_performance_manager/events_gtfs_rt.py
@@ -224,14 +224,14 @@ def positions_to_events(vehicle_positions: pl.DataFrame) -> pl.DataFrame:
             on="trip_id",
             how="left",
         )
-        .with_columns(
-            pl.coalesce(["latitude_STOPPED_AT", "latitude_IN_TRANSIT_TO"]).alias("latitude"),
-            pl.coalesce(["longitude_STOPPED_AT", "longitude_IN_TRANSIT_TO"]).alias("longitude"),
-        )
         .rename(
             {
                 "vehicle_timestamp_STOPPED_AT": "gtfs_arrival_dt",
                 "vehicle_timestamp_IN_TRANSIT_TO": "gtfs_travel_to_dt",
+                # only grab the IN_TRANSIT_TO rows because they seem to better
+                # align to actual trips than STOPPED_AT does
+                "latitude_IN_TRANSIT_TO": "latitude",
+                "longitude_IN_TRANSIT_TO": "longitude",
             }
         )
         .with_columns(
