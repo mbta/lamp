37eb98b24cd9cdfd74f8cc9a37e663e2ce9e75b1
Henry Huang <hhuang@mbta.com>
2025-07-01 01:41:01 -0400

    CAN REMOVE - add checkpoints to get checkpoint_id and checkpoint_name to compare with TM

--- src/lamp_py/bus_performance_manager/events_gtfs_schedule.py
+++ src/lamp_py/bus_performance_manager/events_gtfs_schedule.py
@@ -189,6 +189,7 @@ def stop_events_for_date(service_date: date) -> pl.DataFrame:
         "checkpoint_id",
     )
 
+    checkpoints = gtfs_from_parquet("checkpoints", service_date).select("checkpoint_id", "checkpoint_name")
     stop_count = stop_times.group_by("trip_id").len("plan_stop_count")
     trip_start = stop_times.group_by("trip_id").agg(pl.col("arrival_time").min().alias("plan_start_time"))
 
@@ -220,6 +221,11 @@ def stop_events_for_date(service_date: date) -> pl.DataFrame:
         .join(
             trip_start,
             on="trip_id",
+            how="left",
+        )
+        .join(
+            checkpoints,
+            on="checkpoint_id",
             how="left",
         )
         # .join(
