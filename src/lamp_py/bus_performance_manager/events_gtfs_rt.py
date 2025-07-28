24c5ee85a90e652c6d217f09da7d48a6c5ac994c
Henry Huang <hhuang@mbta.com>
2025-07-28 13:15:38 -0400

    filter event_positions on IN_TRANSIT_TO before doing group - adjust groups.

--- src/lamp_py/bus_performance_manager/events_gtfs_rt.py
+++ src/lamp_py/bus_performance_manager/events_gtfs_rt.py
@@ -227,14 +227,16 @@ def positions_to_events(vehicle_positions: pl.DataFrame) -> pl.DataFrame:
     # only grab the IN_TRANSIT_TO rows lat/lon because they seem to better
     # align to actual trips than STOPPED_AT does - caused by
     # vendor - details in linked Asana Ticket/PR #542
-    event_position = vehicle_positions.group_by("trip_id", "stop_sequence", "vehicle_timestamp").agg(
-        pl.col("latitude").min(), pl.col("longitude").min()
+    event_position = (
+        vehicle_positions.filter(pl.col("current_status") == "IN_TRANSIT_TO")
+        .group_by("route_id", "trip_id", "stop_id", "stop_sequence", "vehicle_id")
+        .agg(pl.col("latitude").min(), pl.col("longitude").min())
     )
     vehicle_events = vehicle_events.join(
         event_position,
         how="left",
-        right_on=["trip_id", "stop_sequence", "vehicle_timestamp"],
-        left_on=["trip_id", "stop_sequence", "IN_TRANSIT_TO"],
+        right_on=["route_id", "trip_id", "stop_id", "stop_sequence", "vehicle_id"],
+        left_on=["route_id", "trip_id", "stop_id", "stop_sequence", "vehicle_id"],
         coalesce=True,
         validate="m:1",
     )
