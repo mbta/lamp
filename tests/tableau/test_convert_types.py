02c3d6744ad8b1820a67fc26b47368138cf83c89
Henry Huang <hhuang@mbta.com>
2025-07-30 13:28:04 -0400

    add new get_default_tableau_schema_from_s3 method that grabs a file from s3, extracts the schema, and then renames fields that are invalid for tableau, and returns that schema

--- tests/tableau/test_convert_types.py
+++ tests/tableau/test_convert_types.py
@@ -1,6 +1,16 @@
+import os
 import pyarrow
 import pytest
-from lamp_py.tableau.conversions.convert_types import convert_to_tableau_compatible_schema
+from lamp_py.tableau.conversions.convert_types import (
+    convert_to_tableau_compatible_schema,
+    get_default_tableau_schema_from_s3,
+)
+
+from lamp_py.runtime_utils.remote_files import S3Location, bus_events
+
+import polars as pl
+
+# from ..test_resources import rt_vehicle_positions as s3_vp
 
 
 # simple case
@@ -64,7 +74,7 @@ def test_tableau_auto_schema_exclude(test_schema1: pyarrow.schema) -> None:
         ]
     )
     excludes = ["service_date", "route_id"]
-    assert assert_test_schema1 == convert_to_tableau_compatible_schema(test_schema1, exclude=excludes)
+    assert assert_test_schema1 == convert_to_tableau_compatible_schema(test_schema1, excludes=excludes)
 
 
 def test_tableau_auto_schema_tz(test_schema2: pyarrow.schema) -> None:
@@ -90,5 +100,23 @@ def test_tableau_auto_schema_all_case(test_schema3: pyarrow.schema) -> None:
     overrides = {"service_date": pyarrow.date32()}
     excludes = ["route_id"]
     assert assert_test_schema3 == convert_to_tableau_compatible_schema(
-        test_schema3, overrides=overrides, exclude=excludes
+        test_schema3, overrides=overrides, excludes=excludes
     )
+
+
+def test_schema_default_from_s3() -> None:
+    bus_events = S3Location(bucket="mbta-performance", prefix=os.path.join("lamp", "bus_vehicle_events"), version="1.2")
+    overrides = {"service_date": pyarrow.date32()}
+    input_schema, output_schema = get_default_tableau_schema_from_s3(input_location=bus_events, overrides=overrides)
+    # print(input_schema)`
+    # print(output_schema)`
+    print(list(set(input_schema).difference(output_schema)))
+    # res = [pyarrow.Field('stop_arrival_dt', timestamp(us, tz=UTC)), pyarrow.Field<gtfs_sort_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<stop_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<service_date: large_string>, pyarrow.Field<tm_actual_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<tm_scheduled_time_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_travel_to_dt: timestamp[us, tz=UTC]>, pyarrow.Field<tm_actual_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_departure_dt: timestamp[us, tz=UTC]>]
+    # diff = {pyarrow.Field<tm_actual_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<service_date: large_string>, pyarrow.Field<tm_scheduled_time_dt: timestamp[us, tz=UTC]>, pyarrow.Field<stop_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<stop_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_travel_to_dt: timestamp[us, tz=UTC]>, pyarrow.Field<tm_actual_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_sort_dt: timestamp[us, tz=UTC]>}
+    breakpoint()
+
+
+def test_spare_schema():
+    df = pl.read_parquet("s3://mbta-ctd-dataplatform-staging-springboard/spare/vehicles.parquet")
+
+    convert_to_tableau_compatible_schema(df.to_arrow().schema)
