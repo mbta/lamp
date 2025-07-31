d9cff86b30dd278f0607a745e0635b6968841636
Henry Huang <hhuang@mbta.com>
2025-07-30 23:56:24 -0400

    move flatten to spare jobs to consolidate before the file structure is nailed down

--- src/lamp_py/tableau/jobs/spare_jobs.py
+++ src/lamp_py/tableau/jobs/spare_jobs.py
@@ -1,13 +1,27 @@
+from lamp_py.ingestion.utils import explode_table_column, flatten_table_schema
 from lamp_py.runtime_utils.remote_files_spare import (
     springboard_spare_vehicles,
     tableau_spare_vehicles,
 )
 from lamp_py.tableau.conversions.convert_types import get_default_tableau_schema_from_s3
-from lamp_py.tableau.conversions.spare.convert_spare_vehicle import flatten_spare_vehicle
+
+# from lamp_py.tableau.conversions.spare.convert_spare_vehicle import flatten_spare_vehicle
 from lamp_py.tableau.jobs.filtered_hyper import FilteredHyperJob
 import pyarrow
+import polars as pl
 
-SPARE_TABLEAU_PROJECT = "SPARE"
+
+def flatten_spare_vehicle(table: pyarrow.Table) -> pyarrow.Table:
+    table = table.drop(["metadata.providerId", "metadata.owner", "emissionsRate", "createdAt", "updatedAt"])
+    df = pl.from_arrow(table)
+    accessibilty_rows = flatten_table_schema(
+        explode_table_column(flatten_table_schema(table), "accessibilityFeatures")
+    )  # .drop(['accessibilityFeatures'])
+
+    return pl.concat([df, pl.from_arrow(accessibilty_rows)], how="align").to_arrow().drop("accessibilityFeatures")
+
+
+SPARE_TABLEAU_PROJECT = "GTFS-RT"
 # overrides1 = {"emissionsRate": pyarrow.int64()}
 excludes1 = {"accessibilityFeatures": ""}
 HyperSpareVehicles = FilteredHyperJob(
