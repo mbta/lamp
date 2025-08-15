0d59dc6769416c2de0eb12b24521be15baefee3b
Henry Huang <hhuang@mbta.com>
2025-08-15 10:29:36 -0400

    testing

--- tests/tableau/test_convert_types.py
+++ tests/tableau/test_convert_types.py
@@ -10,61 +10,118 @@ from lamp_py.runtime_utils.remote_files import S3Location, bus_events
 
 import polars as pl
 
-# from ..test_resources import rt_vehicle_positions as s3_vp
+from ..test_resources import rt_vehicle_positions as s3_vp
+
+import logging
+from unittest import mock
+from dataclasses import dataclass
+
+current_dir = os.path.join(os.path.dirname(__file__))
+
+SERVICE_DATE = date(2024, 8, 1)
+
+gtfs = GTFSArchive(bucket="https://performancedata.mbta.com", prefix="lamp/gtfs_archive")
 
 
-# # simple case
-# @pytest.fixture
-# def test_schema1() -> pyarrow.schema:
-#     return pyarrow.schema(
-#         [
-#             ("service_date", pyarrow.string()),
-#             ("route_id", pyarrow.large_string()),
-#             ("exact_plan_trip_match", pyarrow.bool_()),
-#         ]
-#     )
+@dataclass
+class S3Location:
+    """
+    wrapper for a bucket name and prefix pair used to define an s3 location
+    """
+
+    bucket: str
+    prefix: str
+    version: str = "1.0"
+
+    @property
+    def s3_uri(self) -> str:
+        """generate the full s3 uri for the location"""
+        return f"{self.bucket}/{self.prefix}"
 
 
-# # simple case
-# @pytest.fixture
-# def test_schema2() -> pyarrow.schema:
-#     return pyarrow.schema(
-#         [
-#             ("service_date", pyarrow.date32()),
-#             ("route_id", pyarrow.large_string()),
-#             ("exact_plan_trip_match", pyarrow.bool_()),
-#             ("start_dt", pyarrow.timestamp(unit="us", tz="UTC")),
-#         ]
-#     )
+@mock.patch("lamp_py.utils.gtfs_utils.object_exists")
+@mock.patch("lamp_py.utils.gtfs_utils.compressed_gtfs", gtfs)
+@mock.patch("lamp_py.runtime_utils.remote_files.S3Location", S3Location)
 
 
-# # all case
-# @pytest.fixture
-# def test_schema3() -> pyarrow.schema:
-#     return pyarrow.schema(
-#         [
-#             ("service_date", pyarrow.string()),
-#             ("route_id", pyarrow.large_string()),
-#             ("exact_plan_trip_match", pyarrow.bool_()),
-#             ("start_dt", pyarrow.timestamp(unit="us", tz="UTC")),
-#         ]
-#     )
+@pytest.fixture
+def spare_vp() -> pl.DataFrame:
+    return = pl.read_csv(
+        os.path.join(current_dir, "spare_vehicle.csv"),
+        schema=bus_events.schema,
+    ).sort(by=["plan_trip_id", "stop_sequence"])
+
+# simple case
+@pytest.fixture
+def test_schema1() -> pyarrow.schema:
+    return pyarrow.schema(
+        [
+            ("service_date", pyarrow.string()),
+            ("route_id", pyarrow.large_string()),
+            ("exact_plan_trip_match", pyarrow.bool_()),
+        ]
+    )
 
 
-# def test_tableau_auto_schema_happy_case(test_schema1: pyarrow.schema) -> None:
-#     assert test_schema1 == convert_to_tableau_compatible_schema(test_schema1)
+# simple case
+@pytest.fixture
+def test_schema2() -> pyarrow.schema:
+    return pyarrow.schema(
+        [
+            ("service_date", pyarrow.date32()),
+            ("route_id", pyarrow.large_string()),
+            ("exact_plan_trip_match", pyarrow.bool_()),
+            ("start_dt", pyarrow.timestamp(unit="us", tz="UTC")),
+        ]
+    )
 
 
-# def test_tableau_auto_schema_override(test_schema1: pyarrow.schema) -> None:
-#     assert_test_schema1 = pyarrow.schema(
-#         [
-#             ("service_date", pyarrow.date32()),  # change to date type
-#             ("route_id", pyarrow.large_string()),
-#             ("exact_plan_trip_match", pyarrow.bool_()),
-#         ]
-#     )
-#     overrides = {"service_date": pyarrow.date32()}
-#     assert assert_test_schema1 == convert_to_tableau_compatible_schema(test_schema1, overrides=overrides)
+# all case
+@pytest.fixture
+def test_schema3() -> pyarrow.schema:
+    return pyarrow.schema(
+        [
+            ("service_date", pyarrow.string()),
+            ("route_id", pyarrow.large_string()),
+            ("exact_plan_trip_match", pyarrow.bool_()),
+            ("start_dt", pyarrow.timestamp(unit="us", tz="UTC")),
+        ]
+    )
+
+
+def test_schema_default_from_s3() -> None:
+    bus_events = S3Location(bucket="mbta-performance", prefix=os.path.join("lamp", "bus_vehicle_events"), version="1.2")
+    overrides = {"service_date": pyarrow.date32()}
+    input_schema, output_schema = get_default_tableau_schema_from_s3(input_location=bus_events, overrides=overrides)
+    # print(input_schema)`
+    # print(output_schema)`
+    print(list(set(input_schema).difference(output_schema)))
+    # res = [pyarrow.Field('stop_arrival_dt', timestamp(us, tz=UTC)), pyarrow.Field<gtfs_sort_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<stop_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<service_date: large_string>, pyarrow.Field<tm_actual_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<tm_scheduled_time_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_travel_to_dt: timestamp[us, tz=UTC]>, pyarrow.Field<tm_actual_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_departure_dt: timestamp[us, tz=UTC]>]
+    # diff = {pyarrow.Field<tm_actual_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<service_date: large_string>, pyarrow.Field<tm_scheduled_time_dt: timestamp[us, tz=UTC]>, pyarrow.Field<stop_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<stop_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_travel_to_dt: timestamp[us, tz=UTC]>, pyarrow.Field<tm_actual_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_sort_dt: timestamp[us, tz=UTC]>}
+    breakpoint()
+
+
+
+def test_spare_schema():
+    df = pl.read_parquet("s3://mbta-ctd-dataplatform-staging-springboard/spare/vehicles.parquet")
+
+    convert_to_tableau_compatible_schema(df.to_arrow().schema)
+
+
+def test_tableau_auto_schema_happy_case(test_schema1: pyarrow.schema) -> None:
+    assert test_schema1 == convert_to_tableau_compatible_schema(test_schema1)
+
+
+def test_tableau_auto_schema_override(test_schema1: pyarrow.schema) -> None:
+    assert_test_schema1 = pyarrow.schema(
+        [
+            ("service_date", pyarrow.date32()),  # change to date type
+            ("route_id", pyarrow.large_string()),
+            ("exact_plan_trip_match", pyarrow.bool_()),
+        ]
+    )
+    overrides = {"service_date": pyarrow.date32()}
+    assert assert_test_schema1 == convert_to_tableau_compatible_schema(test_schema1, overrides=overrides)
 
 
 def test_tableau_auto_schema_exclude(test_schema1: pyarrow.schema) -> None:
@@ -77,16 +134,16 @@ def test_tableau_auto_schema_exclude(test_schema1: pyarrow.schema) -> None:
     assert assert_test_schema1 == convert_to_tableau_compatible_schema(test_schema1, excludes=excludes)
 
 
-# def test_tableau_auto_schema_tz(test_schema2: pyarrow.schema) -> None:
-#     assert_test_schema2 = pyarrow.schema(
-#         [
-#             ("service_date", pyarrow.date32()),  # change to date type
-#             ("route_id", pyarrow.large_string()),
-#             ("exact_plan_trip_match", pyarrow.bool_()),
-#             ("start_dt", pyarrow.timestamp(unit="us", tz=None)),
-#         ]
-#     )
-#     assert assert_test_schema2 == convert_to_tableau_compatible_schema(test_schema2)
+def test_tableau_auto_schema_tz(test_schema2: pyarrow.schema) -> None:
+    assert_test_schema2 = pyarrow.schema(
+        [
+            ("service_date", pyarrow.date32()),  # change to date type
+            ("route_id", pyarrow.large_string()),
+            ("exact_plan_trip_match", pyarrow.bool_()),
+            ("start_dt", pyarrow.timestamp(unit="us", tz=None)),
+        ]
+    )
+    assert assert_test_schema2 == convert_to_tableau_compatible_schema(test_schema2)
 
 
 def test_tableau_auto_schema_all_case(test_schema3: pyarrow.schema) -> None:
@@ -103,20 +160,3 @@ def test_tableau_auto_schema_all_case(test_schema3: pyarrow.schema) -> None:
         test_schema3, overrides=overrides, excludes=excludes
     )
 
-
-def test_schema_default_from_s3() -> None:
-    bus_events = S3Location(bucket="mbta-performance", prefix=os.path.join("lamp", "bus_vehicle_events"), version="1.2")
-    overrides = {"service_date": pyarrow.date32()}
-    input_schema, output_schema = get_default_tableau_schema_from_s3(input_location=bus_events, overrides=overrides)
-    # print(input_schema)`
-    # print(output_schema)`
-    print(list(set(input_schema).difference(output_schema)))
-    # res = [pyarrow.Field('stop_arrival_dt', timestamp(us, tz=UTC)), pyarrow.Field<gtfs_sort_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<stop_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<service_date: large_string>, pyarrow.Field<tm_actual_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<tm_scheduled_time_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_travel_to_dt: timestamp[us, tz=UTC]>, pyarrow.Field<tm_actual_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_departure_dt: timestamp[us, tz=UTC]>]
-    # diff = {pyarrow.Field<tm_actual_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<service_date: large_string>, pyarrow.Field<tm_scheduled_time_dt: timestamp[us, tz=UTC]>, pyarrow.Field<stop_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_arrival_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<stop_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_travel_to_dt: timestamp[us, tz=UTC]>, pyarrow.Field<tm_actual_departure_dt: timestamp[us, tz=UTC]>, pyarrow.Field<gtfs_sort_dt: timestamp[us, tz=UTC]>}
-    breakpoint()
-
-
-def test_spare_schema():
-    df = pl.read_parquet("s3://mbta-ctd-dataplatform-staging-springboard/spare/vehicles.parquet")
-
-    convert_to_tableau_compatible_schema(df.to_arrow().schema)
