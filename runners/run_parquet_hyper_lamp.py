9b5dd2ae5812a8e8b466290f45f240427c7cfaaf
Henry Huang <hhuang@mbta.com>
2025-09-04 16:43:38 -0400

    split up runners for spare and lamp tableau

--- runners/run_parquet_hyper_lamp.py
+++ runners/run_parquet_hyper_lamp.py
@@ -4,12 +4,10 @@ from typing import List
 
 from lamp_py.aws.ecs import check_for_parallel_tasks
 from lamp_py.runtime_utils.env_validation import validate_environment
-from lamp_py.tableau.conversions import convert_gtfs_rt_trip_updates
+from lamp_py.tableau.conversions import convert_gtfs_rt_trip_updates, convert_gtfs_rt_vehicle_position
 from lamp_py.tableau.hyper import HyperJob
 from lamp_py.tableau.jobs.bus_performance import HyperBusPerformanceAll, HyperBusPerformanceRecent
 from lamp_py.tableau.jobs.filtered_hyper import FilteredHyperJob
-from lamp_py.tableau.jobs.spare_jobs import spare_job_list
-from lamp_py.tableau.jobs.spare_jobs import SPARE_TABLEAU_PROJECT
 from lamp_py.tableau.pipeline import (
     start_bus_parquet_updates,
 )
@@ -19,14 +17,11 @@ from lamp_py.tableau.jobs.lamp_jobs import (
     HyperGtfsRtTripUpdatesHeavyRail,
 )
 from lamp_py.tableau.spare.default_converter import convert_to_tableau_flat_schema, default_converter_from_s3
-from lamp_py.utils.filter_bank import FilterBankRtTripUpdates
-from lamp_py.tableau.spare.autogen_01_schema_printer import (
-    springboard_spare_cases_with_history,
-    tableau_spare_cases_with_history,
-)
+from lamp_py.utils.filter_bank import FilterBankRtTripUpdates, FilterBankRtVehiclePositions
+
 from lamp_py.runtime_utils.remote_files import (
     # springboard_rt_vehicle_positions,
-    # springboard_devgreen_rt_vehicle_positions,
+    springboard_devgreen_rt_vehicle_positions,
     springboard_rt_trip_updates,  # main feed, all lines, unique records
     springboard_devgreen_lrtp_trip_updates,  # dev green feed, green line only, all records
     # springboard_lrtp_trip_updates,  # main feed, green line only, all records
@@ -34,7 +29,7 @@ from lamp_py.runtime_utils.remote_files import (
     tableau_rt_trip_updates_lightrail_60_day,
     # tableau_rt_vehicle_positions_heavyrail_30_day,
     tableau_rt_trip_updates_heavyrail_30_day,
-    # tableau_devgreen_rt_vehicle_positions_lightrail_60_day,
+    tableau_devgreen_rt_vehicle_positions_lightrail_60_day,
     tableau_devgreen_rt_trip_updates_lightrail_60_day,
     # tableau_rt_vehicle_positions_all_light_rail_7_day,
 )
@@ -62,75 +57,22 @@ TestHyperGtfsRtTripUpdates = FilteredHyperJob(
 TestHyperGtfsRtTripUpdatesHeavyRail = FilteredHyperJob(
     remote_input_location=springboard_rt_trip_updates,
     remote_output_location=tableau_rt_trip_updates_heavyrail_30_day,
-    rollup_num_days=30,
+    rollup_num_days=3,
     processed_schema=convert_gtfs_rt_trip_updates.schema(),
     dataframe_filter=convert_gtfs_rt_trip_updates.heavyrail,
     parquet_filter=FilterBankRtTripUpdates.ParquetFilter.heavy_rail,
     tableau_project_name=GTFS_RT_TABLEAU_PROJECT,
 )
 
-
-def start_spare_single() -> None:
-
-    job = FilteredHyperJob(
-        remote_input_location=springboard_spare_cases_with_history,
-        remote_output_location=tableau_spare_cases_with_history,
-        rollup_num_days=None,
-        processed_schema=default_converter_from_s3(springboard_spare_cases_with_history),
-        parquet_preprocess=None,
-        dataframe_filter=convert_to_tableau_flat_schema,
-        parquet_filter=None,
-        tableau_project_name=SPARE_TABLEAU_PROJECT,
-    )
-    outs = job.run_parquet(None)
-    outs2 = job.create_local_hyper()
-
-
-def start_spare() -> None:
-    """Run all HyperFile Update Jobs"""
-    local_parquet = False
-    run_pq_remote = False
-    local_hyper = False
-    run_hyper_remote = False
-    combined = True
-
-    if local_parquet:
-        for job in spare_job_list:
-            try:
-                outs = job.create_parquet(None)
-                shutil.copy(job.local_parquet_path, job.local_hyper_path.replace(".hyper", ".parquet"))
-            except Exception as e:
-                print(f"{job.remote_parquet_path} parquet/local unable to generate - {e}")
-
-    if run_pq_remote:
-        for job in spare_job_list:
-            try:
-                outs = job.run_parquet(None)
-            except Exception as e:
-                print(f"{job.remote_parquet_path} parquet/upload unable to generate - {e}")
-
-    if local_hyper:
-        for job in spare_job_list:
-            try:
-                shutil.copy(job.local_hyper_path.replace(".hyper", ".parquet"), job.local_parquet_path)
-                outs = job.create_local_hyper(use_local=True)
-            except Exception as e:
-                print(f"{job.remote_parquet_path} hyper/local unable to generate - {e}")
-
-    if run_hyper_remote:
-        for job in spare_job_list:
-            try:
-                outs = job.run_hyper()
-            except Exception as e:
-                print(f"{job.remote_parquet_path} hyper/upload unable to generate - {e}")
-
-    if combined:
-        for job in spare_job_list:
-            try:
-                outs = job.run_parquet_hyper_combined_job()
-            except Exception as e:
-                print(f"{job.remote_parquet_path} combined unable to generate - {e}")
-            #
+TestHyperDevGreenGtfsRtVehiclePositions = FilteredHyperJob(
+    remote_input_location=springboard_devgreen_rt_vehicle_positions,
+    remote_output_location=tableau_devgreen_rt_vehicle_positions_lightrail_60_day,
+    rollup_num_days=3,
+    processed_schema=convert_gtfs_rt_vehicle_position.schema(),
+    dataframe_filter=convert_gtfs_rt_vehicle_position.lrtp,
+    parquet_filter=FilterBankRtVehiclePositions.ParquetFilter.light_rail,
+    tableau_project_name=GTFS_RT_TABLEAU_PROJECT,
+)
 
 
 def start_hyper() -> None:
@@ -138,37 +80,51 @@ def start_hyper() -> None:
 
     hyper_jobs: List[HyperJob] = [
         # HyperBusPerformanceAll(),
-        HyperBusPerformanceRecent(),
+        # HyperBusPerformanceRecent(),
         # HyperDevGreenGtfsRtTripUpdates,
         # HyperGtfsRtTripUpdatesHeavyRail,
         # TestHyperGtfsRtTripUpdatesHeavyRail,
         # TestHyperGtfsRtTripUpdates,
-        # TestHyperDevGreenGtfsRtTripUpdates,
+        TestHyperDevGreenGtfsRtTripUpdates,
+        TestHyperDevGreenGtfsRtVehiclePositions,
     ]
-    # breakpoint()
 
-    run_create_pq = False
-    if run_create_pq:
+    local_parquet = True
+    run_pq_remote = False
+    local_hyper = True
+    run_hyper_remote = False
+
+    if local_parquet:
         for job in hyper_jobs:
-            outs = job.create_parquet(None)
+            try:
+                outs = job.create_parquet(None)
+                shutil.copy(job.local_parquet_path, job.local_hyper_path.replace(".hyper", ".parquet"))
+            except Exception as e:
+                print(f"{job.remote_parquet_path} parquet/local unable to generate - {e}")
 
-    run_pq = True
-    if run_pq:
+    if run_pq_remote:
         for job in hyper_jobs:
-            outs = job.run_parquet(None)
+            try:
+                outs = job.run_parquet(None)
+            except Exception as e:
+                print(f"{job.remote_parquet_path} parquet/upload unable to generate - {e}")
 
-    local_hyper = False
     if local_hyper:
         for job in hyper_jobs:
-            outs = job.create_local_hyper()
+            try:
+                shutil.copy(job.local_hyper_path.replace(".hyper", ".parquet"), job.local_parquet_path)
+                outs = job.create_local_hyper(use_local=True)
+            except Exception as e:
+                print(f"{job.remote_parquet_path} hyper/local unable to generate - {e}")
 
-    hyper = True
-    if hyper:
+    if run_hyper_remote:
         for job in hyper_jobs:
-            outs = job.run_hyper()
+            try:
+                outs = job.run_hyper()
+            except Exception as e:
+                print(f"{job.remote_parquet_path} hyper/upload unable to generate - {e}")
 
 
 if __name__ == "__main__":
-    start_spare()
-    # start_spare_single()
-    # start_hyper()
+
+    start_hyper()
---
+++ runners/run_parquet_hyper_spare.py
@@ -0,0 +1,79 @@
+import shutil
+
+from lamp_py.tableau.jobs.filtered_hyper import FilteredHyperJob
+from lamp_py.tableau.jobs.spare_jobs import spare_job_list
+from lamp_py.tableau.jobs.spare_jobs import SPARE_TABLEAU_PROJECT
+
+from lamp_py.tableau.spare.default_converter import convert_to_tableau_flat_schema, default_converter_from_s3
+from lamp_py.tableau.spare.autogen_01_schema_printer import (
+    springboard_spare_cases_with_history,
+    tableau_spare_cases_with_history,
+)
+
+
+def start_spare_single() -> None:
+
+    job = FilteredHyperJob(
+        remote_input_location=springboard_spare_cases_with_history,
+        remote_output_location=tableau_spare_cases_with_history,
+        rollup_num_days=None,
+        processed_schema=default_converter_from_s3(springboard_spare_cases_with_history),
+        parquet_preprocess=None,
+        dataframe_filter=convert_to_tableau_flat_schema,
+        parquet_filter=None,
+        tableau_project_name=SPARE_TABLEAU_PROJECT,
+    )
+    outs = job.run_parquet(None)
+    outs2 = job.create_local_hyper()
+
+
+def start_spare() -> None:
+    """Run all HyperFile Update Jobs"""
+    local_parquet = False
+    run_pq_remote = False
+    local_hyper = False
+    run_hyper_remote = False
+    combined = True
+
+    if local_parquet:
+        for job in spare_job_list:
+            try:
+                outs = job.create_parquet(None)
+                shutil.copy(job.local_parquet_path, job.local_hyper_path.replace(".hyper", ".parquet"))
+            except Exception as e:
+                print(f"{job.remote_parquet_path} parquet/local unable to generate - {e}")
+
+    if run_pq_remote:
+        for job in spare_job_list:
+            try:
+                outs = job.run_parquet(None)
+            except Exception as e:
+                print(f"{job.remote_parquet_path} parquet/upload unable to generate - {e}")
+
+    if local_hyper:
+        for job in spare_job_list:
+            try:
+                shutil.copy(job.local_hyper_path.replace(".hyper", ".parquet"), job.local_parquet_path)
+                outs = job.create_local_hyper(use_local=True)
+            except Exception as e:
+                print(f"{job.remote_parquet_path} hyper/local unable to generate - {e}")
+
+    if run_hyper_remote:
+        for job in spare_job_list:
+            try:
+                outs = job.run_hyper()
+            except Exception as e:
+                print(f"{job.remote_parquet_path} hyper/upload unable to generate - {e}")
+
+    if combined:
+        for job in spare_job_list:
+            try:
+                outs = job.run_parquet_hyper_combined_job()
+            except Exception as e:
+                print(f"{job.remote_parquet_path} combined unable to generate - {e}")
+            #
+
+
+if __name__ == "__main__":
+    start_spare()
+    # start_spare_single()
