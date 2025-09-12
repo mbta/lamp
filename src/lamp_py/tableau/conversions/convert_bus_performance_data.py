a7483f7ad9ad1e04994b908e7f62e22dd66cc67d
Henry Huang <hhuang@mbta.com>
2025-09-12 14:58:49 -0400

    verification of plan_stop_departure_dt - add America/New_York timezone on bus_events df before tableau upload to make clear which are EST and which are UTC

--- src/lamp_py/tableau/conversions/convert_bus_performance_data.py
+++ src/lamp_py/tableau/conversions/convert_bus_performance_data.py
@@ -50,6 +50,7 @@ def apply_bus_analysis_conversions(polars_df: pl.DataFrame) -> Table:
         pl.col("gtfs_sort_dt").dt.convert_time_zone(time_zone="America/New_York").dt.replace_time_zone(None),
         pl.col("gtfs_departure_dt").dt.convert_time_zone(time_zone="America/New_York").dt.replace_time_zone(None),
         pl.col("gtfs_arrival_dt").dt.convert_time_zone(time_zone="America/New_York").dt.replace_time_zone(None),
+        pl.col("plan_start_dt").dt.convert_time_zone(time_zone="America/New_York").dt.replace_time_zone(None),
         pl.col("plan_stop_departure_dt").dt.convert_time_zone(time_zone="America/New_York").dt.replace_time_zone(None),
     )
 
