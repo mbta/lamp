0216706d4fbf5235f6525565d1a38a9005077afb
Corey N. Runkel <runkelcorey@gmail.com>
2025-09-16 10:57:53 -0400

    Add test for service_date to timestamp conversion

--- tests/bus_performance_manager/test_metrics.py
+++ tests/bus_performance_manager/test_metrics.py
@@ -1,13 +1,18 @@
-from datetime import datetime
+from datetime import datetime, date, time, timedelta
 
 import dataframely as dy
+import polars as pl
 import pytest
 
 from lamp_py.bus_performance_manager.events_tm import TransitMasterEvents
 from lamp_py.bus_performance_manager.combined_bus_schedule import CombinedSchedule
 from lamp_py.bus_performance_manager.events_gtfs_rt import GTFSEvents
 from lamp_py.bus_performance_manager.events_joined import join_rt_to_schedule
-from lamp_py.bus_performance_manager.events_metrics import enrich_bus_performance_metrics, BusPerformanceMetrics
+from lamp_py.bus_performance_manager.events_metrics import (
+    enrich_bus_performance_metrics,
+    BusPerformanceMetrics,
+    timestamp_to_service_date,
+)
 
 
 @pytest.fixture(name="rng")
@@ -63,3 +68,26 @@ def test_preserve_non_null_tm_values(bus_metrics_dataframes: tuple) -> None:
     bus_events = enrich_bus_performance_metrics(join_rt_to_schedule(*bus_metrics_dataframes))
 
     BusPerformanceMetrics.validate({"bus_events": bus_events, "tm_events": bus_metrics_dataframes[2]})
+
+
+def test_timestamp_to_service_date(rng: dy.random.Generator, service_date_start_hour: int = 3) -> None:
+    "It returns the correct service_date."
+
+    test_date = rng.sample_date(min=date(2020, 1, 1), max=date.today())[0]
+    test_times = pl.concat(
+        [rng.sample_time(2, min=time(2, 55), max=time(3)), rng.sample_time(3, min=time(3), max=time(3, 5))]
+    )
+
+    test_df = pl.DataFrame(
+        {
+            "expected_date": [test_date - timedelta(1)] * 2 + [test_date] * 3,
+            "test_dates": [test_date] * 5,
+            "test_times": test_times,
+        }
+    ).with_columns(
+        timestamp_to_service_date(pl.col("test_dates").dt.combine(pl.col("test_times")), service_date_start_hour).alias(
+            "service_date"
+        )
+    )
+
+    assert test_df.filter(pl.col("expected_date") != pl.col("service_date")).height == 0
