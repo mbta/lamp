d8fb298630f4feb19df1e02a71a73b5d3d9586af
Henry Huang <hhuang@mbta.com>
2025-09-17 16:25:51 -0400

    black

--- analysis/vehicle_position_viewer.py
+++ analysis/vehicle_position_viewer.py
@@ -12,6 +12,7 @@ def _():
     import pyarrow.dataset as ds
     from pyarrow.fs import S3FileSystem
     import plotly.express as px
+
     return S3FileSystem, ds, mo, pl, px
 
 
@@ -26,17 +27,30 @@ def _():
 def _(px):
 
     def plot_lla(vp):
-        fig3 = px.scatter_map(vp, lat="vehicle.position.latitude", lon="vehicle.position.longitude", zoom=8, height=300, hover_data="vehicle.timestamp")
+        fig3 = px.scatter_map(
+            vp,
+            lat="vehicle.position.latitude",
+            lon="vehicle.position.longitude",
+            zoom=8,
+            height=300,
+            hover_data="vehicle.timestamp",
+        )
         fig3.update_layout(mapbox_style="open-street-map")
-        fig3.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-        config2 = {'scrollZoom': True}
+        fig3.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+        config2 = {"scrollZoom": True}
         return fig3.show(config2)
+
     return (plot_lla,)
 
 
 @app.cell
 def _(ds_helper, pl, vehicle_id):
-    vp_raw = ds_helper(["s3://mbta-ctd-dataplatform-springboard/lamp/RT_VEHICLE_POSITIONS/year=2025/month=9/day=17/2025-09-17T00:00:00.parquet"], ( pl.col('id') == vehicle_id))
+    vp_raw = ds_helper(
+        [
+            "s3://mbta-ctd-dataplatform-springboard/lamp/RT_VEHICLE_POSITIONS/year=2025/month=9/day=17/2025-09-17T00:00:00.parquet"
+        ],
+        (pl.col("id") == vehicle_id),
+    )
     return (vp_raw,)
 
 
@@ -48,7 +62,7 @@ def _(mo, vp_raw):
 
 @app.cell
 def _(pl, plot_lla, trip_id, vp_raw):
-    plot_lla(vp_raw.filter(pl.col('vehicle.trip.trip_id') == trip_id))
+    plot_lla(vp_raw.filter(pl.col("vehicle.trip.trip_id") == trip_id))
     return
 
 
@@ -56,12 +70,13 @@ def _(pl, plot_lla, trip_id, vp_raw):
 def _(S3FileSystem, ds, pl):
     def ds_helper(s3_uris, filter):
         ds_paths = [s.replace("s3://", "") for s in s3_uris]
-        dset = ds.dataset(ds_paths, format="parquet",  filesystem=S3FileSystem())
+        dset = ds.dataset(ds_paths, format="parquet", filesystem=S3FileSystem())
         if filter is not None:
             aa = pl.scan_pyarrow_dataset(dset).filter(filter).collect()
         else:
             aa = pl.scan_pyarrow_dataset(dset).collect()
         return aa
+
     return (ds_helper,)
 
 
