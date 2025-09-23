53acba1dbed91e39b45d62248313868aa3fb2141
Corey Runkel <39202587+runkelcorey@users.noreply.github.com>
2025-09-23 13:17:43 -0400

    bug(bus): Preserve TM events in downstream data (#583)
    * Log join_rt_to_schedule
    
    * Move BusEvents upstream to events_joined
    
    * Pre-join TM events to ensure events from all vehicles appear in schedule
    
    * Check for missing trips

--- src/lamp_py/bus_performance_manager/events_joined.py
+++ src/lamp_py/bus_performance_manager/events_joined.py
@@ -6,7 +6,60 @@ import polars as pl
 from lamp_py.bus_performance_manager.events_gtfs_schedule import bus_gtfs_schedule_events_for_date
 from lamp_py.bus_performance_manager.events_tm import TransitMasterEvents
 from lamp_py.bus_performance_manager.combined_bus_schedule import CombinedSchedule
+from lamp_py.bus_performance_manager.events_gtfs_rt import GTFSEvents
 from lamp_py.runtime_utils import lamp_exception
+from lamp_py.runtime_utils.process_logger import ProcessLogger
+from lamp_py.utils.filter_bank import SERVICE_DATE_END_HOUR
+
+
+class BusEvents(CombinedSchedule, TransitMasterEvents, GTFSEvents):  # pylint: disable=too-many-ancestors
+    "Stop events from GTFS-RT, TransitMaster, and GTFS Schedule."
+    trip_id = dy.String(primary_key=True)
+    service_date = dy.Date(primary_key=True)
+    stop_sequences_vehicle_label_key = dy.String(
+        primary_key=True, regex=r"[0-9_]{2}\|[0-9_]{2}\|(\w|_)+"
+    )  # zero-padded tm_stop_sequence, zero-padded stop_sequence, and vehicle_label separated by |
+    tm_stop_sequence = dy.Int64(nullable=True, primary_key=False)
+    vehicle_label = dy.String(nullable=True, primary_key=False)
+    stop_sequence = dy.Int64(nullable=True, primary_key=False)
+
+
+class BusPerformanceManager(dy.Collection):
+    "Relationships between BusPM datasets."
+    tm: dy.LazyFrame[TransitMasterEvents]
+    bus: dy.LazyFrame[BusEvents]
+    gtfs: dy.LazyFrame[GTFSEvents]
+
+    @dy.filter()
+    def preserve_all_trips(self) -> pl.LazyFrame:
+        "If trips appear in GTFS or TM, then they appear in downstream records."
+        missing_gtfs_trips = (
+            self.gtfs.select(self.common_primary_keys())
+            .unique()
+            .join(self.bus, how="anti", on=self.common_primary_keys())
+        )
+        missing_tm_trips = (
+            self.tm.select(self.common_primary_keys())
+            .unique()
+            .join(self.bus, how="anti", on=self.common_primary_keys())
+        )
+
+        return (
+            self.bus.select(self.common_primary_keys())
+            .unique()
+            .join(pl.concat([missing_gtfs_trips, missing_tm_trips]), on=self.common_primary_keys(), how="anti")
+        )
+
+    @dy.filter()
+    def preserve_tm_events(self) -> pl.LazyFrame:
+        "If values in TransitMaster are not null, then downstream records should also not be null for those columns."
+        keys = ["trip_id", "tm_stop_sequence", "tm_actual_arrival_dt", "tm_actual_departure_dt", "tm_scheduled_time_dt"]
+
+        missing_tm_events = self.tm.join(  # locate events that have mismatched event values
+            self.bus, how="anti", on=keys, nulls_equal=True
+        )
+
+        return self.bus.join(missing_tm_events, how="anti", on=self.common_primary_keys())  # filter out those events
 
 
 def match_plan_trips(gtfs: pl.DataFrame, schedule: pl.DataFrame) -> pl.DataFrame:
@@ -222,8 +275,8 @@ def join_schedule_to_rt(gtfs: pl.DataFrame) -> pl.DataFrame:
 
 
 def join_rt_to_schedule(
-    schedule: dy.DataFrame[CombinedSchedule], gtfs: pl.DataFrame, tm: dy.DataFrame[TransitMasterEvents]
-) -> pl.DataFrame:
+    schedule: dy.DataFrame[CombinedSchedule], gtfs: dy.DataFrame[GTFSEvents], tm: dy.DataFrame[TransitMasterEvents]
+) -> dy.DataFrame[BusEvents]:
     """
     Join gtfs-rt and transit master (tm) event dataframes using "asof" strategy for stop_sequence columns.
     There are frequent occasions where the stop_sequence and tm_stop_sequence are not exactly the same.
@@ -271,13 +324,24 @@ def join_rt_to_schedule(
     # there are frequent occasions where the stop_sequence and tm_stop_sequence are not exactly the same
     # usually off by 1 or so. By matching the nearest stop sequence
     # after grouping by trip, route, vehicle, and most importantly for sequencing - stop_id
+    process_logger = ProcessLogger("join_rt_to_schedule")
+    process_logger.log_start()
+
+    schedule_vehicles = schedule.join(
+        pl.concat(
+            [tm.select("trip_id", "vehicle_label", "stop_id"), gtfs.select("trip_id", "vehicle_label", "stop_id")]
+        ).unique(),
+        how="left",
+        on=["trip_id", "stop_id"],
+        coalesce=True,
+    )
 
     schedule_gtfs = (
-        schedule.sort(by="stop_sequence")
+        schedule_vehicles.sort(by="stop_sequence")
         .join_asof(
             gtfs.sort(by="stop_sequence"),
             on="stop_sequence",
-            by=["trip_id", "stop_id"],
+            by=["trip_id", "stop_id", "vehicle_label"],
             strategy="nearest",
             coalesce=True,
             suffix="_right_gtfs",
@@ -316,6 +380,38 @@ def join_rt_to_schedule(
             "timepoint_abbr_right_tm",
             "timepoint_name_right_tm",
         )
+        .with_columns(
+            pl.concat_str(
+                [
+                    pl.coalesce(pl.col("tm_stop_sequence").cast(pl.String).str.zfill(2), pl.lit("__")),
+                    pl.coalesce(pl.col("stop_sequence").cast(pl.String).str.zfill(2), pl.lit("__")),
+                    pl.coalesce(pl.col("vehicle_label"), pl.lit("_____")),
+                ],
+                separator="|",
+            ).alias("stop_sequences_vehicle_label_key"),
+            pl.coalesce(
+                pl.col("service_date"),
+                pl.when(pl.col("plan_start_dt").dt.hour() < SERVICE_DATE_END_HOUR)
+                .then(pl.col("plan_start_dt").dt.offset_by("-1d").dt.date())
+                .otherwise(pl.col("plan_start_dt").dt.date()),
+            ).alias("service_date"),
+        )
     )
 
-    return schedule_gtfs_tm
+    valid, invalid = BusEvents.filter(schedule_gtfs_tm)
+
+    process_logger.add_metadata(valid_records=valid.height, validation_errors=sum(invalid.counts().values()))
+
+    if invalid.counts():
+        process_logger.log_failure(dy.exc.ValidationError(", ".join(invalid.counts().keys())))
+
+    valid_collection = BusPerformanceManager.is_valid(
+        {"tm": tm.lazy(), "bus": schedule_gtfs_tm.lazy(), "gtfs": gtfs.lazy()}
+    )
+
+    if not valid_collection:
+        process_logger.log_failure(dy.exc.ValidationError(BusPerformanceManager.__name__ + " failed validation"))
+
+    process_logger.log_complete()
+
+    return valid
