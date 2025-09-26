7f294a46bec87ad341d7b9087d567bd3fbd74b7c
Corey Runkel <39202587+runkelcorey@users.noreply.github.com>
2025-09-26 17:56:23 -0400

    feat(bus) Impute gtfs_arrival_time for last scheduled stop (#586)

--- tests/bus_performance_manager/test_gtfs_rt_ingestion.py
+++ tests/bus_performance_manager/test_gtfs_rt_ingestion.py
@@ -115,7 +115,7 @@ def test_gtfs_rt_to_bus_events(exists_patch: mock.MagicMock) -> None:
         assert event["direction_id"] == 0
 
         if event["stop_id"] == "173":
-            assert event["gtfs_travel_to_dt"] == datetime(
+            assert event["gtfs_in_transit_to_dts"]["first_timestamp"] == datetime(
                 year=2024, month=6, day=1, hour=13, minute=1, second=19, tzinfo=timezone.utc
             )
             assert event["gtfs_arrival_dt"] == datetime(
@@ -123,7 +123,7 @@ def test_gtfs_rt_to_bus_events(exists_patch: mock.MagicMock) -> None:
             )
 
         if event["stop_id"] == "655":
-            assert event["gtfs_travel_to_dt"] == datetime(
+            assert event["gtfs_in_transit_to_dts"]["first_timestamp"] == datetime(
                 year=2024, month=6, day=1, hour=12, minute=50, second=31, tzinfo=timezone.utc
             )
             assert event["gtfs_arrival_dt"] == datetime(
@@ -131,7 +131,7 @@ def test_gtfs_rt_to_bus_events(exists_patch: mock.MagicMock) -> None:
             )
 
         if event["stop_id"] == "903":
-            assert event["gtfs_travel_to_dt"] == datetime(
+            assert event["gtfs_in_transit_to_dts"]["first_timestamp"] == datetime(
                 year=2024, month=6, day=1, hour=13, minute=3, second=39, tzinfo=timezone.utc
             )
             assert event["gtfs_arrival_dt"] == datetime(
@@ -152,13 +152,13 @@ def test_gtfs_rt_to_bus_events(exists_patch: mock.MagicMock) -> None:
 
         # no arrival time at this stop
         if event["stop_id"] == "12005":
-            assert event["gtfs_travel_to_dt"] == datetime(
+            assert event["gtfs_in_transit_to_dts"]["first_timestamp"] == datetime(
                 year=2024, month=6, day=1, hour=12, minute=47, second=23, tzinfo=timezone.utc
             )
             assert event["gtfs_arrival_dt"] is None
 
         if event["stop_id"] == "17091":
-            assert event["gtfs_travel_to_dt"] == datetime(
+            assert event["gtfs_in_transit_to_dts"]["first_timestamp"] == datetime(
                 year=2024, month=6, day=1, hour=12, minute=52, second=41, tzinfo=timezone.utc
             )
             assert event["gtfs_arrival_dt"] is None
@@ -352,14 +352,14 @@ def route_three() -> pl.DataFrame:
 
 def route_four() -> pl.DataFrame:
     """
-    No Sopped At data, only In Transit To
+    No Stopped At data, only In Transit To
     """
     data = {
         "route_id": ["4", "4", "4", "4", "4", "4"],
         "direction_id": [0, 0, 0, 0, 0, 0],
         "trip_id": ["404", "404", "404", "404", "404", "404"],
         "stop_id": ["1", "1", "1", "2", "2", "2"],
-        "stop_sequence": [1, 1, 1, 2, 2, 2],
+        "stop_sequence": [2, 2, 2, 3, 3, 3],
         "start_time": [
             "11:45:00",
             "11:45:00",
