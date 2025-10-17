77aff46a8f1ae38cc657e096d4e12b6187d38611
Corey N. Runkel <runkelcorey@gmail.com>
2025-10-17 11:42:38 -0400

    Update stop order test

--- tests/bus_performance_manager/test_events_metrics.py
+++ tests/bus_performance_manager/test_events_metrics.py
@@ -1,5 +1,5 @@
 from contextlib import nullcontext
-from datetime import datetime
+from datetime import datetime, date
 
 import polars as pl
 import pytest
@@ -39,35 +39,45 @@ def test_dy_departure_after_arrival(
 
 
 @pytest.mark.parametrize(
-    ["stop_arrival_dt", "stop_departure_dt", "num_rows"],
+    ["stop_arrival_dt", "stop_departure_dt", "service_date", "num_rows"],
     [
         (
             [datetime(2000, 1, 1, 1), datetime(2000, 1, 1)],
             [datetime(2000, 1, 1, 2), None],
+            [date(2000, 1, 1), date(2000, 1, 1)],
             pytest.raises(ValidationError),
         ),
-        ([datetime(2000, 1, 1), None], [datetime(2000, 1, 1, 1), datetime(2000, 1, 1)], pytest.raises(ValidationError)),
+        ([datetime(2000, 1, 1), None], [datetime(2000, 1, 1, 1), datetime(2000, 1, 1)], [date(2000, 1, 1), date(2000, 1, 1)], pytest.raises(ValidationError)),
         (
             [datetime(2000, 1, 1), datetime(2000, 1, 1, 2)],
             [datetime(2000, 1, 1, 1), datetime(2000, 1, 1, 3)],
+            [date(2000, 1, 1), date(2000, 1, 1)],
             nullcontext(2),
         ),
+        (
+            [datetime(2000, 1, 1, 1), datetime(2000, 1, 1)],
+            [datetime(2000, 1, 1, 2), None],
+            [date(2000, 1, 1), date(2000, 1, 2)],
+            nullcontext(2),
+        )
     ],
     ids=[
         "out-of-order-arrival",
         "out-of-order_departure",
         "valid-arrival_equal-departure",
+        "different_service-dates",
     ],
 )
 def test_dy_stop_sequence_implies_time_order(
-    dy_gen: Generator, stop_arrival_dt: list[datetime], stop_departure_dt: list[datetime], num_rows: pytest.RaisesExc
+    dy_gen: Generator, stop_arrival_dt: list[datetime], stop_departure_dt: list[datetime], service_date: list[date], num_rows: pytest.RaisesExc
 ) -> None:
     "It returns false if any departure or arrival time is earlier than the preceding record."
     df = BusPerformanceMetrics.sample(
         num_rows=2,
         overrides={
             "vehicle_label": ["123", "123"],
-            "tm_stop_sequence": [2, 3],
+            "stop_sequence": [2, 3],
+            "service_date": service_date,
             "stop_arrival_dt": stop_arrival_dt,
             "stop_departure_dt": stop_departure_dt,
         },
