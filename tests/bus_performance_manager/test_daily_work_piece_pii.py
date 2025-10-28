d4de4675a49f0632361f860409aa0ac5172e1cf4
Henry Huang <hhuang@mbta.com>
2025-10-28 15:02:50 -0400

    shell of tests to write

---
+++ tests/bus_performance_manager/test_daily_work_piece_pii.py
@@ -0,0 +1,19 @@
+def test_daily_work_piece_files_as_frame() -> None:
+    pass
+
+
+def test_event_files_to_load() -> None:
+    pass
+    # test the new loader - assert dates
+
+
+def test_create_public_operator_id_map() -> None:
+    pass
+
+
+def test_combine_schedule_and_run_id_operator_id() -> None:
+    pass
+
+
+def test_operator_id_over_multiple_days() -> None:
+    pass
