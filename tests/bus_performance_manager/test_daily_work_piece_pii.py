5723f7e56add7f12103eadd44aa0888dfc650575
Henry Huang <hhuang@mbta.com>
2025-10-30 13:26:48 -0400

    restructure argument to pass in seed to make test reproducable

--- tests/bus_performance_manager/test_daily_work_piece_pii.py
+++ tests/bus_performance_manager/test_daily_work_piece_pii.py
@@ -1,19 +1,43 @@
-def test_daily_work_piece_files_as_frame() -> None:
-    pass
-
-
-def test_event_files_to_load() -> None:
-    pass
-    # test the new loader - assert dates
+from datetime import date
+import dataframely as dy
+import polars as pl
+from lamp_py.bus_performance_manager.events_tm import TMDailyWorkPiece, create_public_operator_id_map
 
 
 def test_create_public_operator_id_map() -> None:
-    pass
+    """
+    Basic test to show functionality of public_operator_id creation. 
+    """
+    # ┌────────────┬───────────────────────┬──────────────┬────────────────────┐
+    # │ tm_trip_id ┆ operator_badge_number ┆ service_date ┆ public_operator_id │
+    # │ ---        ┆ ---                   ┆ ---          ┆ ---                │
+    # │ str        ┆ str                   ┆ date         ┆ i64                │
+    # ╞════════════╪═══════════════════════╪══════════════╪════════════════════╡
+    # │ 123        ┆ oy@\a_BRaC'  ;r>      ┆ 2025-10-31   ┆ 2025103160494      │
+    # │ 456        ┆ IDejv                 ┆ 2025-10-31   ┆ 2025103165125      │
+    # │ 789        ┆ j9                    ┆ 2025-10-31   ┆ 2025103115306      │
+    # └────────────┴───────────────────────┴──────────────┴────────────────────┘
+
+    test_df = TMDailyWorkPiece.sample(
+        num_rows=3,
+        overrides={
+            "tm_trip_id": ["123", "456", "789"],
+            # "public_operator_id": [None, None, None, None, None],
+            "service_date": [
+                date(year=2025, month=10, day=31),
+                date(year=2025, month=10, day=31),
+                date(year=2025, month=10, day=31),
+                # date(year=2025, month=10, day=30),
+                # date(year=2025, month=10, day=30),
+            ],
+        },
+        generator=dy.random.Generator(seed=0),
+    )
+    output_df = create_public_operator_id_map(test_df, seed=0)
+
+    assert output_df["public_operator_id"].n_unique() == 3
+    assert output_df["public_operator_id"].equals(pl.Series([2025103160494, 2025103165125, 2025103115306]))
 
 
-def test_combine_schedule_and_run_id_operator_id() -> None:
-    pass
-
-
-def test_operator_id_over_multiple_days() -> None:
-    pass
+# rule - operator id and trip_id have same # of unique
+# rule - operator_id for the same trip are all the same
