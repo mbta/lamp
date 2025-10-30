62f608233714180e562abbd4de071e2fdfe15a6d
Henry Huang <hhuang@mbta.com>
2025-10-30 16:12:09 -0400

    put bus_operator_mapping in springboard for now

--- src/lamp_py/runtime_utils/remote_files.py
+++ src/lamp_py/runtime_utils/remote_files.py
@@ -146,7 +146,9 @@ tm_pattern_geo_node_xref_file = S3Location(
 
 # published by LAMP
 bus_events = S3Location(bucket=S3_PUBLIC, prefix=os.path.join(LAMP, "bus_vehicle_events"), version="1.2")
-bus_operator_mapping = S3Location(bucket=S3_ARCHIVE, prefix=os.path.join(LAMP, "bus_operator_mapping"), version="1.0")
+
+# temporary! - will be moved
+bus_operator_mapping = S3Location(bucket=S3_SPRINGBOARD, prefix=os.path.join(LAMP, "bus_operator_mapping"), version="1.0")
 
 # Kinesis stream glides events
 glides_trips_updated = S3Location(
