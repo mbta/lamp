3d4ab270adbb8cebca1f980f9d2b63591142888e
Henry Huang <hhuang@mbta.com>
2025-11-13 16:36:38 -0500

    rename lamp rail back to to lamp all for now

--- src/lamp_py/tableau/jobs/lamp_jobs.py
+++ src/lamp_py/tableau/jobs/lamp_jobs.py
@@ -128,8 +128,8 @@ HyperBusOperatorMappingAll = FilteredHyperJob(
 HyperRtRailSubway = HyperRtRail(
     route_type_operator="<",
     route_type_operand="2",  # enum 1 = local rail
-    hyper_file_name=os.path.join(tableau_rail_subway.s3_uri, "LAMP_RAIL_RT_fields.hyper"),
-    remote_parquet_path=os.path.join(tableau_rail_subway.s3_uri, "LAMP_RAIL_RT_fields.parquet"),
+    hyper_file_name=os.path.join(tableau_rail_subway.s3_uri, "LAMP_ALL_RT_fields.hyper"),
+    remote_parquet_path=os.path.join(tableau_rail_subway.s3_uri, "LAMP_ALL_RT_fields.parquet"),
     lamp_version="1.2.2",
 )
 
