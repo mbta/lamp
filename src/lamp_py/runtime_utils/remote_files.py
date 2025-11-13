3fd03fd8624d9b96d40fdb330399dbc73c3e7d2c
Henry Huang <hhuang@mbta.com>
2025-11-13 14:06:07 -0500

    add tableau output location for commuter rail

--- src/lamp_py/runtime_utils/remote_files.py
+++ src/lamp_py/runtime_utils/remote_files.py
@@ -160,10 +160,17 @@ public_alerts_file = S3Location(
     bucket=S3_PUBLIC,
     prefix=os.path.join(TABLEAU, "alerts", "LAMP_RT_ALERTS.parquet"),
 )
-tableau_rail = S3Location(
+
+tableau_rail_subway = S3Location(
     bucket=S3_PUBLIC,
-    prefix=os.path.join(TABLEAU, "rail"),
+    prefix=os.path.join(TABLEAU, "rail", "LAMP_ALL_RT_fields.parquet"),
 )
+
+tableau_rail_commuter = S3Location(
+    bucket=S3_PUBLIC,
+    prefix=os.path.join(TABLEAU, "rail", "LAMP_COMMUTER_RAIL_RT_fields.parquet"),
+)
+
 tableau_bus_recent = S3Location(bucket=S3_PUBLIC, prefix=os.path.join(TABLEAU, "bus", "LAMP_RECENT_Bus_Events.parquet"))
 tableau_bus_all = S3Location(bucket=S3_PUBLIC, prefix=os.path.join(TABLEAU, "bus", "LAMP_ALL_Bus_Events.parquet"))
 
