a58729c141589689f093f6f98d6b814b1753060c
Henry Huang <hhuang@mbta.com>
2025-11-13 21:12:33 -0500

    hyper_file_name argument is just the file name..

--- src/lamp_py/tableau/jobs/lamp_jobs.py
+++ src/lamp_py/tableau/jobs/lamp_jobs.py
@@ -128,7 +128,7 @@ HyperBusOperatorMappingAll = FilteredHyperJob(
 HyperRtRailSubway = HyperRtRail(
     route_type_operator="<",
     route_type_operand="2",  # enum 1 = local rail
-    hyper_file_name=os.path.join(tableau_rail_subway.s3_uri, "LAMP_ALL_RT_fields.hyper"),
+    hyper_file_name="LAMP_ALL_RT_fields.hyper",
     remote_parquet_path=os.path.join(tableau_rail_subway.s3_uri, "LAMP_ALL_RT_fields.parquet"),
     lamp_version="1.2.2",
 )
@@ -136,7 +136,7 @@ HyperRtRailSubway = HyperRtRail(
 HyperRtRailCommuter = HyperRtRail(
     route_type_operator="=",
     route_type_operand="2",  # enum 2 = commuter rail
-    hyper_file_name=os.path.join(tableau_rail_commuter.s3_uri, "LAMP_COMMUTER_RAIL_RT_fields.hyper"),
+    hyper_file_name="LAMP_COMMUTER_RAIL_RT_fields.hyper",
     remote_parquet_path=os.path.join(tableau_rail_commuter.s3_uri, "LAMP_COMMUTER_RAIL_RT_fields.parquet"),
     lamp_version="1.0.0",
 )
