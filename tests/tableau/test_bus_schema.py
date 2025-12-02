ffac9221f9c69de77dac3c15e7b1f87b03423b92
Corey N. Runkel <crunkel@mbta.com>
2025-12-02 17:59:03 -0500

    Update tests

--- tests/tableau/test_bus_schema.py
+++ tests/tableau/test_bus_schema.py
@@ -2,7 +2,6 @@ from lamp_py.bus_performance_manager.events_metrics import BusPerformanceMetrics
 from lamp_py.tableau.conversions.convert_bus_performance_data import apply_bus_analysis_conversions
 from lamp_py.tableau.jobs.bus_performance import bus_schema
 import polars as pl
-import pyarrow
 
 
 def test_bus_parquet_convertible_to_bus_tableau_columns() -> None:
@@ -11,7 +10,7 @@ def test_bus_parquet_convertible_to_bus_tableau_columns() -> None:
 
     polars_df = pl.from_arrow(sample).select(bus_schema.names)
 
-    excluded_columns = {"tm_planned_sequence_start", "tm_gtfs_sequence_diff"}
+    excluded_columns = {"tm_planned_sequence_start"}
     # all columns in bus dataset should be in tableau dataset - just with type conversions
     assert set(sample.columns).difference(polars_df.columns) == excluded_columns
 
