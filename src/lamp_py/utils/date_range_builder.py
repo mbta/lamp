ba5d65d09dbef3ec5330d58115267f0834de76c8
Henry Huang <hhuang@mbta.com>
2025-12-10 16:48:20 -0500

    add build_time_range_paths method that constructs all datetime paths between two dates

--- src/lamp_py/utils/date_range_builder.py
+++ src/lamp_py/utils/date_range_builder.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import datetime
 
 
 # Create a DataFrame with two date columns
@@ -23,5 +24,30 @@ def build_data_range_paths(template_string: str, start_date: date, end_date: dat
         # prefix_whole_path = f"year={yy}/month={mm}/day={dd}/{yy}-{mm:02d}-{dd:02d}T00:00:00.parquet"
 
         formatted = template_string.format(yy=tmp.year, mm=tmp.month, dd=tmp.day)
+        date_paths.append(formatted)
+    return date_paths
+
+
+def build_time_range_paths(
+    template_string: str,
+    start_time: datetime.time,
+    end_time: datetime.time,
+    delta: datetime.timedelta = datetime.timedelta(seconds=1),
+) -> list[str]:
+    """
+    Given an f-string template, fill in the {} in template with all the days between
+    start_date and end_date (inclusive) and return the result as a list of strings
+    """
+
+    # loop up by seconds
+    # add 1 for inclusive
+    date_diff_days = (start_time - end_time).days * -1 + 1
+
+    date_paths = []
+    #
+    for i in range(0, date_diff_days):
+        tmp = start_time + timedelta(days=i)
+
+        formatted = template_string.format(hh=tmp.hh, min=tmp.min, dd=tmp.day)
         date_paths.append(formatted)
     return date_paths
