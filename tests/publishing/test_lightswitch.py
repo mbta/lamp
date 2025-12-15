97cbb05bb5ce72fbb6cf0daa6eeeaafb175a3d10
Corey N. Runkel <crunkel@mbta.com>
2025-12-15 12:41:04 -0500

    Make test local

--- tests/publishing/test_lightswitch.py
+++ tests/publishing/test_lightswitch.py
@@ -1,7 +1,9 @@
 import os
 from contextlib import nullcontext
+from pathlib import Path
 from typing import Generator
 
+import polars as pl
 import pytest
 import duckdb
 
@@ -19,7 +21,7 @@ from tests.test_resources import rt_vehicle_positions, tm_route_file
 def fixture_duckdb_con(
     path: str = ":memory:", read_only: bool = False
 ) -> Generator[duckdb.DuckDBPyConnection, None, None]:
-    "Reusable DuckDB instance, scoped to each function."
+    """Reusable DuckDB instance, scoped to each function."""
     yield duckdb.connect(path, read_only)
     try:
         os.remove(path)
@@ -39,7 +41,7 @@ def fixture_duckdb_con(
 def test_build_view(
     duckdb_con: duckdb.DuckDBPyConnection, partition_strategy: str, data_location: S3Location, result: pytest.RaisesExc
 ) -> None:
-    "It gracefully creates the view using the specified partition strategy."
+    """It gracefully creates the view using the specified partition strategy."""
     assert result == build_view(duckdb_con, "test", data_location, partition_strategy)
 
 
@@ -63,19 +65,11 @@ def test_build_view(
 def test_add_views_to_local_metastore(
     duckdb_con: duckdb.DuckDBPyConnection, view_dict: dict, view_names: pytest.RaisesExc
 ) -> None:
-    "It builds the views that are passed to it."
+    """It builds the views that are passed to it."""
     with view_names:
         assert view_names.enter_result == add_views_to_local_metastore(duckdb_con, view_dict)  # type: ignore[attr-defined]
 
 
-@pytest.mark.parametrize(
-    ["bucket"],
-    [
-        ("s3://mbta-ctd-dataplatform-springboard",),
-        ("s3://mbta-ctd-dataplatform-staging-springboard",),
-    ],
-    ids=["prod", "staging"],
-)
 @pytest.mark.parametrize(
     ["directory_name", "raises"],
     [
@@ -89,15 +83,19 @@ def test_add_views_to_local_metastore(
 )
 def test_register_read_ymd(
     duckdb_con: duckdb.DuckDBPyConnection,
-    bucket: str,
+    tmp_path: Path,
     directory_name: str,
     raises: pytest.RaisesExc,
 ) -> None:
     """It raises errors on directories that don't exist."""
+    file_path = tmp_path.joinpath("lamp/RT_VEHICLE_POSITIONS/year=2024/month=6/day=1/2024-06-01T00:00:00.parquet")
+    file_path.parent.mkdir(parents=True)
+    pl.read_parquet("tests/test_files/SPRINGBOARD/RT_VEHICLE_POSITIONS/year=2024").write_parquet(file_path)
+
     with raises:
         with duckdb_con:
             authenticate(duckdb_con)
             register_read_ymd(duckdb_con)
             assert duckdb_con.sql(
-                f"SELECT * FROM read_ymd('{directory_name}', '2024-06-01' :: DATE, '2024-06-02' :: DATE, '{bucket}')"
+                f"SELECT * FROM read_ymd('{directory_name}', '2024-06-01' :: DATE, '2024-06-02' :: DATE, '{tmp_path.resolve()}')"
             )
