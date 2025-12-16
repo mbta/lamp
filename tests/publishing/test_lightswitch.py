3d014bc346d02566283419dec2064d75e3095408
Corey Runkel <39202587+runkelcorey@users.noreply.github.com>
2025-12-16 13:24:54 -0500

    feat(lightswitch): add read_ymd function (#647)
    * Add read_ymd function to lightswitch
    
    * Make test local
    
    * Remove obsolete authentication step
    
    * Remove unused import

--- tests/publishing/test_lightswitch.py
+++ tests/publishing/test_lightswitch.py
@@ -1,12 +1,17 @@
 import os
 from contextlib import nullcontext
+from pathlib import Path
 from typing import Generator
 
+import polars as pl
 import pytest
 import duckdb
 
-from lamp_py.publishing.lightswitch import build_view, add_views_to_local_metastore
-from lamp_py.runtime_utils.lamp_exception import EmptyDataStructureException
+from lamp_py.publishing.lightswitch import (
+    build_view,
+    add_views_to_local_metastore,
+    register_read_ymd,
+)
 from lamp_py.runtime_utils.remote_files import S3Location
 from tests.test_resources import rt_vehicle_positions, tm_route_file
 
@@ -15,7 +20,7 @@ from tests.test_resources import rt_vehicle_positions, tm_route_file
 def fixture_duckdb_con(
     path: str = ":memory:", read_only: bool = False
 ) -> Generator[duckdb.DuckDBPyConnection, None, None]:
-    "Reusable DuckDB instance, scoped to each function."
+    """Reusable DuckDB instance, scoped to each function."""
     yield duckdb.connect(path, read_only)
     try:
         os.remove(path)
@@ -35,7 +40,7 @@ def fixture_duckdb_con(
 def test_build_view(
     duckdb_con: duckdb.DuckDBPyConnection, partition_strategy: str, data_location: S3Location, result: pytest.RaisesExc
 ) -> None:
-    "It gracefully creates the view using the specified partition strategy."
+    """It gracefully creates the view using the specified partition strategy."""
     assert result == build_view(duckdb_con, "test", data_location, partition_strategy)
 
 
@@ -59,6 +64,36 @@ def test_build_view(
 def test_add_views_to_local_metastore(
     duckdb_con: duckdb.DuckDBPyConnection, view_dict: dict, view_names: pytest.RaisesExc
 ) -> None:
-    "It builds the views that are passed to it."
+    """It builds the views that are passed to it."""
     with view_names:
         assert view_names.enter_result == add_views_to_local_metastore(duckdb_con, view_dict)  # type: ignore[attr-defined]
+
+
+@pytest.mark.parametrize(
+    ["directory_name", "raises"],
+    [
+        ("RT_VEHICLE_POSITIONS", nullcontext()),
+        ("foo", pytest.raises(Exception)),
+    ],
+    ids=[
+        "exists",
+        "does-not-exist",
+    ],
+)
+def test_register_read_ymd(
+    duckdb_con: duckdb.DuckDBPyConnection,
+    tmp_path: Path,
+    directory_name: str,
+    raises: pytest.RaisesExc,
+) -> None:
+    """It raises errors on directories that don't exist."""
+    file_path = tmp_path.joinpath("lamp/RT_VEHICLE_POSITIONS/year=2024/month=6/day=1/2024-06-01T00:00:00.parquet")
+    file_path.parent.mkdir(parents=True)
+    pl.read_parquet("tests/test_files/SPRINGBOARD/RT_VEHICLE_POSITIONS/year=2024").write_parquet(file_path)
+
+    with raises:
+        with duckdb_con:
+            register_read_ymd(duckdb_con)
+            assert duckdb_con.sql(
+                f"SELECT * FROM read_ymd('{directory_name}', '2024-06-01' :: DATE, '2024-06-02' :: DATE, '{tmp_path.resolve()}')"
+            )
