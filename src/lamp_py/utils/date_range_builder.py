132385d454e8da8f9fcb29d4ebdd807538e4b4d8
Henry Huang <hhuang@mbta.com>
2025-12-18 13:42:18 -0500

    remove unused method

--- src/lamp_py/utils/date_range_builder.py
+++ src/lamp_py/utils/date_range_builder.py
@@ -26,28 +26,3 @@ def build_data_range_paths(template_string: str, start_date: date, end_date: dat
         formatted = template_string.format(yy=tmp.year, mm=tmp.month, dd=tmp.day)
         date_paths.append(formatted)
     return date_paths
-
-
-def build_time_range_paths(
-    template_string: str,
-    start_time: datetime.time,
-    end_time: datetime.time,
-    delta: datetime.timedelta = datetime.timedelta(seconds=1),
-) -> list[str]:
-    """
-    Given an f-string template, fill in the {} in template with all the datetimes between
-    start_date and end_date (inclusive) and return the result as a list of strings
-    """
-
-    # loop up by seconds
-    # add 1 for inclusive
-    date_diff_days = (start_time - end_time).days * -1 + 1
-
-    date_paths = []
-    #
-    for i in range(0, date_diff_days):
-        tmp = start_time + timedelta(days=i)
-
-        formatted = template_string.format(hh=tmp.hh, min=tmp.min, dd=tmp.day)
-        date_paths.append(formatted)
-    return date_paths
