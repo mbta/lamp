cdd3f254eab602fcc4bee07ef43f2bda321ac0a9
Henry Huang <hhuang@mbta.com>
2025-12-30 15:58:27 -0500

    add context for custom logging level in file_list_from_s3 to suppress logs selectively. this is enabled in file_list_from_date_range to reduce wordy printing. regularly scheduled logging is re-enabled at the end of the context mgr

--- runners/run_parquet_hyper_lamp.py
+++ runners/run_parquet_hyper_lamp.py
@@ -1,80 +1,77 @@
+import os
 import shutil
 from typing import List
 
-from lamp_py.aws.ecs import check_for_parallel_tasks
-from lamp_py.runtime_utils.env_validation import validate_environment
-from lamp_py.tableau.conversions.convert_gtfs_rt_trip_updates import (
-    LightRailTerminalTripUpdates,
-    HeavyRailTerminalTripUpdates,
-)
-from lamp_py.tableau.conversions.convert_gtfs_rt_vehicle_position import LightRailTerminalVehiclePositions
-from lamp_py.tableau.conversions import convert_gtfs_rt_trip_updates, convert_gtfs_rt_vehicle_position
 from lamp_py.tableau.hyper import HyperJob
-from lamp_py.tableau.jobs.bus_performance import HyperBusPerformanceAll, HyperBusPerformanceRecent
+from lamp_py.tableau.jobs.bus_performance import HyperBusPerformanceRecent, HyperBusPerformanceAll
 from lamp_py.tableau.jobs.filtered_hyper import FilteredHyperJob, days_ago
-from lamp_py.tableau.pipeline import (
-    start_bus_parquet_updates,
-)
-from lamp_py.tableau.jobs.lamp_jobs import (
-    GTFS_RT_TABLEAU_PROJECT,
-    HyperDevGreenGtfsRtTripUpdates,
-    HyperGtfsRtTripUpdatesHeavyRail,
-)
-from lamp_py.utils.filter_bank import FilterBankRtTripUpdates, FilterBankRtVehiclePositions
+from datetime import date
+from lamp_py.tableau.jobs.filtered_hyper import FilteredHyperJob
 
 from lamp_py.runtime_utils.remote_files import (
-    # springboard_rt_vehicle_positions,
-    springboard_devgreen_rt_vehicle_positions,
-    springboard_rt_trip_updates,  # main feed, all lines, unique records
-    springboard_devgreen_lrtp_trip_updates,  # dev green feed, green line only, all records
-    # springboard_lrtp_trip_updates,  # main feed, green line only, all records
-    # tableau_rt_vehicle_positions_lightrail_60_day,
-    tableau_rt_trip_updates_lightrail_60_day,
-    # tableau_rt_vehicle_positions_heavyrail_30_day,
-    tableau_rt_trip_updates_heavyrail_30_day,
-    tableau_devgreen_rt_vehicle_positions_lightrail_60_day,
-    tableau_devgreen_rt_trip_updates_lightrail_60_day,
-    # tableau_rt_vehicle_positions_all_light_rail_7_day,
+    LAMP,
+    S3_ARCHIVE,
+    S3Location,
+    bus_events,
+)
+from lamp_py.tableau.jobs.bus_performance import bus_schema
+from lamp_py.tableau.conversions.convert_bus_performance_data import apply_bus_analysis_conversions
+
+
+from lamp_py.tableau.jobs.lamp_jobs import (
+    LAMP_API_PROJECT,
+    HyperGtfsRtVehiclePositions,
+    HyperGtfsRtTripUpdates,
+    HyperGtfsRtVehiclePositionsHeavyRail,
+    HyperGtfsRtTripUpdatesHeavyRail,
+    HyperGtfsRtVehiclePositionsAllLightRail,
+    HyperDevGreenGtfsRtVehiclePositions,
+    HyperDevGreenGtfsRtTripUpdates,
+    HyperBusOperatorMappingRecent,
+    HyperBusOperatorMappingAll,
+    # HyperRtRailSubway,
+    # HyperRtRailCommuter,
 )
 
-TestHyperDevGreenGtfsRtTripUpdates = FilteredHyperJob(
-    remote_input_location=springboard_devgreen_lrtp_trip_updates,
-    remote_output_location=tableau_devgreen_rt_trip_updates_lightrail_60_day,
-    start_date=days_ago(2),
-    processed_schema=LightRailTerminalTripUpdates.to_pyarrow_schema(),
-    dataframe_filter=convert_gtfs_rt_trip_updates.lrtp_devgreen,
-    parquet_filter=FilterBankRtTripUpdates.ParquetFilter.light_rail,
-    tableau_project_name=GTFS_RT_TABLEAU_PROJECT,
-)
+TestHyperGtfsRtVehiclePositions = HyperGtfsRtVehiclePositions
+TestHyperGtfsRtTripUpdates = HyperGtfsRtTripUpdates
+TestHyperGtfsRtVehiclePositionsHeavyRail = HyperGtfsRtVehiclePositionsHeavyRail
+TestHyperGtfsRtTripUpdatesHeavyRail = HyperGtfsRtTripUpdatesHeavyRail
+TestHyperGtfsRtVehiclePositionsAllLightRail = HyperGtfsRtVehiclePositionsAllLightRail
+TestHyperDevGreenGtfsRtVehiclePositions = HyperDevGreenGtfsRtVehiclePositions
+TestHyperDevGreenGtfsRtTripUpdates = HyperDevGreenGtfsRtTripUpdates
+TestHyperBusOperatorMappingRecent = HyperBusOperatorMappingRecent
+TestHyperBusOperatorMappingAll = HyperBusOperatorMappingAll
+# TestHyperRtRailSubway = HyperRtRailSubway
+# TestHyperRtRailCommuter = HyperRtRailCommuter
+TestHyperBusPerformanceRecent = HyperBusPerformanceRecent
+TestHyperBusPerformanceAll = HyperBusPerformanceAll
 
-TestHyperGtfsRtTripUpdates = FilteredHyperJob(
-    remote_input_location=springboard_rt_trip_updates,
-    remote_output_location=tableau_rt_trip_updates_lightrail_60_day,
-    start_date=days_ago(2),
-    processed_schema=LightRailTerminalTripUpdates.to_pyarrow_schema(),
-    dataframe_filter=None,
-    parquet_filter=FilterBankRtTripUpdates.ParquetFilter.light_rail,
-    tableau_project_name=GTFS_RT_TABLEAU_PROJECT,
-)
 
-TestHyperGtfsRtTripUpdatesHeavyRail = FilteredHyperJob(
-    remote_input_location=springboard_rt_trip_updates,
-    remote_output_location=tableau_rt_trip_updates_heavyrail_30_day,
-    start_date=days_ago(2),
-    processed_schema=HeavyRailTerminalTripUpdates.to_pyarrow_schema(),
-    dataframe_filter=convert_gtfs_rt_trip_updates.heavyrail,
-    parquet_filter=FilterBankRtTripUpdates.ParquetFilter.heavy_rail,
-    tableau_project_name=GTFS_RT_TABLEAU_PROJECT,
-)
+TestHyperGtfsRtVehiclePositions.start_date = days_ago(1)
+TestHyperGtfsRtTripUpdates.start_date = days_ago(1)
+TestHyperGtfsRtVehiclePositionsHeavyRail.start_date = days_ago(1)
+TestHyperGtfsRtTripUpdatesHeavyRail.start_date = days_ago(1)
+TestHyperGtfsRtVehiclePositionsAllLightRail.start_date = days_ago(1)
+TestHyperDevGreenGtfsRtVehiclePositions.start_date = days_ago(1)
+TestHyperDevGreenGtfsRtTripUpdates.start_date = days_ago(1)
+TestHyperBusOperatorMappingRecent.start_date = days_ago(1)
+TestHyperBusOperatorMappingAll.start_date = days_ago(1)
+# TestHyperRtRailSubway.start_date=days_ago(1)
+# TestHyperRtRailCommuter.start_date=days_ago(1)
 
-TestHyperDevGreenGtfsRtVehiclePositions = FilteredHyperJob(
-    remote_input_location=springboard_devgreen_rt_vehicle_positions,
-    remote_output_location=tableau_devgreen_rt_vehicle_positions_lightrail_60_day,
-    start_date=days_ago(3),
-    processed_schema=LightRailTerminalVehiclePositions.to_pyarrow_schema(),
-    dataframe_filter=convert_gtfs_rt_vehicle_position.lrtp,
-    parquet_filter=FilterBankRtVehiclePositions.ParquetFilter.light_rail,
-    tableau_project_name=GTFS_RT_TABLEAU_PROJECT,
+TestHyperBus = FilteredHyperJob(
+    remote_input_location=bus_events,
+    remote_output_location=S3Location(
+        bucket=S3_ARCHIVE, prefix=os.path.join(LAMP, "bus_rating_datasets", "year=2025", "Fall.parquet"), version="1.0"
+    ),
+    start_date=date(2025, 12, 12),
+    end_date=date(2025, 12, 13),
+    processed_schema=bus_schema,
+    dataframe_filter=apply_bus_analysis_conversions,
+    parquet_filter=None,
+    tableau_project_name=LAMP_API_PROJECT,
+    partition_template="{yy}{mm:02d}{dd:02d}.parquet",
 )
 
 
@@ -82,28 +79,32 @@ def start_hyper() -> None:
     """Run all HyperFile Update Jobs"""
 
     hyper_jobs: List[HyperJob] = [
-        HyperBusPerformanceAll(),
-        # HyperBusPerformanceRecent(),
-        # HyperDevGreenGtfsRtTripUpdates,
-        # HyperGtfsRtTripUpdatesHeavyRail,
-        # TestHyperGtfsRtTripUpdatesHeavyRail,
-        # TestHyperGtfsRtTripUpdates,
-        # TestHyperDevGreenGtfsRtTripUpdates,
-        # TestHyperDevGreenGtfsRtVehiclePositions,
+        TestHyperGtfsRtVehiclePositions,
+        TestHyperGtfsRtTripUpdates,
+        TestHyperGtfsRtVehiclePositionsHeavyRail,
+        TestHyperGtfsRtTripUpdatesHeavyRail,
+        TestHyperGtfsRtVehiclePositionsAllLightRail,
+        TestHyperDevGreenGtfsRtVehiclePositions,
+        TestHyperDevGreenGtfsRtTripUpdates,
+        TestHyperBusOperatorMappingRecent,
+        TestHyperBusOperatorMappingAll,
+        TestHyperBus,
+        # TestHyperRtRailSubway,
+        # TestHyperRtRailCommuter,
     ]
 
     local_parquet = True
-    run_pq_remote = True
-    local_hyper = True
+    run_pq_remote = False
+    local_hyper = False
     run_hyper_remote = False
 
     if local_parquet:
         for job in hyper_jobs:
-            try:
-                outs = job.create_parquet(None)
-                shutil.copy(job.local_parquet_path, job.local_hyper_path.replace(".hyper", ".parquet"))
-            except Exception as e:
-                print(f"{job.remote_parquet_path} parquet/local unable to generate - {e}")
+            # try:
+            outs = job.create_parquet(None)
+            shutil.copy(job.local_parquet_path, job.local_hyper_path.replace(".hyper", ".parquet"))
+            # except Exception as e:
+            #     print(f"{job.remote_parquet_path} parquet/local unable to generate - {e}")
 
     if run_pq_remote:
         for job in hyper_jobs:
