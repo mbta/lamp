bf3000d8a6f38cdbc27f5d78ddfb4582705c24df
Corey N. Runkel <crunkel@mbta.com>
2026-01-06 16:21:14 -0500

    Bump data versions for RT rail

--- src/lamp_py/tableau/jobs/lamp_jobs.py
+++ src/lamp_py/tableau/jobs/lamp_jobs.py
@@ -174,7 +174,7 @@ HyperRtRailSubway = HyperRtRail(
     route_type_operand=RouteType.COMMUTER_RAIL,
     hyper_file_name="LAMP_ALL_RT_fields.hyper",
     remote_parquet_path=os.path.join(tableau_rail_subway.s3_uri, "LAMP_ALL_RT_fields.parquet"),
-    lamp_version="1.2.2",
+    lamp_version="1.2.3",
 )
 
 # commuter rail - Enum types == 2 == COMMUTER_RAIL
@@ -183,5 +183,5 @@ HyperRtRailCommuter = HyperRtRail(
     route_type_operand=RouteType.COMMUTER_RAIL,
     hyper_file_name="LAMP_COMMUTER_RAIL_RT_fields.hyper",
     remote_parquet_path=os.path.join(tableau_rail_commuter.s3_uri, "LAMP_COMMUTER_RAIL_RT_fields.parquet"),
-    lamp_version="1.0.0",
+    lamp_version="1.1.0",
 )
