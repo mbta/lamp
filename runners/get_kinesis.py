c3b685b8e20958c7163baeeec3a3563b14c8a7e7
Corey N. Runkel <crunkel@mbta.com>
2026-01-07 13:43:29 -0500

    Update get_kinesis runner to read from prod

--- runners/get_kinesis.py
+++ runners/get_kinesis.py
@@ -2,6 +2,6 @@ from queue import Queue
 from lamp_py.aws.kinesis import KinesisReader
 from lamp_py.ingestion.glides import ingest_glides_events
 
-r = KinesisReader(stream_name="ctd-glides-dev")
+r = KinesisReader(stream_name="ctd-glides-prod")
 
 ingest_glides_events(r, Queue(), upload=False)
