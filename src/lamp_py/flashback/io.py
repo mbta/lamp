7da5a1aa2866f635a04f63ca233737298c379788
Henry Huang <1400827+huangh@users.noreply.github.com>
2026-02-18 23:21:25 -0500

    wip write out both new schema and old schema for vehicle positions to compare - ensure the same API read is used to write both for apples to apples

--- src/lamp_py/flashback/io.py
+++ src/lamp_py/flashback/io.py
@@ -4,8 +4,8 @@ import dataframely as dy
 import polars as pl
 from aiohttp import ClientError, ClientSession
 
-from lamp_py.flashback.events import StopEventsJSON, StopEventsTable
-from lamp_py.ingestion.convert_gtfs_rt import VehiclePositions
+from lamp_py.flashback.events import StopEventsJSON, StopEventsWithStatus, StopEventsTable, unnest_vehicle_positions, unnest_vehicle_positions_new
+from lamp_py.ingestion.convert_gtfs_rt import VehiclePositions, VehiclePositionsApiFormat
 from lamp_py.runtime_utils.process_logger import ProcessLogger
 from lamp_py.runtime_utils.remote_files import S3Location
 from lamp_py.runtime_utils.remote_files import stop_events as stop_events_location
@@ -38,6 +38,34 @@ def get_remote_events(location: S3Location = stop_events_location) -> dy.DataFra
 
     return existing_events
 
+async def get_vehicle_positions_new(
+    url: str = "https://cdn.mbta.com/realtime/VehiclePositions_enhanced.json",
+    sleep_interval: int = 3,
+    max_retries: int = 10,
+) -> tuple[dy.DataFrame[StopEventsWithStatus], dy.DataFrame[VehiclePositions]]:
+    """Fetch the latest VehiclePositions data."""
+    process_logger = ProcessLogger("get_vehicle_positions", url=url)
+    process_logger.log_start()
+
+    async with ClientSession() as session:
+        for attempt in range(max_retries + 1):
+            try:
+                async with session.get(url) as response:
+                    response.raise_for_status()
+                    data = await response.read()
+                    break
+            except ClientError as e:
+                process_logger.log_failure(e)
+                if attempt == max_retries:
+                    raise ClientError(f"Maximum retries ({max_retries}) exceeded") from e
+                sleep(sleep_interval)
+
+    vehicle_positions = pl.read_ndjson(data, schema=VehiclePositionsApiFormat.to_polars_schema())
+    valid = process_logger.log_dataframely_filter_results(*VehiclePositionsApiFormat.filter(vehicle_positions))
+    
+    process_logger.log_complete()
+
+    return unnest_vehicle_positions_new(valid), unnest_vehicle_positions(valid)
 
 async def get_vehicle_positions(
     url: str = "https://cdn.mbta.com/realtime/VehiclePositions_enhanced.json",
@@ -67,12 +95,13 @@ async def get_vehicle_positions(
 
     process_logger.log_complete()
 
-    return valid
+    return unnest_vehicle_positions(valid)
 
 
-def write_stop_events(stop_events: dy.DataFrame[StopEventsJSON], location: S3Location = stop_events_location) -> None:
+def write_stop_events(stop_events: dy.DataFrame[StopEventsJSON], location: S3Location = stop_events_location, local_override: str | None = None) -> None:
     """Write stop events to specified location."""
-    process_logger = ProcessLogger("write_stop_events", s3_uri=location.s3_uri)
+    output_path = local_override or location.s3_uri
+    process_logger = ProcessLogger("write_stop_events", s3_uri=output_path)
     process_logger.log_start()
-    stop_events.write_parquet(location.s3_uri, compression_level=9, retries=5, use_pyarrow=True)
+    stop_events.write_json(output_path)
     process_logger.log_complete()
