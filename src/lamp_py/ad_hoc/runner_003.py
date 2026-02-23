c3b59efc660f8512cc376be8fb391539ac8e3431
Corey N. Runkel <crunkel@mbta.com>
2026-02-23 12:14:33 -0500

    Rename existing springboard LRTP files to TP files

--- src/lamp_py/ad_hoc/runner_003.py
+++ src/lamp_py/ad_hoc/runner_003.py
@@ -1,31 +1,32 @@
-from lamp_py.runtime_utils.process_logger import ProcessLogger
-from lamp_py.runtime_utils.remote_files import LAMP
-from lamp_py.runtime_utils.remote_files import S3_SPRINGBOARD
 from lamp_py.aws.s3 import file_list_from_s3, get_s3_client
+from lamp_py.runtime_utils.process_logger import ProcessLogger
+from lamp_py.runtime_utils.remote_files import LAMP, S3_SPRINGBOARD
 
 
 def runner() -> None:
-    """
-    Rename springboard datasets with LRTP to TP.
-    """
+    """Rename springboard datasets with LRTP to TP."""
     ad_hoc_logger = ProcessLogger(process_name="ad_hoc_runner")
     ad_hoc_logger.log_start()
 
     lrtp_file_list = file_list_from_s3(
         S3_SPRINGBOARD,
-        f"{LAMP}/*LRTP*/**",
+        f"{LAMP}/DEV_GREEN_LRTP",
+    ) + file_list_from_s3(
+        S3_SPRINGBOARD,
+        f"{LAMP}/LRTP",
     )
 
-    ad_hoc_logger.add_metadata(lrtp_files = len(lrtp_file_list))
+    ad_hoc_logger.add_metadata(lrtp_files=len(lrtp_file_list))
 
     s3 = get_s3_client()
 
     for file in lrtp_file_list:
-        s3.copy(
-            {
-                "Bucket": S3_SPRINGBOARD,
-                "Key": file
-            },
-            S3_SPRINGBOARD,
-            file.replace("LRTP", "TP")
-        )
+        new_key = file.replace("LRTP", "TP")
+        s3.copy({"Bucket": S3_SPRINGBOARD, "Key": file}, S3_SPRINGBOARD, new_key)
+        ad_hoc_logger.add_metadata(original_key=file, new_key=new_key)
+
+    # move all file before deleting originals
+
+    for file in lrtp_file_list:
+        s3.delete_object(Bucket=S3_SPRINGBOARD, Key=file)
+        ad_hoc_logger.add_metadata(deleted_key=file)
