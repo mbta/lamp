0c1e91e1ea410792823bbe6b9a34ee78247b49af
Corey N. Runkel <crunkel@mbta.com>
2026-02-23 13:55:55 -0500

    Exclude today's files from copying/deleting

--- src/lamp_py/ad_hoc/runner_003.py
+++ src/lamp_py/ad_hoc/runner_003.py
@@ -1,4 +1,5 @@
-from lamp_py.aws.s3 import file_list_from_s3, get_s3_client
+from datetime import datetime, timedelta, timezone
+from lamp_py.aws.s3 import file_list_from_s3_with_details, get_s3_client
 from lamp_py.runtime_utils.process_logger import ProcessLogger
 from lamp_py.runtime_utils.remote_files import LAMP, S3_SPRINGBOARD
 
@@ -8,10 +9,10 @@ def runner(delete: bool = True) -> None:
     ad_hoc_logger = ProcessLogger(process_name="ad_hoc_runner")
     ad_hoc_logger.log_start()
 
-    lrtp_file_list = file_list_from_s3(
+    lrtp_file_list = file_list_from_s3_with_details(
         S3_SPRINGBOARD,
         f"{LAMP}/DEV_GREEN_LRTP",
-    ) + file_list_from_s3(
+    ) + file_list_from_s3_with_details(
         S3_SPRINGBOARD,
         f"{LAMP}/LRTP",
     )
@@ -21,13 +22,22 @@ def runner(delete: bool = True) -> None:
     s3 = get_s3_client()
 
     for file in lrtp_file_list:
-        existing_key = file.replace(f"s3://{S3_SPRINGBOARD}/", "")
-        new_key = existing_key.replace("LRTP", "TP")
-        s3.copy({"Bucket": S3_SPRINGBOARD, "Key": existing_key}, S3_SPRINGBOARD, new_key)
-        ad_hoc_logger.add_metadata(original_key=existing_key, new_key=new_key)
+        existing_key = file["s3_obj_path"].replace(f"s3://{S3_SPRINGBOARD}/", "")
+        if datetime.now(timezone.utc) - file["last_modified"] > timedelta(
+            hours=6
+        ):  # skip anything modified today to avoid overwriting unfinished files
+            new_key = existing_key.replace("LRTP", "TP")
+            s3.copy({"Bucket": S3_SPRINGBOARD, "Key": existing_key}, S3_SPRINGBOARD, new_key)
+            ad_hoc_logger.add_metadata(original_key=existing_key, new_key=new_key)
+        else:
+            ad_hoc_logger.add_metadata(skipped_key=existing_key, reason="File is not old enough to move")
 
     # move all file before deleting originals
     if delete:
         for file in lrtp_file_list:
-            s3.delete_object(Bucket=S3_SPRINGBOARD, Key=file)
-            ad_hoc_logger.add_metadata(deleted_key=file)
+            existing_key = file["s3_obj_path"].replace(f"s3://{S3_SPRINGBOARD}/", "")
+            if datetime.now(timezone.utc) - file["last_modified"] > timedelta(hours=6):
+                s3.delete_object(Bucket=S3_SPRINGBOARD, Key=existing_key)
+                ad_hoc_logger.add_metadata(deleted_key=existing_key)
+            else:
+                ad_hoc_logger.add_metadata(skipped_deletion_key=existing_key, reason="File is not old enough to delete")
