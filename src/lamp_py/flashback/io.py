b9662ab3563f7db29e08c285b92b30856a0d65db
Henry Huang <1400827+huangh@users.noreply.github.com>
2026-02-25 04:39:15 -0500

    remove write_stop_events - function can be consolidated in lambda

--- src/lamp_py/flashback/io.py
+++ src/lamp_py/flashback/io.py
@@ -4,7 +4,13 @@ import dataframely as dy
 import polars as pl
 from aiohttp import ClientError, ClientSession
 
-from lamp_py.flashback.events import StopEventsJSON, StopEventsWithStatus, StopEventsTable, unnest_vehicle_positions, unnest_vehicle_positions_new
+from lamp_py.flashback.events import (
+    StopEventsJSON,
+    StopEventsWithStatus,
+    StopEventsTable,
+    unnest_vehicle_positions,
+    unnest_vehicle_positions_new,
+)
 from lamp_py.ingestion.convert_gtfs_rt import VehiclePositions, VehiclePositionsApiFormat
 from lamp_py.runtime_utils.process_logger import ProcessLogger
 from lamp_py.runtime_utils.remote_files import S3Location
@@ -40,11 +46,12 @@ def get_remote_events(location: S3Location = stop_events_location) -> dy.DataFra
 
     return existing_events
 
-async def get_vehicle_positions_new(
+
+async def get_vehicle_positions(
     url: str = "https://cdn.mbta.com/realtime/VehiclePositions_enhanced.json",
     sleep_interval: int = 3,
     max_retries: int = 10,
-) -> tuple[dy.DataFrame[StopEventsWithStatus], dy.DataFrame[VehiclePositions]]:
+) -> dy.DataFrame[StopEventsWithStatus]:
     """Fetch the latest VehiclePositions data."""
     process_logger = ProcessLogger("get_vehicle_positions", url=url)
     process_logger.log_start()
@@ -64,18 +71,19 @@ async def get_vehicle_positions_new(
 
     vehicle_positions = pl.read_ndjson(data, schema=VehiclePositionsApiFormat.to_polars_schema())
     valid = process_logger.log_dataframely_filter_results(*VehiclePositionsApiFormat.filter(vehicle_positions))
-    
+
     process_logger.log_complete()
 
-    return unnest_vehicle_positions_new(valid), unnest_vehicle_positions(valid)
+    return unnest_vehicle_positions_new(valid)
 
-async def get_vehicle_positions(
+
+async def get_vehicle_positions_old(
     url: str = "https://cdn.mbta.com/realtime/VehiclePositions_enhanced.json",
     sleep_interval: int = 3,
     max_retries: int = 10,
 ) -> dy.DataFrame[VehiclePositions]:
     """Fetch the latest VehiclePositions data."""
-    process_logger = ProcessLogger("get_vehicle_positions", url=url)
+    process_logger = ProcessLogger("get_vehicle_positions_old", url=url)
     process_logger.log_start()
 
     async with ClientSession() as session:
@@ -98,12 +106,3 @@ async def get_vehicle_positions(
     process_logger.log_complete()
 
     return unnest_vehicle_positions(valid)
-
-
-def write_stop_events(stop_events: dy.DataFrame[StopEventsJSON], location: S3Location = stop_events_location, local_override: str | None = None) -> None:
-    """Write stop events to specified location."""
-    output_path = local_override or location.s3_uri
-    process_logger = ProcessLogger("write_stop_events", s3_uri=output_path)
-    process_logger.log_start()
-    stop_events.write_json(output_path)
-    process_logger.log_complete()
