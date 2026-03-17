6dff58692a7324b31050d6fb8152583667549ffb
Henry Huang <1400827+huangh@users.noreply.github.com>
2026-03-17 06:43:06 -0400

    move lightrailgps to correct partitioning scheme

--- src/lamp_py/publishing/lightswitch.py
+++ src/lamp_py/publishing/lightswitch.py
@@ -24,8 +24,8 @@ HIVE_VIEWS = {
         rf.tm_stop_crossing,
         rf.tm_daily_work_piece,
         rf.tm_daily_logged_message,
-        rf.light_rail_gps,
     ],
+    "/*/*.parquet": [rf.light_rail_gps],
     "": [  # files
         rf.tm_daily_sched_adherence_waiver_file,
         rf.tm_geo_node_file,
