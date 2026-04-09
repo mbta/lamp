777d243888ea508898068e85ffd781ea1c30f199
Corey N. Runkel <crunkel@mbta.com>
2026-04-09 14:54:36 -0400

    Refactor Glides schemas with Dataframely & polars

--- src/lamp_py/tableau/jobs/glides.py
+++ src/lamp_py/tableau/jobs/glides.py
@@ -1,14 +1,13 @@
 from typing import Optional
 
 import pyarrow
-import pyarrow.parquet as pq
-import pyarrow.dataset as pd
-from pyarrow.fs import S3FileSystem
+import dataframely as dy
 
 import polars as pl
 
 from lamp_py.tableau.hyper import HyperJob
 from lamp_py.postgres.postgres_utils import DatabaseManager
+from lamp_py.ingestion.glides import TripUpdatesTable, OperatorSignInsTable
 from lamp_py.runtime_utils.remote_files import (
     glides_trips_updated,
     glides_operator_signed_in,
@@ -19,75 +18,21 @@ from lamp_py.aws.s3 import file_list_from_s3
 
 GLIDES_TABLEAU_PROJECT = "Glides"
 
-glides_trips_updated_schema = pyarrow.schema(
-    [
-        ("data.metadata.location.gtfsId", pyarrow.large_string()),
-        ("data.metadata.location.todsId", pyarrow.large_string()),
-        ("data.metadata.author.emailAddress", pyarrow.large_string()),
-        ("data.metadata.author.badgeNumber", pyarrow.large_string()),
-        ("data.metadata.inputType", pyarrow.large_string()),
-        ("data.metadata.inputTimestamp", pyarrow.timestamp("ms")),  # yyyy-mm-ddT hh:mm:ssZ str (UTC) -> Datetime (EST)
-        ("id", pyarrow.large_string()),
-        ("type", pyarrow.large_string()),
-        ("time", pyarrow.timestamp("ms")),  # yyyy-mm-ddT hh:mm:ssZ timestamp (UTC) -> Datetime (EST)
-        ("source", pyarrow.large_string()),
-        ("specversion", pyarrow.large_string()),
-        ("dataschema", pyarrow.large_string()),
-        ("data.tripUpdates.previousTripKey.serviceDate", pyarrow.date32()),  # YYYY-MM-DD str -> date
-        ("data.tripUpdates.previousTripKey.tripId", pyarrow.large_string()),
-        ("data.tripUpdates.previousTripKey.startLocation.gtfsId", pyarrow.large_string()),
-        ("data.tripUpdates.previousTripKey.startLocation.todsId", pyarrow.large_string()),
-        ("data.tripUpdates.previousTripKey.endLocation.gtfsId", pyarrow.large_string()),
-        ("data.tripUpdates.previousTripKey.endLocation.todsId", pyarrow.large_string()),
-        ("data.tripUpdates.previousTripKey.startTime", pyarrow.large_string()),  # HH:MM:SS str -> time
-        ("data.tripUpdates.previousTripKey.endTime", pyarrow.large_string()),  # HH:MM:SS str -> time
-        ("data.tripUpdates.previousTripKey.revenue", pyarrow.large_string()),
-        ("data.tripUpdates.previousTripKey.glidesId", pyarrow.large_string()),
-        ("data.tripUpdates.type", pyarrow.large_string()),
-        ("data.tripUpdates.tripKey.serviceDate", pyarrow.date32()),  # YYYY-MM-DD str -> date
-        ("data.tripUpdates.tripKey.tripId", pyarrow.large_string()),
-        ("data.tripUpdates.tripKey.startLocation.gtfsId", pyarrow.large_string()),
-        ("data.tripUpdates.tripKey.startLocation.todsId", pyarrow.large_string()),
-        ("data.tripUpdates.tripKey.endLocation.gtfsId", pyarrow.large_string()),
-        ("data.tripUpdates.tripKey.endLocation.todsId", pyarrow.large_string()),
-        ("data.tripUpdates.tripKey.startTime", pyarrow.large_string()),  # HH:MM:SS str -> time
-        ("data.tripUpdates.tripKey.endTime", pyarrow.large_string()),  # HH:MM:SS str -> time
-        ("data.tripUpdates.tripKey.revenue", pyarrow.large_string()),
-        ("data.tripUpdates.tripKey.glidesId", pyarrow.large_string()),
-        ("data.tripUpdates.comment", pyarrow.large_string()),
-        ("data.tripUpdates.startLocation.gtfsId", pyarrow.large_string()),
-        ("data.tripUpdates.startLocation.todsId", pyarrow.large_string()),
-        ("data.tripUpdates.endLocation.gtfsId", pyarrow.large_string()),
-        ("data.tripUpdates.endLocation.todsId", pyarrow.large_string()),
-        ("data.tripUpdates.startTime", pyarrow.large_string()),  # HH:MM:SS str -> time
-        ("data.tripUpdates.endTime", pyarrow.large_string()),  # HH:MM:SS str -> time
-        ("data.tripUpdates.cars", pyarrow.large_string()),
-        ("data.tripUpdates.revenue", pyarrow.large_string()),
-        ("data.tripUpdates.dropped", pyarrow.large_string()),
-        ("data.tripUpdates.scheduled", pyarrow.large_string()),
-    ]
-)
 
-glides_operator_signed_in_schema = pyarrow.schema(
-    [
-        ("data.metadata.location.gtfsId", pyarrow.large_string()),
-        ("data.metadata.location.todsId", pyarrow.large_string()),
-        ("data.metadata.author.emailAddress", pyarrow.large_string()),
-        ("data.metadata.author.badgeNumber", pyarrow.large_string()),
-        ("data.metadata.inputType", pyarrow.large_string()),
-        ("data.metadata.inputTimestamp", pyarrow.timestamp("ms")),  # yyyy-mm-ddT hh:mm:ssZ str (UTC) -> Datetime (EST)
-        ("data.operator.badgeNumber", pyarrow.large_string()),
-        ("data.signedInAt", pyarrow.timestamp("ms")),  # yyyy-mm-ddT hh:mm:ssZ str (UTC) -> Datetime (EST)
-        ("data.signature.type", pyarrow.large_string()),
-        ("data.signature.version", pyarrow.int16()),
-        ("id", pyarrow.large_string()),
-        ("type", pyarrow.large_string()),
-        ("time", pyarrow.timestamp("ms")),  # yyyy-mm-ddT hh:mm:ssZ timestamp (UTC) -> Datetime (EST)
-        ("source", pyarrow.large_string()),
-        ("specversion", pyarrow.large_string()),
-        ("dataschema", pyarrow.large_string()),
-    ]
-)
+class TripUpdatesTableau(TripUpdatesTable):  # type: ignore[misc, valid-type]
+    """Glides Trip Updates data, transformed for Tableau consumption."""
+
+    input_timestamp = dy.Datetime(time_unit="ms", alias="data.metadata.inputTimestamp", nullable=True)
+    previous_trip_service_date = dy.Date(alias="data.tripUpdates.previousTripKey.serviceDate", nullable=True)
+    trip_service_date = dy.Date(alias="data.tripUpdates.tripKey.serviceDate", nullable=True)
+
+
+class OperatorSignInsTableau(OperatorSignInsTable):  # type: ignore[misc, valid-type]
+    """Glides Operator Sign-Ins data, transformed for Tableau consumption."""
+
+    input_timestamp = dy.Datetime(time_unit="ms", alias="data.metadata.inputTimestamp", nullable=True)
+    signed_in_at = dy.Datetime(time_unit="ms", alias="data.signedInAt", nullable=True)
+    time = dy.Datetime(time_unit="ms", alias="time", nullable=True)
 
 
 def create_trips_updated_glides_parquet(job: HyperJob, num_files: Optional[int]) -> None:
@@ -95,40 +40,25 @@ def create_trips_updated_glides_parquet(job: HyperJob, num_files: Optional[int])
     Grab the glides_events files and process
     """
     s3_uris = file_list_from_s3(bucket_name=glides_trips_updated.bucket, file_prefix=glides_trips_updated.prefix)
-    ds_paths = [s.replace("s3://", "") for s in s3_uris]
 
     if num_files is not None:
-        ds_paths = ds_paths[-num_files:]
+        s3_uris = s3_uris[-num_files:]
 
-    ds = pd.dataset(
-        ds_paths,
-        format="parquet",
-        filesystem=S3FileSystem(),
+    ds = (
+        pl.scan_parquet(s3_uris)
+        .with_columns(
+            pl.col("data.metadata.inputTimestamp")
+            .str.strptime(pl.Datetime("ms"), "%Y-%m-%dT%H:%M:%SZ", strict=False)
+            .dt.convert_time_zone(time_zone="US/Eastern")
+            .dt.replace_time_zone(None),
+            pl.col("time").dt.convert_time_zone(time_zone="US/Eastern").dt.replace_time_zone(None),
+            pl.col("data.tripUpdates.previousTripKey.serviceDate").str.to_date("%Y-%m-%d", strict=False),
+            pl.col("data.tripUpdates.tripKey.serviceDate").str.to_date("%Y-%m-%d", strict=False),
+        )
+        .select(TripUpdatesTableau.column_names())
     )
 
-    with pq.ParquetWriter(job.local_parquet_path, schema=job.output_processed_schema) as writer:
-        for batch in ds.to_batches(batch_size=500_000):
-            polars_df = pl.from_arrow(batch)
-            if not isinstance(polars_df, pl.DataFrame):
-                raise TypeError(f"Expected a Polars DataFrame or Series, but got {type(polars_df)}")
-            polars_df = polars_df.with_columns(
-                pl.col("data.metadata.inputTimestamp")
-                .str.strptime(pl.Datetime("ms"), "%Y-%m-%dT%H:%M:%SZ", strict=False)
-                .dt.convert_time_zone(time_zone="US/Eastern")
-                .dt.replace_time_zone(None),
-                pl.col("time").dt.convert_time_zone(time_zone="US/Eastern").dt.replace_time_zone(None),
-                # all of these service dates and start/end times are already EST - don't do any conversions
-                pl.col("data.tripUpdates.previousTripKey.serviceDate").str.to_date("%Y-%m-%d", strict=False),
-                # pl.col("data.tripUpdates.previousTripKey.startTime").str.to_time("%H:%M:%S", strict=False),
-                # pl.col("data.tripUpdates.previousTripKey.endTime").str.to_time("%H:%M:%S", strict=False),
-                pl.col("data.tripUpdates.tripKey.serviceDate").str.to_date("%Y-%m-%d", strict=False),
-                # pl.col("data.tripUpdates.tripKey.startTime").str.to_time("%H:%M:%S", strict=False),
-                # pl.col("data.tripUpdates.tripKey.endTime").str.to_time("%H:%M:%S", strict=False),
-                # pl.col("data.tripUpdates.startTime").str.to_time("%H:%M:%S", strict=False),
-                # pl.col("data.tripUpdates.endTime").str.to_time("%H:%M:%S", strict=False),
-            )
-
-            writer.write_table(polars_df.select(job.output_processed_schema.names).to_arrow())
+    TripUpdatesTableau.validate(ds, cast=True).write_parquet(job.local_parquet_path)
 
 
 def create_operator_signed_in_glides_parquet(job: HyperJob, num_files: Optional[int]) -> None:
@@ -138,35 +68,27 @@ def create_operator_signed_in_glides_parquet(job: HyperJob, num_files: Optional[
     s3_uris = file_list_from_s3(
         bucket_name=glides_operator_signed_in.bucket, file_prefix=glides_operator_signed_in.prefix
     )
-    ds_paths = [s.replace("s3://", "") for s in s3_uris]
 
     if num_files is not None:
-        ds_paths = ds_paths[-num_files:]
+        s3_uris = s3_uris[-num_files:]
 
-    ds = pd.dataset(
-        ds_paths,
-        format="parquet",
-        filesystem=S3FileSystem(),
+    ds = (
+        pl.scan_parquet(s3_uris)
+        .with_columns(
+            pl.col("data.metadata.inputTimestamp")
+            .str.strptime(pl.Datetime("ms"), "%Y-%m-%dT%H:%M:%SZ", strict=False)
+            .dt.convert_time_zone(time_zone="US/Eastern")
+            .dt.replace_time_zone(None),
+            pl.col("data.signedInAt")
+            .str.strptime(pl.Datetime("ms"), "%Y-%m-%dT%H:%M:%SZ", strict=False)
+            .dt.convert_time_zone(time_zone="US/Eastern")
+            .dt.replace_time_zone(None),
+            pl.col("time").dt.convert_time_zone(time_zone="US/Eastern").dt.replace_time_zone(None),
+        )
+        .select(OperatorSignInsTableau.column_names())
     )
 
-    with pq.ParquetWriter(job.local_parquet_path, schema=job.output_processed_schema) as writer:
-        for batch in ds.to_batches(batch_size=500_000):
-            polars_df = pl.from_arrow(batch)
-            if not isinstance(polars_df, pl.DataFrame):
-                raise TypeError(f"Expected a Polars DataFrame or Series, but got {type(polars_df)}")
-            polars_df = polars_df.with_columns(
-                pl.col("data.metadata.inputTimestamp")
-                .str.strptime(pl.Datetime(time_unit="ms"), "%Y-%m-%dT%H:%M:%SZ", strict=False)
-                .dt.convert_time_zone(time_zone="US/Eastern")
-                .dt.replace_time_zone(None),
-                pl.col("data.signedInAt")
-                .str.strptime(pl.Datetime(time_unit="ms"), "%Y-%m-%dT%H:%M:%SZ", strict=False)
-                .dt.convert_time_zone(time_zone="US/Eastern")
-                .dt.replace_time_zone(None),
-                pl.col("time").dt.convert_time_zone(time_zone="US/Eastern").dt.replace_time_zone(None),
-            )
-
-            writer.write_table(polars_df.select(job.output_processed_schema.names).to_arrow())
+    OperatorSignInsTableau.validate(ds, cast=True).write_parquet(job.local_parquet_path)
 
 
 class HyperGlidesTripUpdates(HyperJob):
@@ -183,7 +105,9 @@ class HyperGlidesTripUpdates(HyperJob):
 
     @property
     def output_processed_schema(self) -> pyarrow.schema:
-        return glides_trips_updated_schema
+        return pyarrow.schema(
+            [("foo", pyarrow.string())]
+        )  # trivial schema to satisfy the HyperJob interface; the actual schema is determined by dataframely schema specified in the function
 
     def create_parquet(self, _: DatabaseManager | None) -> None:
         self.update_parquet(None)
@@ -208,7 +132,9 @@ class HyperGlidesOperatorSignIns(HyperJob):
 
     @property
     def output_processed_schema(self) -> pyarrow.schema:
-        return glides_operator_signed_in_schema
+        return pyarrow.schema(
+            [("foo", pyarrow.string())]
+        )  # trivial schema to satisfy the HyperJob interface; the actual schema is determined by dataframely schema specified in the function
 
     def create_parquet(self, _: DatabaseManager | None) -> None:
         self.update_parquet(None)
--- tests/tableau/test_glides.py
+++ tests/tableau/test_glides.py
@@ -5,7 +5,6 @@ from pathlib import Path
 import dataframely as dy
 import polars as pl
 import pytest
-from pyarrow.dataset import Dataset
 from pytest_mock import MockerFixture
 
 from lamp_py.ingestion.glides import GlidesRecord, OperatorSignInsTable, TripUpdatesTable
@@ -26,14 +25,14 @@ def test_glides_hyper_job(
     job: HyperJob,
     schema: GlidesRecord,
     raises: pytest.RaisesExc,
-    monkeypatch: pytest.MonkeyPatch,
     mocker: MockerFixture,
     dy_gen: dy.random.Generator,
     tmp_path: Path,
     num_records: int = 10,
 ) -> None:
     """It creates a compatible Parquet file."""
-    sample_data = (
+    springboard_path = tmp_path / "springboard.parquet"
+    (
         schema.sample(
             num_records,
             generator=dy_gen,
@@ -42,18 +41,13 @@ def test_glides_hyper_job(
                     num_records, min=datetime(2024, 1, 1), max=datetime(2039, 12, 31), time_unit="us"
                 ).cast(pl.Datetime(time_unit="ms"))
             },
-        )
-        .to_arrow()
-        .to_batches()
+        ).write_parquet(springboard_path.as_posix())
     )
 
-    monkeypatch.setattr("lamp_py.aws.s3.file_list_from_s3", ["foo", "bar"])
-    mock_dataset = mocker.MagicMock(Dataset)
-    mock_dataset.to_batches.return_value = sample_data
-    mocker.patch("pyarrow.dataset.dataset", return_value=mock_dataset)
+    mocker.patch("lamp_py.tableau.jobs.glides.file_list_from_s3", return_value=[springboard_path.as_uri()])
 
     test_path = tmp_path.joinpath("test.parquet")
-    monkeypatch.setattr(job, "local_parquet_path", test_path)
+    mocker.patch.object(job, "local_parquet_path", test_path.as_posix())
 
     with raises:
         job.create_parquet(None)
