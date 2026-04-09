99cb09d0f808e1f7c902add4e8f8aa1859b521de
Henry Huang <1400827+huangh@users.noreply.github.com>
2026-04-09 06:02:27 -0400

    temp bump to 12pm to see things happen

--- src/lamp_py/ingestion/daily/config.py
+++ src/lamp_py/ingestion/daily/config.py
@@ -1,2 +1,2 @@
 START_HOUR = 1  # 1 am UTC, 9 pm EST - after rollover of ingestion dataset.
-END_HOUR = 7  # 7 am UTC, 3 am EST - before start of next day.
+END_HOUR = 12  # 7 am UTC, 3 am EST - before start of next day.
