38011ce6b2abbf34c251cf22a5bcd9c40d18665e
Corey N. Runkel <crunkel@mbta.com>
2026-04-13 14:02:51 -0400

    Update layout

--- analysis/flashback_performance.py
+++ analysis/flashback_performance.py
@@ -1,7 +1,7 @@
 import marimo
 
 __generated_with = "0.16.5"
-app = marimo.App()
+app = marimo.App(layout_file="layouts/flashback_performance.grid.json")
 
 
 @app.cell
@@ -113,12 +113,16 @@ def _(flashback_horizon, pl, unique_dates):
     return (vp,)
 
 
+@app.cell
+def _(mo):
+    mo.md(r"""## Coverage""")
+    return
+
+
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(
         r"""
-    ## Coverage
-
     If an event happens, will I see it?
 
     $$
@@ -129,6 +133,11 @@ def _(mo):
     {MissingEvents}
     {TotalEvents}
     $$
+
+    - ✅ if 99% or greater
+    - ⚠️ if 97–98%
+    - ❌ if less than 97%
+
 
     We can approximate total events as the events that appear in our `VehiclePositions` archive with date, trip, route, and vehicle information. We expect arrivals for every stop after the first stop on a trip and departures for every stop before the last stop on a trip.
     """
@@ -163,36 +172,38 @@ def _(missing_events, plausible_events):
     return (missing,)
 
 
+@app.function
+def match_threshold(value: int | float, thresholds: dict[int | float, str]) -> str:
+    for threshold in sorted(thresholds.keys(), reverse=True):
+        if value >= threshold:
+            return thresholds[threshold]
+    raise NotImplementedError
+
+
+@app.cell
+def _():
+    coverage_thresholds = {0.99: "✅", 0.97: "⚠️", 0: "❌"}
+    return (coverage_thresholds,)
+
+
+@app.cell
+def _(missing):
+    coverage = 1 - missing
+    return (coverage,)
+
+
 @app.cell(hide_code=True)
-def _(missing, mo):
+def _(coverage, coverage_thresholds, mo):
     mo.stat(
-        value=f"{1 - missing:.1%}",
+        value=f"{match_threshold(coverage, coverage_thresholds)} {coverage:.1%}",
         caption=f"of events within the last 2 hours",
-    )
-    return
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(
-        r"""
-    ## Performance
-
-    How quickly can we process an event after it appears in `VehiclePositions`?
-
-    $$
-    performance = timestamp_{Flashback} - timestamp_{VehiclePositions}
-    $$
-
-    [Splunk captures this data as `duration` on an ongoing basis](https://mbta.splunkcloud.com/en-US/app/search/search?q=search%20index%3Dlamp-*%20process_name%3Dflashback%20status%3Dcomplete%20%7C%20bin%20duration%20span%3D3%20as%20duration%20%7C%20stats%20count(_raw)%20BY%20index%2C%20duration%20%7C%20sort%20index%2C%20duration&display.page.search.mode=verbose&dispatch.sample_ratio=1&workload_pool=&earliest=-24h%40h&latest=now&display.page.search.tab=statistics&display.general.type=statistics).
-    """
     )
     return
 
 
 @app.cell
 def _(mo):
-    mo.stat("99.96%", caption="of updates are processed within 4 seconds")
+    mo.md(r"""## Accuracy""")
     return
 
 
@@ -200,8 +211,6 @@ def _(mo):
 def _(mo):
     mo.md(
         r"""
-    ## Accuracy
-
     When I see an event, does it have the correct arrival and departure times?
 
     $$
@@ -209,6 +218,10 @@ def _(mo):
     $$
 
     To define correct, we'll use our less performant `VehiclePositions` dataset again. Since these 2 datasets poll the `VehiclePositions` data at different frequencies, they may observe different timestamps from vehicles. As such, we should tolerate a small difference in those timestamps.
+
+    - ✅ if 99% of timestamps are within 15 seconds
+    - ⚠️ if 99% of timestamps are within 30 seconds
+    - ❌ if less than 99% of timestamps are within 30 seconds
     """
     )
     return
@@ -229,16 +242,29 @@ def _(StopEvents, pl, plausible_events, stop_events):
             accuracy=pl.col("value").sub(pl.col("value_right")).abs().quantile(0.99).dt.total_seconds(fractional=False)
         )
         .collect()
+        .row()[0]
     )
     return (event_accuracy,)
 
 
 @app.cell
-def _(event_accuracy, mo):
+def _():
+    accuracy_thresholds = {1_000: "❌", 16: "⚠️", 0: "✅"}
+    return (accuracy_thresholds,)
+
+
+@app.cell
+def _(accuracy_thresholds, event_accuracy, mo):
     mo.stat(
-        f"{event_accuracy.row()[0]} seconds",
-        caption="99th percentile difference between observed and actual event timestamps",
+        label="99% of events have a timestamp difference of",
+        value=f"{match_threshold(event_accuracy, accuracy_thresholds)} {event_accuracy} seconds or less",
     )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""## Lag""")
     return
 
 
@@ -246,15 +272,17 @@ def _(event_accuracy, mo):
 def _(mo):
     mo.md(
         r"""
-    ## Latency
-
     After an event occurs, how long do I need to wait before I see it?
 
     $$
-    latency = timestamp_{publication} - timestamp_{event}
+    lag = timestamp_{publication} - timestamp_{event}
     $$
 
-    This metric is meaningful—but is mostly outside our control: after an event occurs, its data must travel through source and TID systems before Flashback processes it.
+    This metric is meaningful—but is **mostly outside our control and our responsibility**: after an event occurs, its data must travel through source and TID systems before Flashback processes it.
+
+    - ✅ if 99% of events appear within 15 seconds
+    - ⚠️ if 99% of events appear within 30 seconds
+    - ❌ if less than 99% of events appear within 30 seconds
     """
     )
     return
@@ -278,33 +306,72 @@ def _(pl, stop_events):
 
 @app.cell
 def _(event_lag, pl):
-    arrival_lag = (
-        event_lag.filter(pl.col("event_lag") <= pl.duration(seconds=1), pl.col("most_recent_event") == "arrived").height
-        / event_lag.filter(pl.col("most_recent_event") == "arrived").height
-    )
-    return (arrival_lag,)
+    lag = event_lag.group_by("most_recent_event").agg(pl.quantile("event_lag", 0.99).dt.total_seconds()).rows()
+    return (lag,)
 
 
 @app.cell
-def _(arrival_lag, mo):
-    mo.stat(value=f"{arrival_lag:.1%}", caption="of observed arrivals published within 15 seconds of arriving")
+def _():
+    lag_thresholds = {30: "❌", 16: "⚠️", 0: "✅"}
+    return (lag_thresholds,)
+
+
+@app.cell
+def _(lag, lag_thresholds, mo):
+    mo.stat(
+        label="99% of departures are available within",
+        value=f"{match_threshold(lag[0][1], lag_thresholds)} {lag[0][1]} seconds",
+    )
     return
 
 
 @app.cell
-def _(event_lag, pl):
-    departure_lag = (
-        event_lag.filter(
-            pl.col("event_lag") <= pl.duration(seconds=15), pl.col("most_recent_event") == "departed"
-        ).height
-        / event_lag.filter(pl.col("most_recent_event") == "departed").height
+def _(lag, lag_thresholds, mo):
+    mo.stat(
+        label="99% of arrivals are available within",
+        value=f"{match_threshold(lag[1][1], lag_thresholds)} {lag[1][1]} seconds",
     )
-    return (departure_lag,)
+    return
 
 
 @app.cell
-def _(departure_lag, mo):
-    mo.stat(value=f"{departure_lag:.1%}", caption="of observed departures published within 15 seconds of departing")
+def _():
+    performance_thresholds = {1_000: "❌", 6: "⚠️", 0: "✅"}
+    return (performance_thresholds,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""### Performance""")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    How quickly can we process an event after it appears in `VehiclePositions`? Narrowly, this is our responsibility.
+
+    $$
+    performance = timestamp_{Flashback} - timestamp_{VehiclePositions}
+    $$
+
+    [Splunk captures this data as `duration` on an ongoing basis](https://mbta.splunkcloud.com/en-US/app/search/search?q=search%20index%3Dlamp-*%20process_name%3Dflashback%20status%3Dcomplete%20%7C%20bin%20duration%20span%3D3%20as%20duration%20%7C%20stats%20count(_raw)%20BY%20index%2C%20duration%20%7C%20sort%20index%2C%20duration&display.page.search.mode=verbose&dispatch.sample_ratio=1&workload_pool=&earliest=-24h%40h&latest=now&display.page.search.tab=statistics&display.general.type=statistics).
+
+    - ✅ if 99% of events are processed within 5 seconds
+    - ⚠️ if 99% of events are processed within 10 seconds
+    - ❌ if less than 99% of events are processed within 10 seconds
+    """
+    )
+    return
+
+
+@app.cell
+def _(mo, performance_thresholds):
+    mo.stat(
+        label="99% of events are processed within",
+        value=f"{match_threshold(5, performance_thresholds)} 5 seconds",
+    )
     return
 
 
