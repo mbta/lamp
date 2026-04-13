a5add8840ecb888ccf4682ab723cd74f66180b28
Henry Huang <1400827+huangh@users.noreply.github.com>
2026-04-13 07:47:14 -0400

    cleanup runner

--- src/lamp_py/ad_hoc/backfill_runner_terminal_predictions.py
+++ src/lamp_py/ad_hoc/backfill_runner_terminal_predictions.py
@@ -18,6 +18,7 @@ from lamp_py.ingestion.convert_gtfs_rt import GtfsRtConverter, TableData
 from lamp_py.ingestion.converter import ConfigType
 
 from lamp_py.aws.s3 import file_list_from_s3, upload_file
+from lamp_py.ingestion.daily.trip_updates import reprocess_trip_updates
 from lamp_py.runtime_utils.remote_files import springboard_rt_vehicle_positions
 from lamp_py.runtime_utils.process_logger import ProcessLogger
 from lamp_py.runtime_utils.remote_files import LAMP, S3_ARCHIVE, S3Location
@@ -301,10 +302,7 @@ def delta_reingestion_runner(
         converter.clean_local_folders()  # clear local output folders after each day to manage disk space
 
 
-def run_backfill() -> None:
-    """
-    Full encapsulated method to call all of this backfill job
-    """
+if __name__ == "__main__":
     local_tmp_output = "/tmp/gtfs-rt-continuous/"
 
     if not os.path.exists(local_tmp_output):
@@ -312,23 +310,4 @@ def run_backfill() -> None:
 
     start = datetime(2026, 3, 1, 0, 0, 0)
     end = datetime(2026, 3, 29, 0, 0, 0)
-
-    all_terminal_stops = LightRailFilter.terminal_stop_ids + HeavyRailFilter.terminal_stop_ids
-    polars_filter = pl.col("trip_update.trip.route_id").is_in(
-        ["Red", "Orange", "Blue", "Green-B", "Green-C", "Green-D", "Green-E", "Mattapan"]
-    ) & pl.col("trip_update.stop_time_update.stop_id").is_in(all_terminal_stops)
-
-    final_output_path = S3Location(S3_ARCHIVE, "lamp/adhoc/RT_TRIP_UPDATES_RAIL_TERMINAL")
-
-    delta_reingestion_runner(
-        start_date=start,
-        end_date=end,
-        local_output_location=local_tmp_output,
-        final_output_path=final_output_path,
-        polars_filter=polars_filter,
-        max_workers=4,
-    )
-
-
-if __name__ == "__main__":
-    run_backfill()
+    reprocess_trip_updates(start_date=start.date(), end_date=end.date())
