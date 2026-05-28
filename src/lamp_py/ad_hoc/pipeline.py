2453b20c0f5faa6d2691e78f924815e56e858bd6
Henry Huang <1400827+huangh@users.noreply.github.com>
2026-05-28 11:37:32 -0400

    reverting changes in pipeline that dont matter for this pr

--- src/lamp_py/ad_hoc/pipeline.py
+++ src/lamp_py/ad_hoc/pipeline.py
@@ -3,9 +3,10 @@
 import logging
 import os
 
-from lamp_py.ad_hoc.runner_004 import runner
 from lamp_py.aws.ecs import check_for_parallel_tasks
 from lamp_py.runtime_utils.env_validation import validate_environment
+
+from lamp_py.ad_hoc.runner_004 import runner
 
 logging.getLogger().setLevel("INFO")
 DESCRIPTION = """Entry Point For Ad-Hoc Runner"""
