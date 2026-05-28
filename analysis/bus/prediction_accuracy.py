1e20796c09018d4f11d79ef6fb169503feeb0930
Corey N. Runkel <crunkel@mbta.com>
2026-05-28 12:05:35 -0400

    Make that join

--- analysis/bus/prediction_accuracy.py
+++ analysis/bus/prediction_accuracy.py
@@ -9,6 +9,7 @@ def _():
     import marimo as mo
     import dataframely as dy
     import polars as pl
+    import numpy as np
     return dy, pl
 
 
@@ -18,7 +19,7 @@ def _(dy):
         service_date = dy.Date(primary_key = True)
         trip_id = dy.String(primary_key = True)
         vehicle_id = dy.String(primary_key = True)
-        stop_sequence = dy.String(primary_key = True)
+        stop_sequence = dy.Int16(primary_key = True)
         event_type = dy.Enum(categories = ["arrival", "departure"], primary_key = True)
     return (StopEvent,)
 
@@ -27,7 +28,7 @@ def _(dy):
 def _(StopEvent, dy):
     class Prediction(StopEvent):
         prediction_dt = dy.Datetime(nullable = True)
-        predicted_dt = dy.Datetime(nullable = True)
+        predicted_dt = dy.Datetime(primary_key = True)
     return (Prediction,)
 
 
@@ -47,7 +48,7 @@ def _(Actual, Prediction, dy):
 
 
 @app.cell
-def _(Actual, Comparison, Prediction, StopEvent, dy):
+def _(Actual, Comparison, Prediction, StopEvent, dy, pl):
     def make_comparison(predicted: dy.LazyFrame[Prediction], actual: dy.LazyFrame[Actual]) -> dy.LazyFrame[Comparison]:
         """Join predicted and actual events together."""
         lf = (
@@ -58,16 +59,21 @@ def _(Actual, Comparison, Prediction, StopEvent, dy):
                 how = "full",
                 coalesce = True
             )
+            .with_columns(
+                time_variance = pl.col("actual_dt") - pl.col("prediction_dt"),
+                time_to_actual = pl.col("actual_dt") - pl.col("predicted_dt")
+            )
+            .select(Comparison.column_names())
         )
-        valid = Comparison.validate(lf)
-        return valid
-    return
+        # valid = Comparison.validate(lf, cast = True)
+        return lf
+    return (make_comparison,)
 
 
 @app.cell
 def _(dy):
     class Accuracy(dy.Schema):
-        accuracy = dy.Float(nullable=True, min=0, max=0)
+        accuracy = dy.Float(nullable=True, min=0, max=1)
     return (Accuracy,)
 
 
@@ -79,23 +85,32 @@ def _(Accuracy, Comparison, dy, pl):
 
         More info at https://github.com/TransitApp/ETA-Accuracy-Benchmark
         """
-        accuracy_thresholds = {
-            "0–3m": (-30, 90),
-            "3–6m": (-60, 150),
-            "6–10m": (-60, 210),
-            "10–15m": (-90, 270),
+        lower_bound = {
+            "0–3m": -30,
+            "3–6m": -60,
+            "6–10m": -60,
+            "10–15m": -90,
+            "15m+": -120
+        }
+
+        upper_bound = {
+            "0–3m": 90,
+            "3–6m": 150,
+            "6–10m": 210,
+            "10–15m": 270,
+            "15m+": 330
         }
 
         lf = (
             comparison
             .with_columns(
-                time_bucket = pl.col("time_to_actual").cut(
+                time_bucket = pl.col("time_to_actual").dt.total_seconds().cut(
                     breaks = [
-                        pl.duration(seconds = 0),
-                        pl.duration(minutes = 3),
-                        pl.duration(minutes = 6),
-                        pl.duration(minutes = 10),
-                        pl.duration(minutes = 15)
+                        0,
+                        3 * 60,
+                        6 * 60,
+                        10 * 60,
+                        15 * 60
                     ],
                     labels = [
                         "<0m",
@@ -107,26 +122,42 @@ def _(Accuracy, Comparison, dy, pl):
                     ]
                 ),
             )
-            .filter(~pl.col("time_bucket").is_in(["0–3m", "15m+"]))
+            # .filter(~pl.col("time_bucket").is_in(["<0m", "15m+"]))
             .with_columns(
-                accuracy=pl.col("time_variance").is_between(
-                    pl.col("time_bucket").replace(accuracy_thresholds)
+                lower_bound=pl.col("time_bucket").cast(pl.String).replace_strict(lower_bound, default = None, return_dtype = pl.Int16),
+                upper_bound=pl.col("time_bucket").cast(pl.String).replace_strict(upper_bound, default = None, return_dtype = pl.Int16)
+            )
+            .with_columns(
+                accurate = pl.coalesce(
+                    pl.col("time_variance").dt.total_seconds().is_between(pl.col("lower_bound"), pl.col("upper_bound")),
+                    pl.lit(False)
                 )
             )
+            .group_by("time_bucket")
+            .agg(accuracy = pl.col("accurate").cast(pl.UInt8).mean())
         )
-    return
+
+        valid = Accuracy.validate(lf, eager = False)
+
+        return lf
+    return (ibi,)
 
 
 @app.cell
 def _(pl):
     date = pl.date(2026, 5, 21)
-    return
+    return (date,)
 
 
 @app.cell
 def _(Actual, pl):
     actual = (
         pl.scan_parquet('s3://mbta-performance/lamp/bus_vehicle_events/20260521.parquet')
+        .filter(
+            pl.col("vehicle_label").is_not_null(),
+            pl.col("gtfs_stop_sequence").is_not_null(),
+            pl.coalesce("gtfs_arrival_dt", "gtfs_departure_dt").is_not_null()
+        )
         .with_columns(
             vehicle_id = pl.col("vehicle_label"),
             stop_sequence = pl.col("gtfs_stop_sequence"),
@@ -146,29 +177,35 @@ def _(Actual, pl):
             event_type = pl.when(pl.col("event_type").eq("gtfs_arrival_dt"))
                 .then(pl.lit("arrival"))
                 .otherwise(pl.lit("departure"))
-                .cast(pl.Enum(categories = ["arrival", "departure"]))
+                .cast(pl.Enum(categories = ["arrival", "departure"])),
+            actual_dt = pl.col("actual_dt").dt.replace_time_zone("America/New_York")
         )
         .select(Actual.column_names())
     )
-    return
+    return (actual,)
 
 
 @app.cell
-def _(pl):
+def _(date, pl):
     predicted = (
         pl.scan_parquet('s3://mbta-ctd-dataplatform-springboard/lamp/RT_TRIP_UPDATES/year=2026/month=5/day=21/2026-05-21T00:00:00.parquet')
-        .slice(1_000_000, 10_000)
-        # .filter(pl.col("trip_update.trip.route_id").str.contains(r"^[1-9]")) # bus routes are numbered
+        .slice(offset = 100_000_000, length = 1_000_000)
+        .filter(
+            pl.col("trip_update.trip.route_id").str.contains(r"^[1-9]"),
+            pl.col("trip_update.vehicle.id").is_not_null(),
+            pl.col("trip_update.stop_time_update.stop_sequence").is_not_null(),
+            pl.coalesce("trip_update.stop_time_update.arrival.time", "trip_update.stop_time_update.departure.time").is_not_null()
+        ) # bus routes are numbered
         .with_columns(
             service_date = pl.col("trip_update.trip.start_date").str.strptime(dtype = pl.Date, format = "%Y%m%d"),
             trip_id = pl.col("trip_update.trip.trip_id"),
-            vehicle_id = pl.col("trip_update.vehicle.id"),
+            vehicle_id = pl.col("trip_update.vehicle.id").str.replace(pattern = "y|d", value = ""),
             stop_sequence = pl.col("trip_update.stop_time_update.stop_sequence"),
-            arrival = pl.col("trip_update.stop_time_update.arrival.time"),
+            arrival = pl.from_epoch("trip_update.stop_time_update.arrival.time"),
             departure = pl.from_epoch("trip_update.stop_time_update.departure.time"),
             predicted_dt = pl.from_epoch("trip_update.timestamp")
         )
-        # .filter(pl.col("service_date").eq(date))
+        .filter(pl.col("service_date").eq(date))
         .unique(keep = "first")
         .unpivot(
             index = [
@@ -176,12 +213,14 @@ def _(pl):
                 "trip_id",
                 "vehicle_id",
                 "stop_sequence",
+                "predicted_dt",
             ],
             on = ["arrival", "departure"],
             value_name = "prediction_dt", 
             variable_name = "event_type"
         )
         .with_columns(
+            pl.selectors.datetime().dt.replace_time_zone("America/New_York"),
             event_type = pl.col("event_type").cast(pl.Enum(categories = ["arrival", "departure"])),
         )
     )
@@ -191,6 +230,30 @@ def _(pl):
 @app.cell
 def _(predicted):
     predicted.collect()
+    return
+
+
+@app.cell
+def _(actual):
+    actual.collect()
+    return
+
+
+@app.cell
+def _(actual, make_comparison, predicted):
+    comparison = make_comparison(predicted, actual)
+    return (comparison,)
+
+
+@app.cell
+def _(comparison):
+    comparison.collect()
+    return
+
+
+@app.cell
+def _(comparison, ibi):
+    ibi(comparison).collect()
     return
 
 
